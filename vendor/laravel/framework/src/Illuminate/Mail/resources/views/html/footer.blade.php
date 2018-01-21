5babb3277a561bbfcc304014d72dffb628406470
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-01-21 20:47:32 +0300

    gabarliyak

---
+++ vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/footer.blade.php
@@ -0,0 +1,16 @@
+<tr>
+    <td>
+        <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0">
+            <tr>
+                <td class="content-cell" align="center">
+					<a href="{{ config('constants.facebook.share_website') }}" target="_blank">
+					  Share on Facebook
+					</a>
+                </td>
+                <td class="content-cell" align="center">
+                    {{ Illuminate\Mail\Markdown::parse($slot) }}
+                </td>
+            </tr>
+        </table>
+    </td>
+</tr>
