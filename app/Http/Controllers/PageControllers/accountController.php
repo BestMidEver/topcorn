08bbbdad661e6b1c0f40a1229abfc000d48cd11c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-01-31 14:57:56 +0300

    https

--- app/Http/Controllers/PageControllers/accountController.php
+++ app/Http/Controllers/PageControllers/accountController.php
@@ -38,6 +38,7 @@ class accountController extends Controller
 
 	public function change_profile(Request $request)
 	{
+		return $request->profile_pic;
 		$request->validate([
 			'name' => 'required|min:6',
 		]);
