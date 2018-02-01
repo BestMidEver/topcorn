08b296c38236fee90e426d8081fc65f5686fd2f3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-01 19:19:48 +0300

    https

--- resources/lang/en/long_texts.php
+++ resources/lang/en/long_texts.php
@@ -3,8 +3,8 @@
 return [
     'donation' => 'Topcorn.io is a project that has survived with the support of the people who loves cinema. Help us to provide you better results and more useful experience.',
     'hint_secondary_language' => 'If there is information that can not be found in the \'Primary Language\', they are filled in with the Secondary Language. You will also see the reviews which are written in the Secondary Language and the trailers in the Secondary Language.',
-    'hint_hover_title' => 'Film linklerinin üzerine fare imlecini götürdüğünüzde filmin orijinal ya da İkinci Dil\'inizdeki adını görebilirsiniz. Bu ayar arama sayfasını etkilemez.',
-    'hint_image_quality' => 'İnternet bağlantınıza uygun olarak görsel kalitesini seçebilirsiniz. Bu ayar, fragmanları etkilemez.',
-    'hint_full_screen' => 'Sağdan ve soldan bırakılan boşlukları azaltır.',
-    'hint_open_new_tab' => 'Film, kişi ve kullanıcıları yeni sekmede açar.',
+    'hint_hover_title' => 'When you hover your mouse cursor over the movie links, you can see the title of the movie in the original language of the movie or in your secondary language. This setting does not affect the search page.',
+    'hint_image_quality' => 'You can choose the image quality according to your internet connection. This setting does not affect trailers.',
+    'hint_full_screen' => 'This setting reduces the spaces from left and right.',
+    'hint_open_new_tab' => 'This setting opens movies, people and users in a new tab.',
 ];
---
+++ resources/lang/hu/long_texts.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'donation' => 'A Topcorn.io, egy olyan projekt, ami a filmkedvelők segítségével működik.  Támogass te is minket, hogy jobb eredményeket és hasznos élményt nyújtson.',
+    'hint_secondary_language' => 'Ha olyan információ áll rendelkezésre, amely nem található az \'anyanyelvben\', akkor a másodlagos nyelven lesz kitöltve. A másodlagos nyelven írt véleményeket és a másodlagos nyelven szereplő filmelőzeteseket is látni fogod.',
+    'hint_hover_title' => 'Ha az egérmutatót a filmlinkek fölé helyezed, akkor a film eredeti vagy a másodlagos nyelvű címét láthatod. Ez a beállítás nem érinti a keresési oldalt.',
+    'hint_image_quality' => 'Kiválaszthatod a képminőséget az internetkapcsolatnak megfelelően. Ez a beállítás nem érinti a filmelőzeteseket.',
+    'hint_full_screen' => 'Csökkentheted az oldal jobb és bal szélén lévő helyet.',
+    'hint_open_new_tab' => 'A film, az emberek és a felhasználók megnyitása új lapon.',
+]; 
