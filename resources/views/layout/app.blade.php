532b7060d326dba202cc56b4723ddddca283b4d0
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-02 16:15:34 +0300

    https

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -57,7 +57,7 @@
       var js, fjs = d.getElementsByTagName(s)[0];
       if (d.getElementById(id)) return;
       js = d.createElement(s); js.id = id;
-      js.src = 'https://connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.11';
+      js.src = 'https://connect.facebook.net/hu/sdk.js#xfbml=1&version=v2.11';
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
 
