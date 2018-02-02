99509603c4f7c874b3791e655069586c3fdadc79
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-02 11:28:27 +0300

    https

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,5 +5,5 @@
 @section('body')
 <h5 class="text-center text-md-left col mt-3 mt-md-4">{{ __('navbar.faq_long') }}</h5>
 
-<p>This product uses the TMDb API but is not endorsed or certified by TMDb.<img class="tmdblogo" src="/images/tmdb.svg"></p>
+<p>This product uses the TMDb API but is not endorsed or certified by TMDb.<a href="https://www.themoviedb.org/" target="_blank"><img class="tmdblogo ml-2" src="/images/tmdb.svg"></a></p>
 @endsection
