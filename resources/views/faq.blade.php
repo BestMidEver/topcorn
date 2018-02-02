e8d03878bd1ca0abe5580d2856d9e6aca4acde81
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-02 11:28:20 +0300

    https

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -5,5 +5,5 @@
 @section('body')
 <h5 class="text-center text-md-left col mt-3 mt-md-4">{{ __('navbar.faq_long') }}</h5>
 
-<p>This product uses the TMDb API but is not endorsed or certified by TMDb.<img class="img-fluid" src="/images/tmdb.svg"></p>
+<p>This product uses the TMDb API but is not endorsed or certified by TMDb.<img class="tmdblogo" src="/images/tmdb.svg"></p>
 @endsection
