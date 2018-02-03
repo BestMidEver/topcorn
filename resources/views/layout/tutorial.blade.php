de8c47f80acc409e07bc1c2dc31d141127d5529b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-03 20:01:52 +0300

    havent

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -20,7 +20,7 @@
 </div>
 @endif
 <div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLongTitle">2 - Arama Sayfası</h5>
