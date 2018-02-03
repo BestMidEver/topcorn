e6b667f6d817ef27d8f5c8ceba39f1ea280eaecd
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-03 12:21:12 +0300

    havent

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -146,7 +146,7 @@ class recommendationsController extends Controller
             'recommendations.this_id as id',
             'recommendations.movie_id as mother_movie_id',
             'movies.'.$hover_title.' as original_title',
-            DB::raw('sum(IF(recommendations.is_similar, 3, 7)*(rateds.rate-3.7))*movies.vote_average AS point'),
+            DB::raw('sum(IF(recommendations.is_similar, 3, 7)*(rateds.rate-3.9))*movies.vote_average AS point'),
             DB::raw('COUNT(*) as count'),
             'movies.vote_average',
             'movies.release_date',
@@ -158,7 +158,7 @@ class recommendationsController extends Controller
             'bans.id as ban_id'
         )
         ->groupBy('movies.id')
-        ->havingRaw('sum(IF(recommendations.is_similar, 3, 7)*(rateds.rate-3.7)) > 4 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
+        ->havingRaw('sum(IF(recommendations.is_similar, 3, 7)*(rateds.rate-3.9)) > 4 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
         ->orderBy('point', 'desc');
 
         if($request->f_genre != [])
