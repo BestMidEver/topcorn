e8e81b97689a5af41b25794795354909a7fa85ba
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-03 16:56:53 +0300

    havent

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -2,17 +2,17 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Kursa Giriş</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
-        ...
+        Zaten kullanımı çk basit olan topcornun bütün temel özelliklerini öğrenmen için sana 5 dakikalık bir kurs hazırladık. 
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sonra Yap</button>
+        <button type="button" class="btn btn-primary">Haydi Kursa Başlayalım</button>
       </div>
     </div>
   </div>
