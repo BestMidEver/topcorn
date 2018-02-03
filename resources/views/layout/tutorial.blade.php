c6b33927e6aa06c50bfc2b436f6c337091752842
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-03 17:11:18 +0300

    havent

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -1,4 +1,4 @@
-<div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
+<div class="modal fade d-none" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -12,7 +12,27 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Sonra Yap</button>
-        <button type="button" class="btn btn-primary">Kursa Başlayalım</button>
+        <button type="button" class="btn btn-primary">0 - Kursa Başlayalım</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">1 - Film Ara</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        Film aramak için gezinti çubuğunda "<i class="fa fa-search d-none d-md-inline" aria-hidden="true"></i> {{ __('navbar.search') }}" linkine tıklayıp "Arama" sayfasına gitmelisin.
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Sonra Yap</button>
+        <button type="button" class="btn btn-primary">0 - Kursa Başlayalım</button>
       </div>
     </div>
   </div>
