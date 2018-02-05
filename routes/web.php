3ceca312fc9bdd148bba79757c9db9d9f5f2e0a2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-05 12:15:12 +0300

    deneme

--- routes/web.php
+++ routes/web.php
@@ -231,7 +231,7 @@ Route::get('test', function(){
         ->havingRaw('sum(IF(recommendations.is_similar, 2, 7)*(rateds.rate-3.8)) > 4 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
         ->orderBy('point', 'desc');
 
-        if(["18"] != []){
+        if([] != []){
             $return_val = $return_val->join('genres', 'genres.movie_id', '=', 'movies.id')
             ->whereIn('genre_id', ["18"])
             ->havingRaw('COUNT(*)='.count(["18"]));
