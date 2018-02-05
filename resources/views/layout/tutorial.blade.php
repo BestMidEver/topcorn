87a92a7d4338f1b8c95278b1edf54f98ce6575da
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-05 15:27:08 +0300

    deneme

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -3,7 +3,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">0 - Kursa Giriş</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Kullanım Klavuzu</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -20,49 +20,8 @@
 </div>
 @endif
 
-@if(1)
-<div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">2 - Arama Sayfası</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <ul class="list-group">
-          <li class="list-group-item list-group-item-success">
-            <div class="h6 text-muted">Sayfaya Erişim</div>
-            Arama sayfasına ulaşmak için sayfanın en üstündeki gezinti çubuğundaki "<i class="fa fa-search" aria-hidden="true"></i>Ara" veya sayfanın en altındaki altbilgisinde "Film/Kişi/Kullanıcı Ara" linklerinden birine tıklamalısın.
-          </li>
-          <li class="list-group-item">
-            <div class="h6 text-muted">Film Arama</div>
-            Film aramak için "Film" sekmesinde arama çubuğuna aramak istediğin filmin adını yazmalısın.
-          </li>
-          <li class="list-group-item">
-            <div class="h6 text-muted">Aratılan Filmi Oylama</div>
-            Film aradıktan sonra çıkan listede muhtemelen birçok filmin olduğu liste göreceksin. Bu filmlerden aradığın filmin altındaki çubuktan "<i class="fa fa-star-half-o" aria-hidden="true"></i> İzledim" tuşuna basıp "1 - Peş Peşe Oylama" dersinde öğrendiğin gibi oylamalısın.
-            <div class="mt-2"><small>"<i class="fa fa-clock-o" aria-hidden="true"></i>" tuşuna basıp, daha sonra izlemeye karar verdiğin filmleri "Sonra İzle" listene kaydedebilirsin.</small></div>
-            <div class="mt-2"><small>"<i class="fa fa-ban" aria-hidden="true"></i>" tuşuna basıp, tavsiye listende görmek istemediğin filmleri "Banlananlar" listene kaydedebilirsin.</small></div>
-          </li>
-          <li class="list-group-item">
-            <div class="h6 text-muted">Kişi Arama</div>
-            Yönetmen, oyuncu, kameraman, yapımcı vb profesyonellerin hepsini kısaca kişi olarak adlandırdık. Kişi aramak için "Kişi" sekmesinde arama çubuğuna aramak istediğin kişinin adını yazabilirsin.
-          </li>
-          <li class="list-group-item">
-            <div class="h6 text-muted">Kullanıcı Arama</div>
-            Topcorn kullanıcılarını aramak için "Kullanıcı" sekmesinde arama çubuğuna aramak istediğin kullanıcının adını veya e-postasını yazabilirsin.
-          </li>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Önceki Ders</button>
-        <button type="button" class="btn btn-primary">Sonraki Ders</button>
-      </div>
-    </div>
-  </div>
-</div>
-@endif
+
+
 
 @if(0)
 <div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
@@ -103,6 +62,53 @@
             <div class="mt-2"><small><i class="fa fa-clock-o" aria-hidden="true"></i> tuşuna basıp, daha sonra izlemeye karar verdiğin filmleri "Sonra İzle" listene kaydedebilirsin.</small></div>
             <div class="mt-2"><small><i class="fa fa-ban" aria-hidden="true"></i> tuşuna basıp, tavsiye listende görmek istemediğin filmleri "Banlananlar" listene kaydedebilirsin.</small></div>
             <div class="mt-2"><small><i class="fa fa-undo" aria-hidden="true"></i> tuşuna basıp, son verdiğin oyu geri alabilirsin.</small></div>
+          </li>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Sonraki Ders</button>
+      </div>
+    </div>
+  </div>
+</div>
+@endif
+
+
+
+
+@if(1)
+<div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">2 - Arama Sayfası</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul class="list-group">
+          <li class="list-group-item list-group-item-success">
+            <div class="h6 text-muted">Sayfaya Erişim</div>
+            Arama sayfasına ulaşmak için sayfanın en üstündeki gezinti çubuğundaki "<i class="fa fa-search" aria-hidden="true"></i>Ara" veya sayfanın en altındaki altbilgisinde "Film/Kişi/Kullanıcı Ara" linklerinden birine tıklamalısın.
+          </li>
+          <li class="list-group-item">
+            <div class="h6 text-muted">Film Arama</div>
+            Film aramak için "Film" sekmesinde arama çubuğuna aramak istediğin filmin adını yazmalısın.
+          </li>
+          <li class="list-group-item">
+            <div class="h6 text-muted">Aratılan Filmi Oylama</div>
+            Film aradıktan sonra çıkan listede muhtemelen birçok filmin olduğu liste göreceksin. Bu filmlerden aradığın filmin altındaki çubuktan <span class="badge badge-secondary"><i class="fa fa-star-half-o" aria-hidden="true"></i> İzledim</span> tuşuna basıp "1 - Peş Peşe Oylama" dersinde öğrendiğin gibi oylamalısın.
+            <div class="mt-2"><small>Filmin posterine tıklayıp, filmin sayfasına gidebilirsin.</small></div>
+            <div class="mt-2"><small><i class="fa fa-clock-o" aria-hidden="true"></i> tuşuna basıp, daha sonra izlemeye karar verdiğin filmleri "Sonra İzle" listene kaydedebilirsin.</small></div>
+            <div class="mt-2"><small><i class="fa fa-ban" aria-hidden="true"></i> tuşuna basıp, tavsiye listende görmek istemediğin filmleri "Banlananlar" listene kaydedebilirsin.</small></div>
+          </li>
+          <li class="list-group-item">
+            <div class="h6 text-muted">Kişi Arama</div>
+            Yönetmen, oyuncu, kameraman, yapımcı vb profesyonellerin hepsini kısaca kişi olarak adlandırdık. Kişi aramak için "Kişi" sekmesinde arama çubuğuna aramak istediğin kişinin adını yazabilirsin.
+          </li>
+          <li class="list-group-item">
+            <div class="h6 text-muted">Kullanıcı Arama</div>
+            Topcorn kullanıcılarını aramak için "Kullanıcı" sekmesinde arama çubuğuna aramak istediğin kullanıcının adını veya e-postasını yazabilirsin.
           </li>
       </div>
       <div class="modal-footer">
