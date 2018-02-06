3b6c8bd6003e551d3463efc06978bfe6919b14fd
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-06 15:52:23 +0300

    level

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('level')->default(0);
             $table->string('facebook_id');
             $table->string('name');
             $table->string('cover_pic')->nullable();
