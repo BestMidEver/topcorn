445560fff9866bc44e56ea6917bebc4dadf8e4e2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-06 17:45:47 +0300

    level

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="tutorial" tabindex="-1" role="dialog" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 
     <!--USERS MANUAL-->
     <div class="modal-content" ng-if="current_level < 2">
