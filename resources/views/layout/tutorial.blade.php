2d01d0f3376a7c074b9020fd36d6ea16cb6b09e4
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-07 19:18:03 +0300

    training

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -127,17 +127,16 @@
             <div class="h6 text-muted">Sayfaya Erişim</div>
             Tavsiyeler sayfasına ulaşmak için sayfanın en üstündeki gezinti çubuğundaki <span class="h6 badge badge-secondary"><i class="fa fa-th-list" aria-hidden="true"></i> Tavsiyeler</span> veya sayfanın en altında altbilgisindeki <span class="h6 badge badge-secondary">Film Tavsiyeleri</span> linklerinden birine tıklayabilirsin.
             <div class="py-2"><span class="text-muted">Görev: </span><span ng-class="{'badge badge-danger':current_level < 301}">Tavsiyeler sayfasına git.</span> <i class="fa fa-check" ng-show="current_level > 300"></i></div>
-            <div class="h6 text-muted mt-4">En Yüksek Oy Alan Filmler</div>
+          </li>
+          <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 301}" ng-if="current_level > 300">
+            <div class="h6 text-muted">En Yüksek Oy Alan Filmler</div>
             En yüksek oy alan izlenmemiş filmleri görmek için <span class="h6 badge badge-secondary">En Yüksek Oy Alan</span> sekmesine tıklayabilirsin.
             <div class="h6 text-muted mt-4">En Popüler Filmler</div>
             En popüler izlenmemiş filmleri görmek için <span class="h6 badge badge-secondary">En Popüler</span> sekmesine tıklayabilirsin.
-            <div class="h6 text-muted mt-4">Benim Zevkime Göre</div>
+            <div class="h6 text-muted mt-4">Benim Zevkime Göre <small>(Bu kısmı kullanmadan önce kursu tamamlamanızı öneriyoruz.)</small></div>
             Bu liste, Topcorn'u Topcorn yapan listedir. Yalnızca size özel film önerilerini burada bulabilirsiniz. Unutmayın, Topcorn'a ne kadar doğru ve çok bilgi verirseniz bu liste o ölçüde zengin ve size uygun olacaktır.
             Senin zevkine göre seçilmiş filmleri görmek için <span class="h6 badge badge-secondary">Benim Zevkime Göre</span> sekmesine tıklayabilirsin.
-            <div class="mt-1"><small>Bu kısmı kullanmadan önce kursu tamamlamanızı öneriyoruz.</small></div>
-          </li>
-          <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 301}" ng-if="current_level > 300">
-            <div class="h6 text-muted">Tavsiyelerdeki Filmleri Oylama</div>
+            <div class="h6 text-muted mt-4">Tavsiyelerdeki Filmleri Oylama</div>
             Muhtemelen birçok filmin olduğu liste göreceksin. Bu filmlerden izlediğin filmlerin altındaki çubuktan <span class="badge badge-secondary"><i class="fa fa-star-half-o" aria-hidden="true"></i> İzledim</span> tuşuna basıp "1 - Peş Peşe Oylama" dersinde öğrendiğin gibi oylamalısın.
             <div class="py-2"><span class="text-muted">Görev: </span><span ng-class="{'badge badge-danger':current_level < 302}">İzlediğin 1 filmi oyla.</span> <i class="fa fa-check" ng-show="current_level > 301"></i></div>
             <div class="mt-2"><small>Filmin posterine tıklayıp, filmin sayfasına gidebilirsin.</small></div>
