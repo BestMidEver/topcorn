bd044752ef9f0e8be458c7c3fa2955b93ae9d054
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-07 19:23:01 +0300

    training

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -130,18 +130,15 @@
           </li>
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 301}" ng-if="current_level > 300">
             <div class="h6 text-muted">En Yüksek Oy Alan Filmler</div>
-            En yüksek oy alan izlenmemiş filmleri görmek için <span class="h6 badge badge-secondary">En Yüksek Oy Alan</span> sekmesine tıklayabilirsin.
+            <span class="h6 badge badge-secondary">En Yüksek Oy Alan</span> sekmesinde en yüksek oyu alan filmleri görebilirsin.
             <div class="h6 text-muted mt-4">En Popüler Filmler</div>
-            En popüler izlenmemiş filmleri görmek için <span class="h6 badge badge-secondary">En Popüler</span> sekmesine tıklayabilirsin.
+            <span class="h6 badge badge-secondary">En Popüler</span> sekmesinde en popüler filmleri görebilirsin.
             <div class="h6 text-muted mt-4">Benim Zevkime Göre <small>(Bu kısmı kullanmadan önce kursu tamamlamanızı öneriyoruz.)</small></div>
             Bu liste, Topcorn'u Topcorn yapan listedir. Yalnızca size özel film önerilerini burada bulabilirsiniz. Unutmayın, Topcorn'a ne kadar doğru ve çok bilgi verirseniz bu liste o ölçüde zengin ve size uygun olacaktır.
-            Senin zevkine göre seçilmiş filmleri görmek için <span class="h6 badge badge-secondary">Benim Zevkime Göre</span> sekmesine tıklayabilirsin.
+            Yalnızca senin zevkine göre seçilmiş filmleri görmek için <span class="h6 badge badge-secondary">Benim Zevkime Göre</span> sekmesine tıklayabilirsin.
             <div class="h6 text-muted mt-4">Tavsiyelerdeki Filmleri Oylama</div>
             Muhtemelen birçok filmin olduğu liste göreceksin. Bu filmlerden izlediğin filmlerin altındaki çubuktan <span class="badge badge-secondary"><i class="fa fa-star-half-o" aria-hidden="true"></i> İzledim</span> tuşuna basıp "1 - Peş Peşe Oylama" dersinde öğrendiğin gibi oylamalısın.
             <div class="py-2"><span class="text-muted">Görev: </span><span ng-class="{'badge badge-danger':current_level < 302}">İzlediğin 1 filmi oyla.</span> <i class="fa fa-check" ng-show="current_level > 301"></i></div>
-            <div class="mt-2"><small>Filmin posterine tıklayıp, filmin sayfasına gidebilirsin.</small></div>
-            <div class="mt-2"><span class="h6 badge badge-secondary"><i class="fa fa-clock-o" aria-hidden="true"></i></span><small> tuşuna basıp, daha sonra izlemeye karar verdiğin filmleri "Sonra İzle" listene kaydedebilirsin.</small></div>
-            <div class="mt-2"><span class="h6 badge badge-secondary"><i class="fa fa-ban" aria-hidden="true"></i></span><small> tuşuna basıp, tavsiye listende görmek istemediğin filmleri "Banlananlar" listene kaydedebilirsin.</small></div>
             <div class="h6 text-muted mt-4">Filmfiltre</div>
             <span class="h6 badge badge-secondary"><i class="fa fa-filter" aria-hidden="true"></i> SÜZGEÇ</span> tuşuna basıp filtreyi ayarlayabilir; listedeki filmleri orijinal diline, türüne ve yılına göre daraltabilirsin.
           </li>
