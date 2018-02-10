8a487a7e96c77d104f0690680c6def6970d55f5f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-10 21:11:54 +0300

    level

--- resources/views/layout/tutorial.blade.php
+++ resources/views/layout/tutorial.blade.php
@@ -168,7 +168,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" ng-click="show_previous('recommendations);">{{ __('tutorial.previous_lesson') }}</button>
-        <button type="button" class="btn btn-primary" ng-hide="current_level < 401" ng-click="level_up(500)">{{ __('tutorial.last_quest_3') }}Mezuniyet</button>
+        <button type="button" class="btn btn-primary" ng-hide="current_level < 401" ng-click="level_up(500)">{{ __('tutorial.last_quest_3') }}</button>
       </div>
     </div>
     <!--VOTE MOVIES-->
@@ -179,7 +179,7 @@
     <!--GRADUATION-->
     <div class="modal-content" ng-if="current_level > 499 && current_level < 600 && show_previous_tutorial != 'quick rate' && show_previous_tutorial != 'search' && show_previous_tutorial != 'recommendations' && show_previous_tutorial != 'last mission'">
       <div class="modal-header">
-        <h5 class="modal-title">5 - Mezuniyet</h5>
+        <h5 class="modal-title">5 - {{ __('tutorial.settings') }}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -187,35 +187,35 @@
       <div class="modal-body">
         <ul class="list-group">
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 500}">
-            <div class="h6 text-muted">Ayarlar Sayfasına Erişim</div>
-            Ayarlar sayfasına ulaşmak için sayfanın en üstünde gezinti çubuğundaki <span class="badge badge-secondary"><i class="fa fa fa-caret-down" aria-hidden="true"></i></span> > <span class="badge badge-secondary">Ayarlar</span> veya sayfanın en altında altbilgisindeki <span class="badge badge-secondary">Ayarlar</span> linklerinden birine tıklayabilirsin.
-            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 501}">Ayarlar sayfasına git.</span> <i class="fa fa-check" ng-show="current_level > 500"></i></div>
+            <div class="h6 text-muted">{{ __('tutorial.settings_1') }}</div>
+            {!! __('tutorial.settings_2') !!}
+            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 501}">{{ __('tutorial.settings_mission') }}</span> <i class="fa fa-check" ng-show="current_level > 500"></i></div>
           </li>
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 501}" ng-if="current_level > 500">
-            <div class="h6 text-muted">Kapak Fotoğrafı Seçme</div>
-            Kapak fotoğrafı açılır listesinden en çok beğendiğin filmlerden birinin kapak fotoğrafını kendi kapak fotoğrafın olarak belirleyebilirsin.
-            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 502}">Bir kapak fotoğrafı seç.</span> <i class="fa fa-check" ng-show="current_level > 501"></i></div>
+            <div class="h6 text-muted">{{ __('tutorial.settings_3') }}</div>
+            {{ __('tutorial.settings_4') }}
+            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 502}">{{ __('tutorial.settings_mission_2') }}</span> <i class="fa fa-check" ng-show="current_level > 501"></i></div>
           </li>
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 502}" ng-if="current_level > 501">
-            <div class="h6 text-muted">Profil Fotoğrafı Seçme</div>
-            Kapak fotoğrafı seçtikten sonra, o filmde oynayan oyunculardan birinin profil fotoğrafını; ya da facebook ile giriş yaptıysan facebook profil fotoğrafını profil fotoğrafı açılır listesinden belirleyebilirsin.
-            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 503}">Bir profil fotoğrafı seç.</span> <i class="fa fa-check" ng-show="current_level > 502"></i></div>
+            <div class="h6 text-muted">{{ __('tutorial.settings_5') }}</div>
+            {{ __('tutorial.settings_6') }}
+            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 503}">{{ __('tutorial.settings_mission_3') }}</span> <i class="fa fa-check" ng-show="current_level > 502"></i></div>
           </li>
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 503}" ng-if="current_level > 502">
-            <div class="h6 text-muted">Değişiklikleri Kaydetme</div>
-            Profilini ayarladıktan sonra değişiklikleri kaydetmek için <span class="badge badge-secondary">Değişiklikleri Kaydet</span> tuşuna basabilirsin.
-            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 504}">Değişiklikleri kaydet.</span> <i class="fa fa-check" ng-show="current_level > 503"></i></div>
+            <div class="h6 text-muted">{{ __('tutorial.settings_7') }}</div>
+            {!! __('tutorial.settings_8') !!}
+            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 504}">{{ __('tutorial.settings_mission_4') }}</span> <i class="fa fa-check" ng-show="current_level > 503"></i></div>
           </li>
           <li class="list-group-item" ng-class="{'list-group-item-success':current_level > 504}" ng-if="current_level > 503">
-            <div class="h6 text-muted">Profil Sayfasına Erişim</div>
-            Kendi profilini görmek için sayfanın en üstünde gezinti çubuğundaki <span class="badge badge-secondary"><i class="fa fa-user-circle-o" aria-hidden="true"></i> Profilim</span> veya sayfanın en altında altbilgisindeki <span class="badge badge-secondary">Profilim</span> linklerinden birine tıklayabilirsin.
-            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 505}">Profil sayfana git.</span> <i class="fa fa-check" ng-show="current_level > 504"></i></div>
+            <div class="h6 text-muted">{{ __('tutorial.settings_9') }}</div>
+            {!! __('tutorial.settings_10') !!}
+            <div class="py-2"><span class="text-muted">{{ __('tutorial.quest') }}: </span><span ng-class="{'badge badge-danger':current_level < 505}">{{ __('tutorial.settings_mission_5') }}</span> <i class="fa fa-check" ng-show="current_level > 504"></i></div>
           </li>
         </ul>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" ng-click="show_previous('last mission');">Önceki Ders</button>
-        <button type="button" class="btn btn-primary" ng-hide="current_level < 505" ng-click="level_up(600)">Diplomanı Al</button>
+        <button type="button" class="btn btn-secondary" ng-click="show_previous('last mission')">{{ __('tutorial.previous_lesson') }}</button>
+        <button type="button" class="btn btn-primary" ng-hide="current_level < 505" ng-click="level_up(600)">{{ __('tutorial.settings_11') }}</button>
       </div>
     </div>
     <!--GRADUATION-->
