acd201d77937cda8db711da622179777876645f5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-12 08:44:06 +0300

    level

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -165,9 +165,9 @@ class recommendationsController extends Controller
         ->orderBy('point', 'desc');
 
         if($request->f_genre != []){
-            $return_val = $return_val->join('genres', 'genres.movie_id', '=', 'movies.id');
-            /*->whereIn('genre_id', $request->f_genre)
-            ->havingRaw('COUNT(*)='.count($request->f_genre));*/
+            $return_val = $return_val->join('genres', 'genres.movie_id', '=', 'movies.id')
+            ->whereIn('genre_id', $request->f_genre);
+            /*->havingRaw('COUNT(*)='.count($request->f_genre));*/
         }
 
         if($request->f_lang != [])
