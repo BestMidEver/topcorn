0182e3d4f54292befc2b4238b343088ef03b39ba
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-17 17:46:17 +0300

    level

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -6,7 +6,7 @@
 <h5 class="text-center text-md-left col mt-3 mt-md-4">{{ __('navbar.donation') }}</h5>
 <button id="gabar" type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="bottom" title="Popover title" data-html="true">Click to toggle popover</button>
 <div id="popover-content-gabar" class="d-none">
-	burası da dutlug <div class="gabarx">gabalar</div>
+	burası da dutlug <button class="gabarx">gabalar</button>
 </div>
 <div class="p-3">
 	<p class="lead">{{ __('long_texts.donation') }}</p>
