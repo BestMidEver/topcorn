67db62a13a81e1cc1f6a921ddb31e3b6820ebcd7
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-17 18:57:22 +0300

    level

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -4,8 +4,8 @@
 
 @section('body')
 <h5 class="text-center text-md-left col mt-3 mt-md-4">{{ __('navbar.donation') }}</h5>
-<button uib-popover-template="'myPopoverTemplate'" popover-title="taytil" type="button" class="btn btn-default" ng-click="gabar()">Popover With Template</button>
-<script type="text/ng-template" id="myPopoverTemplate">
+<button popover-placement="right-top" uib-popover-template="'tpl.html'" popover-title="taytil" type="button" class="btn btn-default" ng-click="gabar()">Popover With Template</button>
+<script type="text/ng-template" id="tpl.html">
     <div>
       asd
     </div>
