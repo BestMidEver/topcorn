ce3a3fe6b21e9d8b4ee2db12a23d6cc2dffd0069
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-17 16:31:03 +0300

    level

--- resources/views/donation.blade.php
+++ resources/views/donation.blade.php
@@ -6,7 +6,7 @@
 <h5 class="text-center text-md-left col mt-3 mt-md-4">{{ __('navbar.donation') }}</h5>
 <button id="gabar" type="button" class="btn btn-lg btn-danger" data-toggle="popover" data-placement="bottom" title="Popover title" data-html="true">Click to toggle popover</button>
 <div id="popover-content-gabar" class="d-none">
-	burası da dutlug <button ng-click="savar.gabar()">gabalar</button>
+	burası da dutlug <a href="https://topcorn.io/donations">gabalar</a>
 </div>
 <div class="p-3">
 	<p class="lead">{{ __('long_texts.donation') }}</p>
