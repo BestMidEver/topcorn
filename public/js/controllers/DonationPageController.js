b0de0603df41fcb4dac8acc63f4c7e6bee9c96b9
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-19 13:13:52 +0300

    level

--- public/js/controllers/DonationPageController.js
+++ public/js/controllers/DonationPageController.js
@@ -225,6 +225,7 @@ MyApp.controller('DonationPageController', function($scope, $http, rate)
 	}
 
 	if(pass.get_watched_movie_number < 500){
+		console.log(pass.get_watched_movie_number)
 		$scope.get_watched_movie_number = function(lvl){
 			rate.get_watched_movie_number()
 			.then(function(response){
