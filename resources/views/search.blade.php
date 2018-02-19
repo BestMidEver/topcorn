dffd2a4d2a5e4ef9272afc05efdce686f955b291
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-19 17:01:06 +0300

    level

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -20,17 +20,11 @@
 </div>
 
 <div class="container-fluid mt-3">
-	<div class="input-group mb-3">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="basic-addon1">@</span>
-  </div>
-  <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-</div>
 	<div class="row">
 		<div class="col"></div>
 		<div class="input-group input-group-lg col-12 col-xl-8">
 			<div class="input-group-prepend">
-				<span class="input-group-addon" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
+				<span class="input-group-text" id="basic-addon1"><i class="fa fa-search" aria-hidden="true"></i></span>
 			</div>
 			<input type="text" id="input_movie" class="form-control" ng-show="active_tab=='movie'" placeholder="{{ __('general.search_movie') }}" aria-describedby="basic-addon1" ng-model="model.movie" ng-change="get_page_data()" ng-model-options="{debounce: 750}" autofocus>
 			<input type="text" id="input_person" class="form-control" ng-show="active_tab=='person'" placeholder="{{ __('general.search_person') }}" aria-describedby="basic-addon1" ng-model="model.person" ng-change="get_page_data()" ng-model-options="{debounce: 750}">
