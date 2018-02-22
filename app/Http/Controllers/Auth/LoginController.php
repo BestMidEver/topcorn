07f578dab2ef057e63e4839fae5d4fdc0f462b08
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-22 11:22:29 +0300

    level

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
         $findUser = User::where(['email' => $userSocial->getEmail()])->first();
 
         if($findUser){
-            Auth::login($findUser, 1);
+            Auth::login($findUser);
 
             $user = Auth::user();
             if($user->facebook_profile_pic == null){
@@ -74,7 +74,7 @@ class LoginController extends Controller
             $user->secondary_lang = Session::get('secondary_lang');
             $user->facebook_profile_pic = $userSocial->avatar;
             $user->save();
-            Auth::login($user, 1);
+            Auth::login($user);
             return redirect()->intended();
         }
     }
