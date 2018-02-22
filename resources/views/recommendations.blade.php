83ee27e3fb1a3613ff50dcf37b32f836eab8e354
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-22 13:51:45 +0300

    remember me

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -4,7 +4,7 @@
 
 @section('body')
 <div class="col mt-1 mb-2 mt-md-4">
-	<h5 class="d-inline align-middle my-2">{{ __('navbar.recommendations') }}</h5>
+	<h5 class="d-inline align-middle my-2">{{ __('navbar.recommendations') }}{{session('remember_me')}}</h5>
 	<button class="btn btn-warning btn-sm text-white ml-3 my-2" type="button" disabled>{{ Auth::user()->name }}</button>
 	<button class="btn btn-warning btn-sm text-white mx-1 my-2" type="button" ng-repeat="user in party_members" ng-click="remove_from_party(user.user_id);">@{{user.name}} <i class="fa fa-times"></i></button>
 	<button class="btn btn-outline-warning btn-sm my-2" type="button" data-toggle="collapse" data-target="#collapseAdd" ng-click="setFocus('input_user')"><i class="fas fa-user-plus"></i> {{ __('general.add_person') }}</button>
