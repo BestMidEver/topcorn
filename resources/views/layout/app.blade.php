6ccb154bd8c11a9874c553db1fd650f2c88a7506
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-24 20:15:18 +0300

    movie

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,6 +10,8 @@
         @show
     </title>
 
+    <link rel="icon" type="image/png" href="/images/topcorn_logo">
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/style.css">
 
