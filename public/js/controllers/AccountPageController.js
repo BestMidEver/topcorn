bf431c9bc943357657fad6cf2bcc370de399dd70
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-26 13:11:59 +0300

    movie

--- public/js/controllers/AccountPageController.js
+++ public/js/controllers/AccountPageController.js
@@ -3,12 +3,9 @@ MyApp.controller('AccountPageController', function($scope, $http, rate)
 	$scope.is_save_disabled = true;
 
 	$scope.check_save_disabled = function(){
-		console.log(pass.user_name, $scope.user_name)
-		console.log(pass.cover_src, $scope.cover_path)
-		console.log(pass.profile_src, $scope.profile_path)
 		if(pass.user_name != $scope.user_name
 			|| ($scope.cover_path != '' && pass.cover_src != $scope.cover_path)
-			|| $scope.profile_path != ''){
+			|| $scope.profile_path != '' && pass.profile_src != $scope.profile_path){
 			$scope.is_save_disabled = false;
 		}else{
 			$scope.is_save_disabled = true;
