a928c2099828a920a28dc6f6624df0ddbf80bd8d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-27 23:35:12 +0300

    movie

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -28,7 +28,7 @@
 						</div>
 						<div class="d-flex flex-row justify-content-end p-2 text-right">
 							<div ng-if="movie.vote_average > 0">
-								<div><span class="text-warning display-4 h6">@{{movie.vote_average}}</span><span class="text-white"> <small>/10</small></span></div>
+								<div><span class="text-warning display-4">@{{movie.vote_average}}</span><span class="text-white"> <small>/10</small></span></div>
 								<div><span class="text-white"><small>@{{movie.vote_count}}</small></span><span class="text-white"> <small><span ng-if="movie.vote_count == 1">{{ __('general.person_time') }}</span><span ng-if="movie.vote_count > 1">{{ __('general.person_times') }}</span></small></span></div>
 							</div>
 						</div>
@@ -61,7 +61,7 @@
 						</div>
 						<div class="col pb-2 pr-2 text-right">
 							<div ng-if="movie.vote_average > 0">
-								<div><span class="text-warning display-4 h6">@{{movie.vote_average}}</span><span class="text-white"> <small>/10</small></span></div>
+								<div><span class="text-warning display-4">@{{movie.vote_average}}</span><span class="text-white"> <small>/10</small></span></div>
 								<div><span class="text-white"><small>@{{movie.vote_count}}</small></span><span class="text-white"> <small><span ng-if="movie.vote_count == 1">{{ __('general.person_time') }}</span><span ng-if="movie.vote_count > 1">{{ __('general.person_times') }}</span></small></span></div>
 							</div>
 						</div>
