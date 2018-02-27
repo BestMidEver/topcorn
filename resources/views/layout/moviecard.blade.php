3b9a67209b1714a154155e2d57dc4c70f4a9602e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-27 16:55:25 +0300

    movie

--- resources/views/layout/moviecard.blade.php
+++ resources/views/layout/moviecard.blade.php
@@ -7,14 +7,14 @@
 					<div class="custom-over-layer h-50 d-flex flex-column justify-content-center">
 						<div class="d-flex flex-row justify-content-center" ng-if="movie.percent > 0">
 							<div class="text-white">
-								<span class="h5">@{{movie.percent}}%</span>
+								<span class="h5">@{{movie.percent}}</span>%
 							</div>
 						</div>
 					</div>
 					<div class="custom-over-layer-bottom h-50 d-flex flex-column justify-content-center">
 						<div class="d-flex flex-row justify-content-center" ng-if="movie.vote_average > 0">
 							<div class="text-white">
-								<span class="h6">@{{movie.vote_average}}/10</span>
+								<span class="h6">@{{movie.vote_average}}</span>/10
 							</div>
 						</div>
 					</div>
