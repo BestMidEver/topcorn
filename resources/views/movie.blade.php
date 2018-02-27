e0da6c9ef32dd25255b9b8c7588b8d87e7d141c2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-27 20:47:50 +0300

    movie

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -17,7 +17,7 @@
 							</div>
 							<div class="col p-2 text-right">
 								<div ng-if="movie.vote_average > 0">
-									<small>Film zevkine göre</small>
+									<small class="text-white">Film zevkine göre</small>
 									<div><span class="text-warning display-4 h6">%@{{user_movie_record.percent}}</span><span class="text-white"> <small>eşleşme</small></span></div>
 									<div><span class="text-white"><small>@{{movie.vote_count}}</small></span><span class="text-white"> <small>puan</small></span></div>
 								</div>
