c568606885016cd8608c42513e427496703f2c1e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-28 00:03:45 +0300

    movie

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -45,8 +45,7 @@
 						</div>
 						<div class="col pb-2 pr-2 text-right">
 							<div ng-if="user_movie_record.percent > 0">
-								<small class="text-white">Film zevkine göre</small>
-								<div><span class="text-warning display-4 d-none d-md-inline">%@{{user_movie_record.percent}}</span><span class="text-warning h5 d-md-none">%@{{user_movie_record.percent}}</span><span class="text-white"> <small>eşleşme</small></span></div>
+								<div><small class="text-white">Film zevkine göre</small><span class="text-warning h4 d-none d-md-inline">%@{{user_movie_record.percent}}</span><span class="text-warning h5 d-md-none">%@{{user_movie_record.percent}}</span><span class="text-white"> <small>eşleşme</small></span></div>
 								<div><span class="text-white"><small>@{{user_movie_record.point}}</small></span><span class="text-white"> <small>puan</small></span></div>
 							</div>
 						</div>
