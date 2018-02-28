81b86566a33530ff57d8396410ee018bdcd4b56d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-28 14:13:01 +0300

    movie

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -165,9 +165,9 @@ class recommendationsController extends Controller
             'bans.id as ban_id'
         )
         ->groupBy('movies.id')
-        ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > 18 AND sum(rateds.rate)*20 DIV COUNT(movies.id) > 60 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
-        ->orderBy('percent', 'desc')
-        ->orderBy('point', 'desc');
+        ->havingRaw('sum(rateds.rate)*20 DIV COUNT(movies.id) > 60 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
+        ->orderBy('point', 'desc')
+        ->orderBy('percent', 'desc');
 
         if($request->f_genre != []){
             $return_val = $return_val->join('genres', 'genres.movie_id', '=', 'movies.id')
