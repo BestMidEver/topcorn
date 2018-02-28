45c01e0ceaea409ee29df9501cc5b2fab10aeac9
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-02-28 18:31:30 +0300

    movie

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -165,7 +165,7 @@ class recommendationsController extends Controller
             'bans.id as ban_id'
         )
         ->groupBy('movies.id')
-        ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > 9 AND sum(rateds.rate)*20 DIV COUNT(movies.id) > 70 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
+        ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > 7 AND sum(rateds.rate)*20 DIV COUNT(movies.id) > 70 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0')
         ->orderBy('p2', 'desc')
         ->orderBy('point', 'desc');
 
