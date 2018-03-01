96ef7560ae5651588e7ece875df9677874a54901
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-01 23:46:45 +0300

    movie

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -14,8 +14,11 @@
 				<div class="d-flex flex-row align-items-center ml-2">
 					<h5><a ng-href="http://www.google.com/search?q=@{{person.name}}" class="text-light" target="_blank"><span class="yeswrap text-left">{{config('constants.gabar')}}@{{person.name}} @{{age}} - {{ __('general.hismovies') }}</span></a></h5>
 				</div>
-				<div class="d-flex flex-row align-items-center text-light ml-2" ng-if="person.place_of_birth.length">
+				<div class="d-flex flex-row align-items-center text-light ml-2" ng-if="person.place_of_birth">
 					<i class="fas fa-map-marker"></i><div class="d-inline pl-1" >@{{person.place_of_birth}}</div>
+				</div>
+				<div class="d-flex flex-row align-items-center text-light ml-2" ng-if="person.deathday">
+					<img src="/images/rip.png" alt="rip" class="img-thumbnail"></i><div class="d-inline pl-1" >@{{person.deathday}}</div>
 				</div>
 			</div>
 		</div>
