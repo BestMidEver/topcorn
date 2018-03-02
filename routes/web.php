b4efde00e7457a8e03b75310d07c99ed7183666b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-02 20:30:15 +0300

    movie

--- routes/web.php
+++ routes/web.php
@@ -195,7 +195,7 @@ Route::get('test', function(){
 	$subquery = DB::table('rateds')
     ->whereIn('rateds.user_id', [7])
     ->where('rateds.rate', '>', 0)
-    ->whereIn('original_language', 'en')
+    ->whereIn('original_language', ['en'])
     ->leftjoin('recommendations', 'recommendations.movie_id', '=', 'rateds.movie_id')
     ->select(
         'recommendations.this_id as id',
