5c7a6cb8a1b5e0795c694124f9a93f527dbd2bf0
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-02 15:09:14 +0300

    performance

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -142,7 +142,7 @@ class recommendationsController extends Controller
         ->where('rateds.rate', '>', 0)
         ->leftjoin('recommendations', 'recommendations.movie_id', '=', 'rateds.movie_id')
         ->join('movies', 'movies.id', '=', 'recommendations.this_id')
-        /*->leftjoin('rateds as r2', function ($join) use ($request) {
+        ->leftjoin('rateds as r2', function ($join) use ($request) {
             $join->on('r2.movie_id', '=', 'movies.id')
             ->whereIn('r2.user_id', $request->f_users);
         })
@@ -155,7 +155,7 @@ class recommendationsController extends Controller
             $join->on('bans.movie_id', '=', 'movies.id')
             ->whereIn('bans.user_id', $request->f_users);
         })
-        ->where('bans.id', '=', null)*/
+        ->where('bans.id', '=', null)
         ->select(
             'recommendations.this_id as id',
             //'recommendations.movie_id as mother_movie_id',
