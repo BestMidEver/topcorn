cb1fe187e97493321817ffae99cc757d3a97873b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-03 22:26:51 +0300

    movie

--- routes/web.php
+++ routes/web.php
@@ -214,7 +214,7 @@ Route::get('test', function(){
         DB::raw('sum(rateds.rate)*20 DIV COUNT(recommendations.this_id) as percent'),
         DB::raw('sum(rateds.rate*recommendations.is_similar)*4 DIV COUNT(recommendations.this_id) as p2'),
         'r2.id as rated_id',
-        'r2.rate as rate_code',
+        'r2.rate as rate_code'
     )
     ->groupBy('recommendations.this_id')
     ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > 7 AND sum(rateds.rate)*20 DIV COUNT(recommendations.this_id) > 75 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0');
