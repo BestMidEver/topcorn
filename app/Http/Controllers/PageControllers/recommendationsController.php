1cb9edeaa154a0c2528757812a662f5508afb0e1
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-04 00:15:43 +0300

    movie

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -158,7 +158,7 @@ class recommendationsController extends Controller
             'r2.rate as rate_code'
         )
         ->groupBy('recommendations.this_id')
-        ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > 7 AND sum(rateds.rate)*20 DIV COUNT(recommendations.this_id) > 75 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0');
+        ->havingRaw('sum((rateds.rate-3)*recommendations.is_similar) > '.count($request->f_users)*7.' AND sum(rateds.rate)*20 DIV COUNT(recommendations.this_id) > 75 AND sum(IF(r2.id IS NULL OR r2.rate = 0, 0, 1)) = 0');
 
         if($request->f_lang != [])
         {
