8555a14e8c6f634a0f69788ea9c40cc0c412fd75
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-05 19:39:48 +0300

    movie

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -103,17 +103,17 @@ class recommendationsController extends Controller
 
         if($request->f_lang != [])
         {
-            $subq = $subq->whereIn('movies.original_language', $request->f_lang);
+            $subq = $subq->whereIn('m2.original_language', $request->f_lang);
         }
 
         if($request->f_min != 1917)
         {
-            $subq = $subq->where('movies.release_date', '>=', Carbon::create($request->f_min,1,1));
+            $subq = $subq->where('m2.release_date', '>=', Carbon::create($request->f_min,1,1));
         }
 
         if($request->f_max != 2018)
         {
-            $subq = $subq->where('movies.release_date', '<=', Carbon::create($request->f_max,12,31));
+            $subq = $subq->where('m2.release_date', '<=', Carbon::create($request->f_max,12,31));
         }
 
         $qqSql_2 = $subq_2->toSql();
