f96d92ad89467f2a5e143e9f4090ea01e2026e5c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-06 20:46:10 +0300

    recom

--- public/js/app.js
+++ public/js/app.js
@@ -38,7 +38,6 @@ $(document).ready(function() {
 		$(this).popover({
 			container: 'body',
 			html: true,
-			placement: 'bottom',
 			trigger: 'manual',
 			content: function() {
 				var id = $(this).attr('id')
