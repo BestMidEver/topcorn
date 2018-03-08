0e5f69e18b68d173dea3a1171f3e24a582162cee
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-08 20:57:24 +0300

    popover

--- resources/views/layout/pagination.blade.php
+++ resources/views/layout/pagination.blade.php
@@ -1,6 +1,6 @@
 <nav aria-label="Page navigation" class="mt-5" ng-show="pagination{{ $suffix }} > 1">
   <ul class="pagination justify-content-center mb-0" ng-init="current_page{{ $suffix }}=1">
-    <li class="page-item" ng-class="{'active':$index+1==current_page{{ $suffix }}}" ng-repeat="p in [] | range:pagination{{ $suffix }}" ng-show="$index+1==1 || ($index+1>current_page{{ $suffix }}-3 && $index+1<current_page{{ $suffix }}+3) || $index+1==pagination{{ $suffix }} || (current_page{{ $suffix }}<4 && $index+1<6) || (current_page{{ $suffix }}>pagination{{ $suffix }}-3 && $index+1>pagination{{ $suffix }}-5)">
+    <li class="page-item" ng-class="{'active':$index+1==current_page{{ $suffix }}}" ng-repeat="p in [] | range:pagination{{ $suffix }}" ng-show="$index+1==1 || ($index+1>current_page{{ $suffix }}-3 && $index+1<current_page{{ $suffix }}+3) || $index+1==pagination{{ $suffix }} || (current_page{{ $suffix }}<5 && $index+1<7) || (current_page{{ $suffix }}>pagination{{ $suffix }}-4 && $index+1>pagination{{ $suffix }}-6)">
       <button type="button" class="page-link" ng-click="paginate{{ $suffix }}($index+1)">@{{$index+1}}</button>
     </li>
   </ul>
