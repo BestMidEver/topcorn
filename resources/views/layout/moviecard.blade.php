8f454e25160820877376600a6c43f7de113f0896
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-08 18:13:17 +0300

    popover

--- resources/views/layout/moviecard.blade.php
+++ resources/views/layout/moviecard.blade.php
@@ -40,12 +40,12 @@
 					<div class="col">
 						<button type="button" class="btn btn-outline-secondary btn-sm btn-block addlater border-0" ng-class="{'text-warning':movie.later_id!=null}" ng-click="later($index)"><span ng-show="movie.later_id!=null"><i class="fas fa-clock"></i></span><span ng-show="movie.later_id==null"><i class="far fa-clock"></i></span></button>
 					</div>
-					<div class="col-7">
-						<button type="button" class="btn btn-sm btn-block border-0" ng-class="rate_class(movie.rate_code)" ng-click="votemodal($index, movie)"
+					<div class="col-7"
 						@if(Auth::User()->tt_search < 50)
 			            data-toggle="popover" data-placement="bottom" title='İpucu<a class="close tooltip-x" href="#close-tooltip">&times;</a>' ng-attr-id="vote@{{$index}}"
 			            @endif
-						><span ng-show="!movie.rate_code>0"><i class="far fa-star"></i></span><span ng-show="movie.rate_code>0"><i class="fas fa-check"></i></span> {{ __('general.seen') }}</button>
+						>
+						<button type="button" class="btn btn-sm btn-block border-0" ng-class="rate_class(movie.rate_code)" ng-click="votemodal($index, movie)"><span ng-show="!movie.rate_code>0"><i class="far fa-star"></i></span><span ng-show="movie.rate_code>0"><i class="fas fa-check"></i></span> {{ __('general.seen') }}</button>
 					</div>
 					<div class="col">
 						<button type="button" class="btn btn-outline-secondary btn-sm btn-block addban border-0" ng-class="{'text-danger':movie.ban_id!=null}" ng-click="ban($index)"><i class="fa fa-ban"></i></button>
