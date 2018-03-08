ae5b474a754fa6cdecbcb60d153062ec1b4e887a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-08 19:38:32 +0300

    popover

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -210,7 +210,7 @@
                         <div class="py-2 small">
                             <div class="text-dark pb-1"
                             @if(Auth::User()->tt_navbar < 100)>{{ __('navbar.like_us_on_facebook') }}</div>
-                            <div class="d-inline">
+                            <div class="d-inline"
                             data-toggle="popover" data-placement="left" title='Bize Yardım Et<a class="close tooltip-x" href="#close-tooltip">&times;</a>' id="like"
                             @endif
                             ><div class="fb-like mr-1 mb-2" data-href="https://www.facebook.com/topcorn.io/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div></div>
