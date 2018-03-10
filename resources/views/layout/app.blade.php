c418a60ad2b19c121c91834231c2b66b0087a0dc
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-11 00:20:33 +0300

    popover

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -50,9 +50,9 @@
 </head>
 
 
-@if(Auth::User->theme==0) $theme='';
-@elseif(Auth::User->theme==1) $theme='drk';
-@elseif(Auth::User->theme==2) $theme='ptc';
+@if(Auth::User()->theme==0) $theme='';
+@elseif(Auth::User()->theme==1) $theme='drk';
+@elseif(Auth::User()->theme==2) $theme='ptc';
 <body class="{{$theme}}">
 
     <div id="fb-root"></div>
