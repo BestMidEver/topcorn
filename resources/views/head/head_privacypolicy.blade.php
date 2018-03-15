922014954e1903effe40339b00358114a6629d8c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-15 12:38:55 +0300

    popover

--- resources/views/head/head_privacypolicy.blade.php
+++ resources/views/head/head_privacypolicy.blade.php
@@ -3,6 +3,7 @@
 pass={
 	"angular_module_array":[], 
 	"lang":"{{ App::getlocale() }}",
+	"is_auth":"{{  Auth::Check()  }}",
 	@if(Auth::check())
 	"tt_navbar":{{ Auth::User()->tt_navbar }},
 	"watched_movie_number":{{ $watched_movie_number }}
