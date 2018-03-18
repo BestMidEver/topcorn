cd0c8f5bf253aa217c7f323b51962050062e8899
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-18 21:33:56 +0300

    logo

--- resources/views/layout/appnew.blade.php
+++ resources/views/layout/appnew.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @yield('meta_description')
+    <meta name="description" content="@yield('meta_description')">
 
     <title>
         @section('title')
