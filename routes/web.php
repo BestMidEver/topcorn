83d86daea741f786818ad99f4bc9318f663a1d51
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-19 21:38:56 +0300

    logo

--- routes/web.php
+++ routes/web.php
@@ -185,7 +185,7 @@ Route::get('suckData', function(){
 Route::get('refreshSitemap', function(){
 	RefreshSitemapJob::dispatch()->onQueue("low");
 	$myfile = fopen("newfile.xml", "w") or die("Unable to open file!");
-    $xml = '<?xml version="1.0" encoding="utf-8"?>'.
+    $xml = '<?xml version="1.0" encoding="utf-8"?>\n'.
     		'<urlset'.
     		'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'.
     		'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'.
