0f59208ef5c7adc11ac51dfa2a9155585ce9eb53
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-19 23:02:05 +0300

    logo

--- routes/web.php
+++ routes/web.php
@@ -183,35 +183,7 @@ Route::get('suckData', function(){
 	return 'sucking data.';
 });
 Route::get('refreshSitemap', function(){
-        $myfile = fopen("newfile.xml", "w") or die("Unable to open file!");
-        $xml = '<?xml version="1.0" encoding="utf-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-<url> <loc>https://topcorn.io/recommendationsss</loc> <lastmod>'.substr(Carbon::now(), 0, 10).'</lastmod> <changefreq>weekly</changefreq> <priority>1</priority> </url>
-<url> <loc>https://topcorn.io/home</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>1</priority> </url>
-<url> <loc>https://topcorn.io/register</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.50</priority> </url>
-<url> <loc>https://topcorn.io/login</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.50</priority> </url>
-<url> <loc>https://topcorn.io/faq</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.80</priority> </url>
-<url> <loc>https://topcorn.io/privacy-policy</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.60</priority> </url>
-<url> <loc>https://topcorn.io/donation</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.60</priority> </url>
-<url> <loc>https://topcorn.io/password/reset</loc> <lastmod>2018-03-19</lastmod> <changefreq>monthly</changefreq> <priority>0.60</priority> </url>
-';
-
-        $movies = DB::table('movies')
-        ->select(
-            'movies.id',
-            'movies.original_title',
-            'movies.updated_at'
-        )
-        ->get();
-        foreach ($movies as $movie) {
-            $xml = $xml.'<url> <loc>https://topcorn.io/movie/'.$movie->id.'-'.str_replace(array(' ','/','?','#'), '-', $movie->original_title).'</loc> <lastmod>'.substr($movie->updated_at, 0, 10).'</lastmod> <changefreq>weekly</changefreq> <priority>0.80</priority> </url> 
-';
-        }
-
-        $xml = $xml . '</urlset> ';
-        fwrite($myfile, $xml);
-        fclose($myfile);
-        }
+	RefreshSitemapJob::dispatch()->onQueue("low");
 	return 'refreshing sitemap.';
 });
 //////////////////////////////////////////////////////////////////////////////////////////
