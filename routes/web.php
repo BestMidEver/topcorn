e4deee5e4ce2944e76c87a80ade91892e126b6fe
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-19 21:34:24 +0300

    logo

--- routes/web.php
+++ routes/web.php
@@ -185,11 +185,16 @@ Route::get('suckData', function(){
 Route::get('refreshSitemap', function(){
 	RefreshSitemapJob::dispatch()->onQueue("low");
 	$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-        $txt = "John Doe\n";
-        fwrite($myfile, $txt);
-        $txt = "Jane Doe\n";
-        fwrite($myfile, $txt);
-        fclose($myfile);
+    $txt = '<?xml version="1.0" encoding="utf-8"?>'.
+    		'<urlset'.
+    		'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'.
+    		'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"'.
+    		'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9'.
+    		'http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'.
+    		''.
+    		'</urlset>';
+    fwrite($myfile, $txt);
+    fclose($myfile);
 	return 'refreshing sitemap.';
 });
 //////////////////////////////////////////////////////////////////////////////////////////
