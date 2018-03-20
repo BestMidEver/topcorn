65d6a4c2256a7196beafc659b7383cee45c3c378
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-20 20:01:22 +0300

    logo

--- resources/views/head/head_home.blade.php
+++ resources/views/head/head_home.blade.php
@@ -18,3 +18,7 @@ pass={
 
 @section('meta_description')
 Too many movies, no time! topcorn.io understands your(or your movie group's) unique movie taste and recommends movies based on it. With topcorn.io it's now easy to make the right choice from any kind of movies. Your future best movies are waiting for you to be found. And it is completely free. What are you waiting for?@endsection
+
+@section('og_tags')
+<meta property="og:image" content="https://topcorn.io/images/topcorn_logo.png"/>
+@endsection
