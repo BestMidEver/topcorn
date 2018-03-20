ff18cdf885fc0eca7685556f333cbc78ec31cdad
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-20 21:19:47 +0300

    logo

--- resources/views/layout/ratemodal.blade.php
+++ resources/views/layout/ratemodal.blade.php
@@ -67,7 +67,7 @@
 						</div>
 						<div class="d-flex flex-row justify-content-between align-items-end">
 							<div class="faderdiv">
-								<button type="button" class="btn btn-verydark btn-lg float-left border-circle text-white" ng-class="modalmovie.later_id!=null ? 'text-warning' : 'text-white'" ng-show="modalmovie.is_quick_rate" ng-click="modalmovie.is_quick_rate ? quick_later() : later(modalmovie.index)">
+								<button type="button" class="btn btn-verydark btn-lg float-left border-circle text-white" ng-class="modalmovie.later_id!=null ? 'text-warning' : 'text-white'" ng-show="modalmovie.is_quick_rate" ng-click="modalmovie.is_quick_rate ? quick_later() : later(modalmovie.index)" data-toggle="tooltip" data-placement="bottom" title="{{ __('general.tooltip_watchlater') }}">
 									<span ng-show="modalmovie.later_id!=null"><i class="fas fa-clock"></i></span><span ng-show="modalmovie.later_id==null"><i class="far fa-clock"></i></span>
 								</button>
 							</div>
@@ -75,7 +75,7 @@
 								<button class="btn btn-verydark btn-lg border-circle text-white" ng-click="modalmovie.is_quick_rate ? quick_rate(0) : rate(modalmovie.index, null)">{{ __('general.havent_seen') }}</button>
 							</div>
 							<div class="faderdiv">
-								<button type="button" class="btn btn-verydark btn-lg float-left border-circle" ng-class="modalmovie.ban_id!=null ? 'text-danger' : 'text-white'" ng-show="modalmovie.is_quick_rate" ng-click="modalmovie.is_quick_rate ? quick_ban() : ban(modalmovie.index)">
+								<button type="button" class="btn btn-verydark btn-lg float-left border-circle" ng-class="modalmovie.ban_id!=null ? 'text-danger' : 'text-white'" ng-show="modalmovie.is_quick_rate" ng-click="modalmovie.is_quick_rate ? quick_ban() : ban(modalmovie.index)" data-toggle="tooltip" data-placement="bottom" title="{{ __('general.tooltip_ban') }}">
 									<i class="fa fa-ban"></i>
 								</button>
 							</div>
