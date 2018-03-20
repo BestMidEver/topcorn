4b22797c664f4c7959681d4989ed41cab2674ed3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-20 14:13:54 +0300

    logo

--- resources/lang/hu/general.php
+++ resources/lang/hu/general.php
@@ -138,7 +138,7 @@ return [
     'sort_by_match' => 'Egyezési pontszám alapján',                                                     //recommendations,      
     'sort_by_percent' => 'Egyezési százalék szerint',                                                     //recommendations,      
     'sort_by_tmdb' => 'TMDb puanı',                                                     //recommendations,      
-    'sort_by_pop' => 'Popülerliği',                                                     //recommendations,      
+    'sort_by_pop' => 'Népszerűség szerint',                                                     //recommendations,      
     'standard' => 'Alapértelmezett',                                        //accountinterface,
 	'theme' => 'Téma',
 	'theme_verydark' => 'Koromsötét',
