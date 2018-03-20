85357ae45f52f711e95d672f4f15df55b8a9a97f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-20 21:46:07 +0300

    logo

--- public/js/app.js
+++ public/js/app.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
 	});
 
 	$('[data-toggle="tooltip"]').tooltip({
-		delay: { "show": 500, "hide": 100 }
+		delay: { "show": 500, "hide": 100 },
 		trigger : 'hover'
 	});
 });
