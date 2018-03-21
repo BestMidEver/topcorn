2ec875612841d65b90d7aec28b1c766a18e4e28b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-21 15:19:21 +0300

    logo

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -309,8 +309,10 @@
 	<div class="p-5" ng-if="!movie.reviews.results.length>0">
 		<div class="text-muted text-center">{{ __('general.no_result_review') }}</div>
 	</div>
+</div>
 
-	<span class="mt-5 h5 mb-0">{{ __('general.fb_comments') }}</span>
+<div class="container-fluid mt-5">
+	<span class="h5 mb-0">{{ __('general.fb_comments') }}</span>
 	<div class="fb-comments" data-href="https://topcorn.io/movie/{{$id_dash_title}}" data-width="100%" data-numposts="6"></div>
 </div>
 <!--Review Section-->
