3687a1a8bc3dc390ffc366d53ee2e8fc77baa4b5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-21 15:28:38 +0300

    logo

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -313,7 +313,7 @@
 
 <div class="container-fluid pt-5">
 	<span class="h5 mb-0">{{ __('general.fb_comments') }}</span>
-	<div class="fb-comments" data-href="https://topcorn.io/movie/{{$id_dash_title}}" data-width="100%" data-numposts="6" {{Auth::User()->theme==1?'data-colorscheme="dark"':'';}}></div>
+	<div class="fb-comments" data-href="https://topcorn.io/movie/{{$id_dash_title}}" data-width="100%" data-numposts="6" data-colorscheme="{{Auth::User()->theme==1?'dark':''}}"></div>
 </div>
 <!--Review Section-->
 
