1d1a1cc214f6a13dda8fab148199b6d1c34b3a91
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-23 01:47:49 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -14,7 +14,7 @@
 	</a>
 </div>
 
-<div class="card-group mt-3">
+<div class="card-columns mt-3">
 	<div class="col-6">
 		<span class="text-dark h5">10. Elveda Las Vegas (1995)</span>
 		<!--Trailer-->
