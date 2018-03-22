ba6b02105c24b1a82c557cf30f37a93d3b8a214f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-22 17:42:02 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\PageControllers;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class listController extends Controller
 {
