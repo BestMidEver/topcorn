0f8641c868827c88a4b70da5818fb89c0521ae4b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-22 18:14:54 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,7 +3,7 @@
 @include('head.head_list')
 
 @section('body')
-<h1 class="h5 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
+<h1 class="h4 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
 
 <div class="text-muted"><small>5 ay önce eklendi, en son 23 gün önce güncellendi.</small></div>
 @endsection
