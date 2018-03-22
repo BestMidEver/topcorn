a0a42dfa50ee7eac08ea67d19d25ae24c0188058
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-22 20:08:39 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -6,5 +6,5 @@
 <h1 class="h4 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
 
 <div class="text-muted col"><small>5 ay önce eklendi, en son 23 gün önce güncellendi.</small></div>
-<div class="text-dark col"><span class="h6">Listenin Sahibi:</span> <a href="#" class="text-dark">YellowWölf</a></div>
+<div class="text-muted col"><span class="h6">Listenin Sahibi:</span> <a href="#" class="text-dark">YellowWölf</a></div>
 @endsection
