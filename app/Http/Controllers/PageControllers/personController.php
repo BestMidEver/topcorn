3f1792241cde128339383da460c2151b7c80e010
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-23 14:45:21 +0300

    logo

--- app/Http/Controllers/PageControllers/personController.php
+++ app/Http/Controllers/PageControllers/personController.php
@@ -10,20 +10,23 @@ use Illuminate\Support\Facades\Auth;
 
 class personController extends Controller
 {    
-	public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
 	public function person($id, $lang = '')
 	{
     	if($lang != '') App::setlocale($lang);
 
-    	$image_quality = Auth::User()->image_quality;
+        if(Auth::check()){
+        	$image_quality = Auth::User()->image_quality;
 
-        $target = Auth::User()->open_new_tab == 1 ? '_blank' : '_self';
+            $target = Auth::User()->open_new_tab == 1 ? '_blank' : '_self';
 
-        $watched_movie_number = Rated::where('user_id', Auth::id())->where('rate', '<>', 0)->count();
+            $watched_movie_number = Rated::where('user_id', Auth::id())->where('rate', '<>', 0)->count();
+        }else{
+            $image_quality = 2;
+
+            $target = '_blank';
+
+            $watched_movie_number = 0;
+        }
 
 		return view('person', compact('id' ,'image_quality', 'target', 'watched_movie_number'));
 	}
