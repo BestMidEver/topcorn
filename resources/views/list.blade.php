480510625f6ddb201363278b2f53755d8a42bda9
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-24 02:53:52 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -11,7 +11,7 @@
 			<div class="d-flex flex-column">
 				<img src="https://graph.facebook.com/v2.10/10211736611553891/picture?type=normal" on-error-src="{{config('constants.image.thumb_nail_error')}}" class="list-thumbnail" alt="Responsive image">
 			</div>
-			<div class="d-flex flex-column">
+			<div class="d-flex flex-column ml-2">
 				<span class="text-dark">Szofijjja</span>
 				<div class="text-muted"><small>5 ay önce ekledi, en son 23 gün önce güncelledi.</small></div>
 			</div>
