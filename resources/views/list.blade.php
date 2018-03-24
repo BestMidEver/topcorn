cc7b57ba7f51a394085e2d635338da763a7dfe87
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-24 05:26:19 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,30 +5,6 @@
 @section('body')
 <h1 class="h3 text-center text-md-left col px-0 mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
 
-<div class="row">
-	<div class="col-6 col-md-8 col-xl-9 mt-3">
-		<a href="#" class="text-no-decoration">
-			<div class="d-flex flex-row">
-				<div class="d-flex flex-column">
-					<img src="https://graph.facebook.com/v2.10/10211736611553891/picture?type=normal" on-error-src="{{config('constants.image.thumb_nail_error')}}" class="list-thumbnail" alt="Responsive image">
-				</div>
-				<div class="d-flex flex-column justify-content-center ml-2">
-					<h6 class="text-dark text-hover-underline mb-0">Szofijjja</h6>
-					<div class="text-muted"><small class="text-no-decoration">5 ay önce ekledi, en son 23 gün önce güncelledi.</small></div>
-				</div>
-			</div>
-		</a>
-	</div>
-	<div class="col-6 col-md-4 col-xl-3 mt-3 float-right">
-		<div class="float-right">
-			<div class="fb-like" data-href="https://topcorn.io/list/1" data-layout="box_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
-
-			<div class="fb-share-button ml-2" data-href="https://topcorn.io/list/1" data-layout="box_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftopcorn.io%2Flist%2F1&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Paylaş</a></div>
-		</div>
-	</div>
-</div>
-
-
 <div class="container-fluid px-md-2">
 	<div class="mt-5">
 		<span class="text-dark h5">10. Elveda Las Vegas (1995)</span>
@@ -151,4 +127,27 @@
 
 		<div class="lead lead-small mt-2">Kısaca Deja Vu olarak bilinen olayın rahatsız edici gizemini herkes bir şekilde deneyimlemiştir. Birisiyle yeni tanıştığınızda sanki onu yıllardır tanıyormuş gibi bir hisse kapılırsınız. Veya herhangi bir yere ilk defa gittiğiniz halde sanki orada daha önce bulunmuş gibi hissedersiniz. Kısacası Deja Vu adı verilen duyguyu bilmeyen yoktur denilebilir. Peki, ya bu tuhaf ve tüyler ürpertici duygu aslında geçmişten gönderilen bir uyarıysa… Veya bilinmeyen geleceğe dair ipuçlarını barındırıyorsa...</div>
 	</div>
+
+<div class="row">
+	<div class="col-6 col-md-8 col-xl-9 mt-3">
+		<a href="#" class="text-no-decoration">
+			<div class="d-flex flex-row">
+				<div class="d-flex flex-column">
+					<img src="https://graph.facebook.com/v2.10/10211736611553891/picture?type=normal" on-error-src="{{config('constants.image.thumb_nail_error')}}" class="list-thumbnail" alt="Responsive image">
+				</div>
+				<div class="d-flex flex-column justify-content-center ml-2">
+					<h6 class="text-dark text-hover-underline mb-0">Szofijjja</h6>
+					<div class="text-muted"><small class="text-no-decoration">5 ay önce ekledi, en son 23 gün önce güncelledi.</small></div>
+				</div>
+			</div>
+		</a>
+	</div>
+	<div class="col-6 col-md-4 col-xl-3 mt-3 float-right">
+		<div class="float-right">
+			<div class="fb-like" data-href="https://topcorn.io/list/1" data-layout="box_count" data-action="like" data-size="large" data-show-faces="false" data-share="false"></div>
+
+			<div class="fb-share-button ml-2" data-href="https://topcorn.io/list/1" data-layout="box_count" data-size="large" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftopcorn.io%2Flist%2F1&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Paylaş</a></div>
+		</div>
+	</div>
+</div>
 @endsection
