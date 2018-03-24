c76a3d7593edbfeb59dac800e8a14695d7002298
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-24 18:55:15 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -7,17 +7,17 @@
 
 <div class="container-fluid px-md-2">
 	<div class="mt-5 row">
-		<div class="col">
+		<div class="col-12 col-xl-6">
 			<span class="text-dark h5">10. Jackie Brown (1997)</span>
 			<div class="row pt-2">
-				<div class="col-6 col-md-3 col-xl-2">
+				<div class="col-4">
 					<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
 				</div>
 
-				<div class="col-3 lead lead-small mt-2">Amerika'nın en uyduruk havayollarından birinde hava hostesi olan Jackie Brown'ın emekliliği giderek yaklaşmaktadır. Hostesimiz ayın sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolünü vererek tam onikiden vuruyor.</div>
+				<div class="col-8 lead lead-small mt-2">Amerika'nın en uyduruk havayollarından birinde hava hostesi olan Jackie Brown'ın emekliliği giderek yaklaşmaktadır. Hostesimiz ayın sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolünü vererek tam onikiden vuruyor.</div>
 			</div>
 		</div>
-		<div class="col">
+		<div class="col-12 col-xl-6">
 			<span class="text-dark h5">9. Başka Bir Dünya (1997)</span>
 			<div class="row pt-2">
 				<div class="col-6 col-md-3 col-xl-2">
