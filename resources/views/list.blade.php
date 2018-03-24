f9d9fa721ba460edaca3fa19bb96bf66cb7607bf
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-24 04:31:53 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -5,22 +5,25 @@
 @section('body')
 <h1 class="h3 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
 
-<div class="col-5 mt-3">
-	<a href="#" class="text-no-decoration">
-		<div class="d-flex flex-row">
-			<div class="d-flex flex-column">
-				<img src="https://graph.facebook.com/v2.10/10211736611553891/picture?type=normal" on-error-src="{{config('constants.image.thumb_nail_error')}}" class="list-thumbnail" alt="Responsive image">
+<div class="row">
+	<div class="col-5 mt-3">
+		<a href="#" class="text-no-decoration">
+			<div class="d-flex flex-row">
+				<div class="d-flex flex-column">
+					<img src="https://graph.facebook.com/v2.10/10211736611553891/picture?type=normal" on-error-src="{{config('constants.image.thumb_nail_error')}}" class="list-thumbnail" alt="Responsive image">
+				</div>
+				<div class="d-flex flex-column ml-2">
+					<h6 class="text-dark text-hover-underline mb-0">Szofijjja</h6>
+					<div class="text-muted"><small class="text-no-decoration">5 ay önce ekledi, en son 23 gün önce güncelledi.</small></div>
+				</div>
 			</div>
-			<div class="d-flex flex-column ml-2">
-				<h6 class="text-dark text-hover-underline mb-0">Szofijjja</h6>
-				<div class="text-muted"><small class="text-no-decoration">5 ay önce ekledi, en son 23 gün önce güncelledi.</small></div>
-			</div>
-		</div>
-	</a>
+		</a>
+	</div>
+	<div class="col-2 mt-3">
+		<div class="fb-share-button" data-href="https://topcorn.io/list/1" data-layout="box_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftopcorn.io%2Flist%2F1&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Paylaş</a></div>
+	</div>
 </div>
-<div class="col-2 mt-3">
-	<div class="fb-share-button" data-href="https://topcorn.io/list/1" data-layout="box_count" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftopcorn.io%2Flist%2F1&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Paylaş</a></div>
-</div>
+
 
 <div class="container-fluid px-md-2">
 	<div class="mt-5">
