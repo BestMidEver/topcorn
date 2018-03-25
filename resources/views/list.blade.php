02af886b1691e0803d768b3d2dfc835130a464d2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-25 13:00:30 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -14,8 +14,11 @@
 	<div class="px-2 mt-4 col-12 col-xl-6">
 		<div class="card h-100">
 			<a href="#" data-toggle="tooltip" data-placement="top" title="Jackie Brown">
+				<div>
+					<span class="text-dark h6 p-1">10.</span>
+				</div>
 				<div class="text-center">
-					<span class="text-dark h6 p-1">10. Jackie Brown (1997)</span>
+					<span class="text-dark h6 p-1">Jackie Brown (1997)</span>
 				</div>
 			</a>
 			<div class="row no-gutters pt-2">
