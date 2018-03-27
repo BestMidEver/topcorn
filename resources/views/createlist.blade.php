7b24eeb33adbc2aae48b23f5b75feb7f23a27678
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-27 15:25:22 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -3,29 +3,208 @@
 @include('head.head_list')
 
 @section('body')
-<h1 class="h5 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
-
-<div class="py-3 col">
-	<h6 class="lead">Film önerisi mi lazım? Hangi filmi izlesem diye kara kara düşünüyor musun?
-20 farklı kategori, 140'ın üzerinde farklı liste, binlerce farklı film... Sinemasever okurlarımıza yaz havaları gelmişken ne hediye versek diye düşündük, izle izle bitiremeyeceğiniz devasa bir film arşivini layık gördük. İzlenebilecek filmler arıyorsanız doğru yerdesiniz. Bu listenin tamamını bitirmek aylar alıyor! </h6>
-</div>
-
-<div class="card-group no-gutters">
-	<div class="px-2 mt-4 col-12 col-xl-6">
-		<a href="#" class="text-no-decoration" data-toggle="tooltip" data-placement="top" title="Jackie Brown">
-			<div class="card h-100">
-					<span class="text-dark h6 p-1 text-hover-underline">10. Jackie Brown (1997)</span>
-				<div class="row no-gutters pt-2">
-					<div class="col-4">
-						<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
-					</div>
-
-					<div class="col-8 lead lead-small">
-						<div class="pl-3 pr-1 pb-1 text-dark">Amerika'nın en uyduruk havayollarından birinde hava hostesi olan Jackie Brown'ın emekliliği giderek yaklaşmaktadır. Hostesimiz ayın sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolün sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolünü vererek tam onikiden vuruyor.</div>
-					</div>
+<div class="container-fluid mt-3">
+	<div class="row">
+		<div class="col"></div>
+		<div class="col-12 col-xl-10">
+			<form id="the_form" class="form-horizontal" role="form" method="POST" action="/createlist">
+				{{ csrf_field() }}
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+						<div class="h6 text-muted">Liste Özellikleri</div>
+				    </div>
 				</div>
-			</div>
-		</a>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="lang">Kimler Görebilir?</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-language"></i></div>
+				                </div>
+				                <select class="form-control" id="lang" name="lang" ng-model="lang" autofocus>
+									<option value="tr">Herkes</option>
+									<option value="en">Sadece Ben</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="lang">Filmlere Açıklama Ekle</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-language"></i></div>
+				                </div>
+				                <select class="form-control" id="lang" name="lang" ng-model="lang">
+									<option value="tr">Etkin</option>
+									<option value="en">Devre Dışı</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="lang">Sıralama</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-language"></i></div>
+				                </div>
+				                <select class="form-control" id="lang" name="lang" ng-model="lang">
+									<option value="tr">Büyükten Küçüğe</option>
+									<option value="en">Küçükten Büyüğe</option>
+									<option value="fd">Devre Dışı</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+
+				<div class="row mt-3">
+				    <div class="col-md-3 field-label-responsive">
+						<div class="h6 text-muted">Etiket</div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="current_password">Başlık</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				            	<div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+				                </div>
+				                <input type="text" class="form-control" id="name" name="name" required>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3">
+				        <div class="form-control-feedback">
+				            @if ($errors->has('name'))
+				                <span class="text-danger align-middle float-right float-md-none">
+				                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
+				                </span>
+				            @endif
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="current_password">Giriş Yazısı</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				            	<div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+				                </div>
+				                <input type="text" class="form-control" id="name" name="name" required>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+
+				<div class="row mt-3">
+				    <div class="col-md-3 field-label-responsive">
+						<div class="h6 text-muted">{{ __('general.options') }}</div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="open_new_tab" ng-mouseenter="hovering_open=true" ng-mouseleave="hovering_open=false" data-toggle="tooltip" data-placement="top" title="{{ __('long_texts.hint_open_new_tab') }}">{{ __('general.open_new_tab') }} <span ng-show="!hovering_open"><i class="far fa-question-circle"></i></span><span ng-show="hovering_open"><i class="fas fa-question-circle"></i></span></label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fas fa-external-link-alt"></i></div>
+				                </div>
+				                <select class="form-control" id="open_new_tab" name="open_new_tab" ng-model="open_new_tab" ng-change="check_save_disabled()">
+									<option value=1>{{ __('general.active') }}</option>
+									<option value=0>{{ __('general.disabled') }}</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="pagination" ng-mouseenter="hovering_pagi=true" ng-mouseleave="hovering_pagi=false" data-toggle="tooltip" data-placement="top" title="{{ __('long_texts.hint_pagination') }}">{{ __('general.pagination') }} <span ng-show="!hovering_pagi"><i class="far fa-question-circle"></i></span><span ng-show="hovering_pagi"><i class="fas fa-question-circle"></i></span></label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fas fa-align-justify"></i></div>
+				                </div>
+				                <select class="form-control" id="pagination" name="pagination" ng-model="pagination" ng-change="check_save_disabled()">
+									<option value=48>48</option>
+									<option value=24>24</option>
+									<option value=12>12</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="show_crew" ng-mouseenter="hovering_crew=true" ng-mouseleave="hovering_crew=false" data-toggle="tooltip" data-placement="top" title="{{ __('long_texts.hint_show_crew') }}">{{ __('general.show_crew') }} <span ng-show="!hovering_crew"><i class="far fa-question-circle"></i></span><span ng-show="hovering_crew"><i class="fas fa-question-circle"></i></span></label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fas fa-users"></i></div>
+				                </div>
+				                <select class="form-control" id="show_crew" name="show_crew" ng-model="show_crew" ng-change="check_save_disabled()">
+									<option value=1>{{ __('general.active') }}</option>
+									<option value=0>{{ __('general.disabled') }}</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="advanced_filter" ng-mouseenter="hovering_adva=true" ng-mouseleave="hovering_adva=false" data-toggle="tooltip" data-placement="top" title="{{ __('long_texts.hint_advanced_filter') }}">{{ __('general.advanced_recommendations') }} <span ng-show="!hovering_adva"><i class="far fa-question-circle"></i></span><span ng-show="hovering_adva"><i class="fas fa-question-circle"></i></span></label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="form-group">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				                <div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fas fa-filter"></i></div>
+				                </div>
+				                <select class="form-control" id="advanced_filter" name="advanced_filter" ng-model="advanced_filter" ng-change="check_save_disabled()">
+									<option value=1>{{ __('general.active') }}</option>
+									<option value=0>{{ __('general.disabled') }}</option>
+								</select>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+
+				<div class="row">
+				    <div class="col-md-6">
+				    	<button type="submit" class="btn btn-link btn-block" ng-disabled="is_save_disabled">Paylaş</button>
+				    </div>
+				    <div class="col-md-6">
+				        <button type="submit" class="btn btn-primary btn-block" ng-disabled="is_save_disabled">Kaydet</button>
+				    </div>
+				</div>
+			</form>
+		</div>
+		<div class="col"></div>
 	</div>
 </div>
 @endsection
