840a3bfe5d7e58707f6f00af3d1221c7e76f0458
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-29 00:02:29 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -171,7 +171,7 @@
 							<div class="col-4 col-xl-3">
 								<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
 							</div>
-							<div class="input-group col-8 pl-3">
+							<div class="input-group col pl-3">
 							  <div class="input-group-prepend">
 							    <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
 							  </div>
