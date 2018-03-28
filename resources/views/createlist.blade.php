133512abc0e8f5c46c8fa433b177dd6fdd84524c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-28 22:04:18 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -151,81 +151,79 @@
 				<div class="row mt-5">
 					<div class="col"></div>
 					<div class="col-12 col-lg-10 col-xl-8">
-						<a href="#" class="text-no-decoration" data-toggle="tooltip" data-placement="top" title="Jackie Brown">
-							<div class="card h-100">
-								<div class="row">
-								    <div class="col-md-3 field-label-responsive">
-								        <label for="current_password">Sırası</label>
-								    </div>
-								    <div class="col-md-6">
-								        <div class="">
-								            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-								            	<div class="input-group-prepend">
-								                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-								                </div>
-								                <input type="number" class="form-control" id="name" name="name" required>
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-md-3">
-								        <div class="form-control-feedback">
-								            @if ($errors->has('name'))
-								                <span class="text-danger align-middle float-right float-md-none">
-								                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
-								                </span>
-								            @endif
-								        </div>
-								    </div>
+						<div class="card h-100">
+							<div class="row">
+							    <div class="col-md-3 field-label-responsive">
+							        <label for="current_password">Sırası</label>
+							    </div>
+							    <div class="col-md-6">
+							        <div class="">
+							            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+							            	<div class="input-group-prepend">
+							                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+							                </div>
+							                <input type="number" class="form-control" id="name" name="name" required>
+							            </div>
+							        </div>
+							    </div>
+							    <div class="col-md-3">
+							        <div class="form-control-feedback">
+							            @if ($errors->has('name'))
+							                <span class="text-danger align-middle float-right float-md-none">
+							                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
+							                </span>
+							            @endif
+							        </div>
+							    </div>
+							</div>
+							<div class="row mt-3">
+							    <div class="col-md-3 field-label-responsive">
+							        <label for="current_password">Adı</label>
+							    </div>
+							    <div class="col-md-6">
+							        <div class="">
+							            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+							            	<div class="input-group-prepend">
+							                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+							                </div>
+							                <input type="text" class="form-control" id="name" name="name" required>
+							            </div>
+							        </div>
+							    </div>
+							    <div class="col-md-3">
+							        <div class="form-control-feedback">
+							            @if ($errors->has('name'))
+							                <span class="text-danger align-middle float-right float-md-none">
+							                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
+							                </span>
+							            @endif
+							        </div>
+							    </div>
+							</div>
+							<div class="row no-gutters pt-2">
+								<div class="col-4 col-xl-3">
+									<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
 								</div>
-								<div class="row mt-3">
-								    <div class="col-md-3 field-label-responsive">
-								        <label for="current_password">Adı</label>
-								    </div>
-								    <div class="col-md-6">
-								        <div class="">
-								            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-								            	<div class="input-group-prepend">
-								                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-								                </div>
-								                <input type="text" class="form-control" id="name" name="name" required>
-								            </div>
-								        </div>
-								    </div>
-								    <div class="col-md-3">
-								        <div class="form-control-feedback">
-								            @if ($errors->has('name'))
-								                <span class="text-danger align-middle float-right float-md-none">
-								                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
-								                </span>
-								            @endif
-								        </div>
-								    </div>
-								</div>
-								<div class="row no-gutters pt-2">
-									<div class="col-4 col-xl-3">
-										<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
-									</div>
 
-									<div class="col-8 lead lead-small">
-										<div class="row mt-3">
-										    <div class="col-md-3 field-label-responsive">
-										        <label for="current_password">Açıklama</label>
-										    </div>
-										    <div class="col-md-6">
-										        <div class="">
-										            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-										            	<div class="input-group-prepend">
-										                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-										                </div>
-										                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
-										            </div>
-										        </div>
-										    </div>
-										</div>
+								<div class="col-8 lead lead-small">
+									<div class="row mt-3">
+									    <div class="col-md-3 field-label-responsive">
+									        <label for="current_password">Açıklama</label>
+									    </div>
+									    <div class="col-md-6">
+									        <div class="">
+									            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+									            	<div class="input-group-prepend">
+									                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+									                </div>
+									                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
+									            </div>
+									        </div>
+									    </div>
 									</div>
 								</div>
 							</div>
-						</a>
+						</div>
 					</div>
 					<div class="col"></div>
 				</div>
