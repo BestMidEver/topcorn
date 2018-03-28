09a80feb4b9bef175d09b36d36b62b33b293c30b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-28 23:05:11 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -19,7 +19,7 @@
 				</div>
 				<div class="row">
 				    <div class="col-12">
-				        <label for="current_password">Başlık</label>
+				        <label for="current_password">Başlık*</label>
 				    </div>
 				    <div class="col-12">
 				        <div class="">
@@ -148,6 +148,39 @@
 						<div class="h6 text-muted">Filmler</div>
 				    </div>
 				</div>
+
+
+				  <div class="form-group">
+				    <label class="control-label col-sm-2" for="email">Email:</label>
+				    <div class="col-sm-10">
+				      <input type="email" class="form-control" id="email" placeholder="Enter email">
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label class="control-label col-sm-2" for="pwd">Password:</label>
+				    <div class="col-sm-10"> 
+				      <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+				    </div>
+				  </div>
+				  <div class="form-group"> 
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <div class="checkbox">
+				        <label><input type="checkbox"> Remember me</label>
+				      </div>
+				    </div>
+				  </div>
+				  <div class="form-group"> 
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" class="btn btn-default">Submit</button>
+				    </div>
+				  </div>
+
+
+
+
+
+
+
 				<div class="row mt-5">
 					<div class="col"></div>
 					<div class="col-12 col-lg-10 col-xl-8">
