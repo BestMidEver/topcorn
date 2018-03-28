16f493c35b51a5b08fd0c9f496ffee5781ae9d9c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-28 20:43:10 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -11,8 +11,73 @@
 		<div class="col-12 col-xl-10">
 			<form id="the_form" class="form-horizontal" role="form" method="POST" action="/createlist">
 				{{ csrf_field() }}
-				<!-- Liste Özellikleri -->
+				<!-- Başlık & Giriş & Sonuç -->
 				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+						<div class="h6 text-muted">Başlık & Giriş & Sonuç</div>
+				    </div>
+				</div>
+				<div class="row">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="current_password">Başlık</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				            	<div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+				                </div>
+				                <input type="text" class="form-control" id="name" name="name" required>
+				            </div>
+				        </div>
+				    </div>
+				    <div class="col-md-3">
+				        <div class="form-control-feedback">
+				            @if ($errors->has('name'))
+				                <span class="text-danger align-middle float-right float-md-none">
+				                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
+				                </span>
+				            @endif
+				        </div>
+				    </div>
+				</div>
+				<div class="row mt-3">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="current_password">Giriş Yazısı</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				            	<div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+				                </div>
+				                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<div class="row mt-3">
+				    <div class="col-md-3 field-label-responsive">
+				        <label for="current_password">Sonuç Yazısı</label>
+				    </div>
+				    <div class="col-md-6">
+				        <div class="">
+				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
+				            	<div class="input-group-prepend">
+				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
+				                </div>
+				                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
+				            </div>
+				        </div>
+				    </div>
+				</div>
+				<!-- Başlık & Giriş & Sonuç -->
+
+
+
+				
+				<!-- Liste Özellikleri -->
+				<div class="row mt-3">
 				    <div class="col-md-3 field-label-responsive">
 						<div class="h6 text-muted">Liste Özellikleri</div>
 				    </div>
@@ -73,71 +138,6 @@
 				    </div>
 				</div>
 				<!-- Liste Özellikleri -->
-
-
-
-
-				<!-- Başlık & Giriş & Sonuç -->
-				<div class="row mt-3">
-				    <div class="col-md-3 field-label-responsive">
-						<div class="h6 text-muted">Başlık & Giriş & Sonuç</div>
-				    </div>
-				</div>
-				<div class="row">
-				    <div class="col-md-3 field-label-responsive">
-				        <label for="current_password">Başlık</label>
-				    </div>
-				    <div class="col-md-6">
-				        <div class="">
-				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-				            	<div class="input-group-prepend">
-				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-				                </div>
-				                <input type="text" class="form-control" id="name" name="name" required>
-				            </div>
-				        </div>
-				    </div>
-				    <div class="col-md-3">
-				        <div class="form-control-feedback">
-				            @if ($errors->has('name'))
-				                <span class="text-danger align-middle float-right float-md-none">
-				                    <i class="fas fa-exclamation-circle"></i> {{ $errors->first('name') }}
-				                </span>
-				            @endif
-				        </div>
-				    </div>
-				</div>
-				<div class="row mt-3">
-				    <div class="col-md-3 field-label-responsive">
-				        <label for="current_password">Giriş Yazısı</label>
-				    </div>
-				    <div class="col-md-6">
-				        <div class="">
-				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-				            	<div class="input-group-prepend">
-				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-				                </div>
-				                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
-				            </div>
-				        </div>
-				    </div>
-				</div>
-				<div class="row mt-3">
-				    <div class="col-md-3 field-label-responsive">
-				        <label for="current_password">Sonuç Yazısı</label>
-				    </div>
-				    <div class="col-md-6">
-				        <div class="">
-				            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-				            	<div class="input-group-prepend">
-				                	<div class="input-group-text" style="width: 2.6rem"><i class="fa fa-user"></i></div>
-				                </div>
-				                <textarea type="text" class="form-control" id="name" name="name" required></textarea>
-				            </div>
-				        </div>
-				    </div>
-				</div>
-				<!-- Başlık & Giriş & Sonuç -->
 
 
 
