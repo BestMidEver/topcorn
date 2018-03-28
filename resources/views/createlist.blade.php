7f7a7b39c249e70164a51116dacc38f94f4b10a7
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-28 22:05:47 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -156,7 +156,7 @@
 							    <div class="field-label-responsive">
 							        <label for="current_password">Sırası</label>
 							    </div>
-							    <div class="col-md-6">
+							    <div>
 							        <div class="">
 							            <div class="input-group mb-2 mr-sm-2 mb-sm-0">
 							            	<div class="input-group-prepend">
@@ -166,7 +166,7 @@
 							            </div>
 							        </div>
 							    </div>
-							    <div class="col-md-3">
+							    <div>
 							        <div class="form-control-feedback">
 							            @if ($errors->has('name'))
 							                <span class="text-danger align-middle float-right float-md-none">
