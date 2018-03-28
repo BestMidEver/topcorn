9a0196ea395330d6d260a09f44e9dd071b46106c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-28 23:37:16 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -153,13 +153,13 @@
 						<div class="row">
 							<div class="input-group mb-3 col-3">
 								<div class="input-group-prepend">
-									<span class="input-group-text" id="basic-addon1">Sırası</span>
+									<span class="input-group-text" id="basic-addon1"><i class="fas fa-hashtag"></i></span>
 								</div>
 								<input type="number" class="form-control" aria-describedby="basic-addon1">
 							</div>
 							<div class="input-group mb-3 col-9">
 								<div class="input-group-prepend">
-									<span class="input-group-text" id="basic-addon1">Filmin Adı</span>
+									<span class="input-group-text" id="basic-addon1"><i class="fas fa-film"></i></span>
 								</div>
 								<input type="text" class="form-control" aria-describedby="basic-addon1">
 							</div>
@@ -171,7 +171,7 @@
 
 							<div class="input-group col-8">
 							  <div class="input-group-prepend pl-3">
-							    <span class="input-group-text">Açıklama</span>
+							    <span class="input-group-text"><i class="fas fa-pencil-alt"></i></span>
 							  </div>
 							  <textarea class="form-control" aria-label="With textarea"></textarea>
 							</div>
