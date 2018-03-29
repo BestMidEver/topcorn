fdf70428195cd7368b14c12061923835b65c4d25
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-03-29 14:34:06 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -162,7 +162,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="d-flex flex-row justify-content-center">
+				<div class="d-flex flex-row justify-content-center mt-3">
 					<button href="#" class="btn btn-verydark border-circle text-white"><i class="fas fa-plus"></i> Film Ekle</button>
 				</div>
 				<!-- Filmler -->
