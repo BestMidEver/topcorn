2b462ba9f28692ffc5540f6f720f17d9589f572e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-02 22:32:41 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -148,7 +148,7 @@
 								</div>
 								<input type="text" class="form-control" aria-describedby="basic-addon1" placeholder="Filmin adını giriniz." ng-focus="search_1=true"  ng-blur="search_1=false" ng-model="input_1" ng-change="search_movie(1)" ng-model-options="{debounce: 750}">
 								<div ng-show="search_1" class="search-movie-results background-white py-3">
-									<div class="result" ng-repeat="movie in movies_1">@{{movie.title}} <small class="text-muted" ng-if="movie.release_date.length > 0"><em>(@{{movie.release_date.substring(0, 4)}})</em></small></div>
+									<div class="result" ng-repeat="movie in movies_1">@{{movie.title}} <small ng-if="movie.release_date.length > 0"><em>(@{{movie.release_date.substring(0, 4)}})</em></small></div>
 								</div>
 							</div>
 						</div>
