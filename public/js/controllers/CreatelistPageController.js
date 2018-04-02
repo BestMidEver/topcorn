9bb8725147988bc5c732afc5a4d93e12b74bf115
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-02 22:07:39 +0300

    logo

--- public/js/controllers/CreatelistPageController.js
+++ public/js/controllers/CreatelistPageController.js
@@ -1,5 +1,6 @@
 MyApp.controller('CreatelistPageController', function($scope, $http, rate)
 {
+		console.log(index, $scope['input_'+index])
 	$scope.search_movie = function(index){
 		console.log(index, $scope['input_'+index])
 	}
