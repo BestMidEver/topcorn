9b62a8bd57bd8fa455a8ea1b3174f4a8cf815a8a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-03 14:04:04 +0300

    logo

--- public/js/controllers/CreatelistPageController.js
+++ public/js/controllers/CreatelistPageController.js
@@ -28,7 +28,7 @@ MyApp.controller('CreatelistPageController', function($scope, $http, $timeout, r
 			rate.search_movies(pass.constants_api_key, pass.lang, temp, 1)
 			.then(function(response){
 				console.log(response.data);
-				$scope['movies_'+index]=response.data.results.slice(0, 10);
+				$scope.model['movies_'+index]=response.data.results.slice(0, 10);
 			});
 		}
 	}
