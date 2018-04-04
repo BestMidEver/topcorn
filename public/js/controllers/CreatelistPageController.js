acf5f6beeef10c367affc9cd147a99616b453664
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-04 12:53:39 +0300

    logo

--- public/js/controllers/CreatelistPageController.js
+++ public/js/controllers/CreatelistPageController.js
@@ -60,8 +60,9 @@ MyApp.controller('CreatelistPageController', function($scope, $http, $timeout, r
 	}
 
 	$scope.remove_from_list = function(index){
-		$scope.model={};
-		$scope.list=$scope.list.splice(index, 1);
+		$scope.$apply(function() {
+			$scope.list=$scope.list.splice(index, 1);
+		});
 		console.log(index, $scope.list)
 		$scope.refresh_list();
 	}
