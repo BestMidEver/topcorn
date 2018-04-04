89852b2f30f4e13c1e8dc2995c6278219720532a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 00:37:51 +0300

    logo

--- database/migrations/2018_03_29_131008_create_lists_table.php
+++ database/migrations/2018_03_29_131008_create_lists_table.php
@@ -15,11 +15,12 @@ class CreateListsTable extends Migration
     {
         Schema::create('lists', function (Blueprint $table) {
             $table->increments('id');
-            $table->tinyInteger('visibility')->default(1);
-            $table->tinyInteger('sort')->default(1);
+            $table->integer('user_id');
             $table->string('title');
             $table->text('entry_1');
             $table->text('entry_2');
+            $table->tinyInteger('visibility')->default(1);
+            $table->tinyInteger('sort')->default(1);
             $table->mediumInteger('fb_comment_count')->default(0);
             $table->mediumInteger('fb_share_count')->default(0);
             $table->mediumInteger('like_count')->default(0);
