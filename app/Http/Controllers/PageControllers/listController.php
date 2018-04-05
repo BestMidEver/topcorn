56eafadc2458d4f8f2983830c7220b9ba695d793
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 21:44:46 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -75,7 +75,7 @@ class listController extends Controller
             $explanation = count($value)>2 ? $value[2]: '';
             $json = $json.'{"position":'.$value[0].',"movie_id":'.$value[1].',"explanation":"'.$explanation.'"}';
         }
-        $json = json_decode($json.']');
+        //$json = json_decode($json.']');
         usort($json, function($a, $b) { //Sort the array using a user defined function
             return $a->position > $b->position ? -1 : 1; //Compare the scores
         });
