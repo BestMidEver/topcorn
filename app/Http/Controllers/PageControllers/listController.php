23af18741ace7ec06798b7bfda6c0408fd730b51
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 23:33:11 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -52,7 +52,7 @@ class listController extends Controller
         }else{
             $liste = [];
         }*/
-        return $liste->get()->toArray();
+        $liste = $liste->get()->toArray();
         return view('createlist', compact('id', 'image_quality', 'target', 'watched_movie_number', 'liste'));
     }
 
