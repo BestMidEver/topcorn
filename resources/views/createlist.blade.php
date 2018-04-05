3fd76280ab1cd449442ac29f0bd31eac17c1c957
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 23:59:19 +0300

    logo

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -93,7 +93,7 @@
 								<div class="input-group-prepend">
 									<div class="input-group-text" style="width: 2.6rem"><i class="fas fa-eye"></i></div>
 								</div>
-								<select class="form-control" id="visibility" name="visibility" ng-model="visibility">
+								<select class="form-control" id="visibility" name="visibility" ng-model="visibility"  ng-value="{{ $liste != 'empty' ? $liste->visibility : 1 }}">
 									<option value="1">Herkes</option>
 									<option value="0">Sadece Ben</option>
 								</select>
