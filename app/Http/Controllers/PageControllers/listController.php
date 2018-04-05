2408f9b26bbbf13243cd8949725bc7d7d480c7e3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 21:15:20 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -69,13 +69,14 @@ class listController extends Controller
 
         Listitem::where(['list_id' => $request->list_id])->delete();
 
-        $json = array(array());
+        $json = 'array()';
         foreach ($request->items as $index=>$value) {
-           $json[$index*1]->position = $value[0];
+           /*$json[$index*1]->position = $value[0];
            $json[$index*1]->movie_id = $value[1];
-           $json[$index*1]->explanation = $value[2];
+           $json[$index*1]->explanation = $value[2];*/
+           $json = $json + '<br>' + $index + '=>' $value;
         }
-        $json = $json_encode($json);
+        //$json = $json_encode($json);
 
         return $json;
 
