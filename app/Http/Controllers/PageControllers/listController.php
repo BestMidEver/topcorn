0d3ee49c1b3a0a2a9167a2b8f15574b3a54cf939
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-06 00:46:05 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -56,7 +56,7 @@ class listController extends Controller
                 'listitems.position',
                 'listitems.explanation'
             )
-            ->first();
+            ->get();
         }else{
             $liste = 'empty';
         }
