3afc9fd6af80b3a7d4aaf82362a363790a3231ea
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-06 00:39:56 +0300

    logo

--- resources/views/head/head_createlist.blade.php
+++ resources/views/head/head_createlist.blade.php
@@ -6,7 +6,7 @@ pass={
 	"lang":"{{ App::getlocale() }}",
 	"tt_navbar":{{ Auth::User()->tt_navbar }},
 	"watched_movie_number":{{ $watched_movie_number }},
-	"liste":$liste
+	"liste":{{ $liste }}
 };
 </script>
 @endsection
