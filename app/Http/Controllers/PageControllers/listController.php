16fa18cf70c54f0cf5e1f5b3a601ec619d93f986
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-05 21:31:32 +0300

    logo

--- app/Http/Controllers/PageControllers/listController.php
+++ app/Http/Controllers/PageControllers/listController.php
@@ -72,7 +72,7 @@ class listController extends Controller
         $json = '[';
         foreach ($request->items as $index=>$value) {
             if($index!=0) $json = $json.',';
-            $json = $json.'{"position":"'.$value[0].'","movie_id":"'.$value[1].'","explanation":"'.$value[2].'"}';
+            $json = $json.'{"position":"'.$value[0].'","movie_id":"'.$value[1].'","explanation":"'.count($value)>1?$value[2]:''.'"}';
         }
         $json = json_encode($json.']');
 
