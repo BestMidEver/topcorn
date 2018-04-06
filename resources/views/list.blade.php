0d62ac1b6649850d369913061d2be53a08af9077
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-06 20:38:01 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -3,29 +3,27 @@
 @include('head.head_list')
 
 @section('body')
-<h1 class="h5 text-center text-md-left col mt-3 mt-md-4">{{ $liste[0]->title }}</h1>
+<h1 class="h5 text-center text-md-left col mt-3 mt-md-4">2000'lerin en iyi bilim kurgu filmleri</h1>
 
 <div class="py-3 col">
-	<h6 class="lead">{{ $liste[0]->entry_1 }}</h6>
+	<h6 class="lead">Film önerisi mi lazım? Hangi filmi izlesem diye kara kara düşünüyor musun?
+20 farklı kategori, 140'ın üzerinde farklı liste, binlerce farklı film... Sinemasever okurlarımıza yaz havaları gelmişken ne hediye versek diye düşündük, izle izle bitiremeyeceğiniz devasa bir film arşivini layık gördük. İzlenebilecek filmler arıyorsanız doğru yerdesiniz. Bu listenin tamamını bitirmek aylar alıyor! </h6>
 </div>
 
 <div class="container-fluid mt-3">
-	<div class="row mt-5" ng-repeat="movie in movies">
+	<div class="row mt-5">
 		<div class="col"></div>
 		<div class="col-12 col-lg-10 col-xl-8">
 			<a href="#" class="text-no-decoration" data-toggle="tooltip" data-placement="top" title="Jackie Brown">
 				<div class="card h-100">
-						<span class="text-dark h6 p-1 text-hover-underline"><span>@{{ movie.position }}.<span> @{{ movie.movie_title }} (1997)</span>
+						<span class="text-dark h6 p-1 text-hover-underline">10. Jackie Brown (1997)</span>
 					<div class="row no-gutters pt-2">
 						<div class="col-4 col-xl-3">
 							<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
 						</div>
 
-						<div class="col-8" ng-if="!movie.explanation">
-							<div class="pl-3 pr-1 pb-1 text-dark">@{{ movie.overview }}</div>
-						</div>
-						<div class="col-8 lead lead-small" ng-if="movie.explanation">
-							<div class="pr-1 pb-1 text-dark quote-line">@{{ movie.explanation }}</div>
+						<div class="col-8">
+							<div class="pl-3 pr-1 pb-1 text-dark">Amerika'nın en uyduruk havayollarından birinde hava hostesi olan Jackie Brown'ın emekliliği giderek yaklaşmaktadır. Hostesimiz ayın sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolünü vererek tam onikiden vuruyor.</div>
 						</div>
 					</div>
 				</div>
@@ -53,10 +51,6 @@
 		</div>
 		<div class="col"></div>
 	</div>
-</div>
-
-<div class="py-3 col mt-5">
-	<h6 class="lead">{{ $liste[0]->entry_2 }}</h6>
 </div>
 
 <div class="mt-5 d-flex flex-row justify-content-between">
