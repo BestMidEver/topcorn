db4a70fb2b9ec255d03a6d79e50d4146ecc7e15b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-06 20:31:37 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -15,14 +15,14 @@
 		<div class="col-12 col-lg-10 col-xl-8">
 			<a href="#" class="text-no-decoration" data-toggle="tooltip" data-placement="top" title="Jackie Brown">
 				<div class="card h-100">
-						<span class="text-dark h6 p-1 text-hover-underline"><span>@{{ movie.position }}.<span> Jackie Brown (1997)</span>
+						<span class="text-dark h6 p-1 text-hover-underline"><span>@{{ movie.position }}.<span> @{{ movie.movie_title }} (1997)</span>
 					<div class="row no-gutters pt-2">
 						<div class="col-4 col-xl-3">
 							<img src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/4XVPYOdMAizdNMSwS0SK3fPJcvR.jpg" on-error-src="" class="card-img-top" alt="Responsive image">
 						</div>
 
 						<div class="col-8">
-							<div class="pl-3 pr-1 pb-1 text-dark">Amerika'nın en uyduruk havayollarından birinde hava hostesi olan Jackie Brown'ın emekliliği giderek yaklaşmaktadır. Hostesimiz ayın sonunu getirebilmek için silah kaçakçısı Ordell için çalışmaktadır. Polis durumdan haberdardır. Ordell de polisin haberdar olduğundan ve dolayısıyla Jackie'nin hayatının bıçak sırtında olduğundan.Olaylar ortaya çıktığuında Jackie ve Ordell cephelerine yeni yardımcılar katılır ve herkes yarım milyon doların peşine düşer.Yönetmen Tarantino, hayranı olduğu, 70'li yılların kült zenci dizisi "Foxy Brown"ın kadın oyuncusu Palm Grier'e Jackie Brown rolünü vererek tam onikiden vuruyor.</div>
+							<div class="pl-3 pr-1 pb-1 text-dark">@{{ ((movie.explanation != 'No overview found.') || movie.explanation) ? movie.explanation : movie.overview }}</div>
 						</div>
 					</div>
 				</div>
