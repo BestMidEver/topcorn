896c1f068d8d8c2b7af48c97c2b2ec6dbe503d19
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-11 14:32:43 +0300

    logo

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,48 @@
 <?php
 
 function timeAgo($time) {
-    return "muhahahah uhauhuhuhu uuuu auuauuu";
+    switch ($time[1]) {
+    	case 'second':
+    		return $time[0].' saniye ';
+    		break;
+    	case 'seconds':
+    		return $time[0].' saniye ';
+    		break;
+    	case 'minute':
+    		return $time[0].' dakika ';
+    		break;
+    	case 'minutes':
+    		return $time[0].' dakika ';
+    		break;
+    	case 'hour':
+    		return $time[0].' saat ';
+    		break;
+    	case 'hours':
+    		return $time[0].' saat ';
+    		break;
+    	case 'day':
+    		return $time[0].' gün ';
+    		break;
+    	case 'days':
+    		return $time[0].' gün ';
+    		break;
+    	case 'week':
+    		return $time[0].' hafta ';
+    		break;
+    	case 'weeks':
+    		return $time[0].' hafta ';
+    		break;
+    	case 'month':
+    		return $time[0].' ay ';
+    		break;
+    	case 'months':
+    		return $time[0].' ay ';
+    		break;
+    	case 'year':
+    		return $time[0].' yıl ';
+    		break;
+    	case 'years':
+    		return $time[0].' yıl ';
+    		break;
+    }
 }
