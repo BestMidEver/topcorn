c5b7490df74199c2d730ccbf00150dc59028c095
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-12 14:39:06 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -22,22 +22,22 @@
 		'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
 		<hr class="my-4">
 		<div class="card-group no-gutters">
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2 p-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
-			<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+			<div class="col-6 col-md-4 col-lg-3 col-xl-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
 		</div>
