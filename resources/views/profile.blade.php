9035a4cf2eb19798a7a59c8f04bdfc37e931b693
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-12 13:53:07 +0300

    logo

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -55,7 +55,7 @@
 <div class="container-fluid mt-3 pb-3">
 	<ul class="nav justify-content-md-center tab1">
 		<li class="nav-item">
-			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab!='get_laters' && active_tab!='get_bans'}" ng-click="mod_title='{{ __('general.definitely_recommend') }}';active_tab='get_rateds/5';get_first_page_data()">{{ __('general.seen_movies') }}</button>
+			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab!='get_laters' && active_tab!='get_bans' && active_tab!='get_lists'}" ng-click="mod_title='{{ __('general.definitely_recommend') }}';active_tab='get_rateds/5';get_first_page_data()">{{ __('general.seen_movies') }}</button>
 		</li>
 		<li class="nav-item">
 			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab=='get_laters'}" ng-click="active_tab='get_laters';get_first_page_data();">{{ __('general.watch_later') }}</button>
@@ -66,7 +66,7 @@
 		@if(Auth::check())
 			@if(Auth::id() == 7)
 		<li class="nav-item">
-			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab=='get_lists'}" ng-click="active_tab='get_lists';get_page_data();">{{ __('general.banneds') }}</button>
+			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab=='get_lists'}" ng-click="active_tab='get_lists';get_page_data();">{{ __('general.lists') }}</button>
 		</li>	
 			@endif
 		@endif
