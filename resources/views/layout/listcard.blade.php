68f181edc23ab0446dba6a22b3a0133dc397bffd
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-12 14:46:21 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -17,11 +17,11 @@
 
 	<div class="jumbotron">
 		<a href="#">
-			<h1 class="display-4">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</h1>
-			<p class="lead">
-				İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film
-			'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
+			<h1 class="display-4 text-dark">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</h1>
 		</a>
+		<p class="lead">
+			İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film
+		'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
 		<hr class="my-4">
 		<div class="card-group no-gutters">
 			<div class="col-6 col-md-4 col-lg-3 col-xl-2 p-2">
