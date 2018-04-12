9b62dea88005cb6bdbf65f73e198db2d1065e536
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-12 14:20:24 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -1,1 +1,50 @@
-<div ng-if="active_tab == 'get_lists'"></div>
+<div ng-if="active_tab == 'get_lists'">
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="jumbotron">
+  <h1 class="display-4">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</h1>
+  <p class="lead">
+İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film
+'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
+  <hr class="my-4">
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
