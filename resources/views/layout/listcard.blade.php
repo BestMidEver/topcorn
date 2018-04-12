70fb086539af0dda78b38a97cb4cac47f9516ef5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-12 14:21:31 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -16,7 +16,7 @@
 
 
 <div class="jumbotron">
-  <h1 class="display-4">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</h1>
+  <h1 class="display-3">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</h1>
   <p class="lead">
 İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film
 'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
