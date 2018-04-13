c431d879088e6071e7e1e21af431ce4074d18146
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-13 12:54:26 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -25,22 +25,22 @@
 		<div class="row">
 			<div class="col"></div>
 				<div class="col-6 card-group no-gutters">
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/wShp6RwmmC5V6uhz9X0zMfs4740.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/aTG2EvePwRx34MwCg70Q20INzQM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/ybQLA0vUHFCPsnuJ6rCRev9YFcV.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/g8qWglC2XXCIN8P51eCljFvCNNJ.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
-					<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
+					<div class="col-2 col-md-2 col-lg-2 col-xl-2">
 						<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w100_and_h100_bestv2/gornum2ob13CQrYt1SjfnnLhkUQ.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 					</div>
 				</div>
