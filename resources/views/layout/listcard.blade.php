1e8952241f74ccdc701d35636da8ec294a730a41
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-13 12:02:17 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -15,14 +15,13 @@
 
 
 
-	<div class="jumbotron py-2 px-0">
+	<div class="jumbotron py-4 px-0">
 		<a href="#">
 			<div class="h6 text-dark px-2">İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film</div>
 		</a>
 		<p class="lead lead-small px-2">
 			İzlerken Aklınızı Başınızdan Alacak 32 Muhteşem Film
 		'Mind Blowing Movies' izlerken kan akışınızı hızlandıran, aklınızı başından alan filmler olarak tanımlanabilir.. Bir filmi izlerken belirli aralıklarla vay arkadaş!', 'harika kurgu!' ya da 'abi süper film olmuş' gibi cümleler kuruyorsanız işte o film 'Mind Blowing Movies' kategorisine alınabilir.</p>
-		<hr class="my-4">
 		<div class="card-group no-gutters">
 			<div class="col-2 col-md-2 col-lg-2 col-xl-2 p-1 p-md-2">
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/fQMSaP88cf1nz4qwuNEEFtazuDM.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
@@ -43,7 +42,7 @@
 				<img class="card-img-top" ng-src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/gornum2ob13CQrYt1SjfnnLhkUQ.jpg" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 			</div>
 		</div>
-		<p class="text-muted text-right px-2"><small>2 gün önce ekledi, en son 5 saat önce güncelledi.</small></p>
+		<div class="text-muted text-right px-2"><small>2 gün önce ekledi, en son 5 saat önce güncelledi.</small></div>
 	</div>
 
 
