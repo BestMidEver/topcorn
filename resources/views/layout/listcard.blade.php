4f1686a5bac1fe139b452147815ff895a279d93e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-14 20:01:04 +0300

    logo

--- resources/views/layout/listcard.blade.php
+++ resources/views/layout/listcard.blade.php
@@ -57,7 +57,7 @@
 		</div>
 	</div>
 
-	<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4">
+	<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4" ng-if="active_tab=='get_lists'">
 		<div class="h-100 d-flex flex-column justify-content-center mx-2">
 			<div class="d-flex flex-row justify-content-center">
 				<a href="/createlist/new" class="btn btn-verydark border-circle text-white btn-lg" data-toggle="tooltip" data-placement="top" title="Liste Oluştur"><i class="fas fa-plus"></i></a>
