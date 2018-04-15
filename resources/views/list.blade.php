0338c8a19ec0f5bb62d21a683e8799c6b7cd91a3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-15 23:09:48 +0300

    logo

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -62,7 +62,7 @@
 									<div class="pr-1 pb-1 text-dark quote-line">@{{ movie.overview }}</div>
 								</div>
 								<div class="col-8 lead lead-small" ng-if="movie.explanation">
-									<div class="pl-3 pr-1 pb-1 text-dark">@{{ movie.explanation }}</div>
+									<div class="pl-3 pr-1 pb-1 text-dark line-break">@{{ movie.explanation }}</div>
 								</div>
 							</div>
 						</div>
