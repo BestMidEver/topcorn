51802ac3d12d314b72b6c5ff7c6f6193f14d9078
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-16 21:37:04 +0300

    logo

--- app/Http/Middleware/id_dash_listname.php
+++ app/Http/Middleware/id_dash_listname.php
@@ -16,7 +16,17 @@ class id_dash_listname
      */
     public function handle($request, Closure $next)
     {
+        $liste = Liste::where(['id' => $request->id]);
+
+        if($liste->count() > 0) $liste = $liste->first();
+        //else return $next($request);
+
+        $correct_url = $liste->id.'-'.str_replace(array(' ','/','?','#','&','<','>',"'",'"','*','%'), '-', $liste->title);
         
+        if($request->user == $correct_url){
             return $next($request);
+        }else{
+            return redirect('/list/'.$correct_url);
+        }
     }
 }
