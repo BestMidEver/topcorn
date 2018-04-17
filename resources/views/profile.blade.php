7b0728ed85afd7bd51a4f430b3b2a2d8087a6ff5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-17 17:16:40 +0300

    logo

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -59,8 +59,8 @@
 	</div>
 	@endif
 	<div class="right-bottom pr-2 fa30">
-		@if(1)
-		<a class="btn btn-link mb-2 text-light btn-sm" href="#"><span class="h6">My Blog</span></a>
+		@if(0)
+		<a class="btn btn-link mb-2 text-light btn-sm" href="#"><span class="h6">Personal Website</span></a>
 		<a class="btn btn-link mb-2 text-light btn-sm" href="#"><i class="fab fa-instagram"></i></a>
 		<a class="btn btn-link mb-2 text-light btn-sm" href="#"><i class="fab fa-facebook-square"></i></a>
 		<a class="btn btn-link mb-2 text-light btn-sm" href="#"><i class="fab fa-linkedin"></i></a>
