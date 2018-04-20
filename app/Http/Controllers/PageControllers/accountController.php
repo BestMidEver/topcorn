63b34009b2fb431b87b75dac0c019b23ba2cbc1d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-20 21:20:47 +0300

    logo

--- app/Http/Controllers/PageControllers/accountController.php
+++ app/Http/Controllers/PageControllers/accountController.php
@@ -44,7 +44,7 @@ class accountController extends Controller
         ->whereNotNull('listlikes.created_at')
         ->count();
 
-        $exploded_url = explode("://", Auth::User()->another_link_url)
+        $exploded_url = explode("://", Auth::User()->another_link_url);
         $url_http = $exploded_url[0].'://';
         $another_link_url = count($exploded_url) > 1 ? $exploded_url[1] : '';
 
