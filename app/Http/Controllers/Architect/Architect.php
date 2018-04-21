35f0a68b3c3c289748989e23164f920c794326aa
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-21 20:07:46 +0300

    logo

--- app/Http/Controllers/Architect/Architect.php
+++ app/Http/Controllers/Architect/Architect.php
@@ -11,7 +11,7 @@ class Architect extends Controller
 {
 	public function refreshSitemap()
     {
-        $myfile = fopen("/sitemap.xml", "w") or die("Unable to open file!");
+        $myfile = fopen("public/sitemap.xml", "w") or die("Unable to open file!");
         $xml = '<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 <url> <loc>https://topcorn.io/recommendations</loc> <lastmod>'.substr(Carbon::now(), 0, 10).'</lastmod> <changefreq>weekly</changefreq> <priority>1</priority> </url>
