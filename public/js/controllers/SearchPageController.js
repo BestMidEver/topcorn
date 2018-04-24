e4a483667567f39bf5bfab7adafa22bfe00f2155
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-24 22:08:22 +0300

    logo

--- public/js/controllers/SearchPageController.js
+++ public/js/controllers/SearchPageController.js
@@ -43,7 +43,7 @@ MyApp.controller('SearchPageController', function($scope, $http, $anchorScroll, 
 
 	$scope.generalinput='';
 	$scope.get_page_data = function()
-	{
+	{console.log(temp, ':D')
 		var temp=$scope.generalinput.replace(/ /g , "%20");
 		if(temp.length == 0){
 			$scope.reset_tab();
