9563d7dcd1cfbac6ec4ad0e5232e2ea3902026f0
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-28 11:51:46 +0300

    logo

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -69,7 +69,7 @@
 	<div class="py-3" ng-if="active_tab=='mood_pick'">
 
 
-		<div class="container-fluid">
+		<div class="container-fluid mb-3">
 			<div class="dropdown d-inline" ng-init="mode_mod_title='{{ __('general.definitely_recommend_movies') }}';">
 				<button class="btn btn-secondary btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					@{{mode_mod_title}}
