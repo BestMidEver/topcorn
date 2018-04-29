c9a50b2f64022e04fb78e4fe90d02b617ba12cb5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-30 00:29:22 +0300

    logo

--- resources/views/layout/recommendations_languages.blade.php
+++ resources/views/layout/recommendations_languages.blade.php
@@ -7,7 +7,7 @@
 				@{{language.o}}
 			</label>-->
 			<div class="custom-control custom-checkbox">
-			  <input type="checkbox" class="custom-control-input" ng-model="f_lang_model[language.i]" ng-change="get_first_page_data()">
+			  <input type="checkbox" class="custom-control-input">
 			  <label class="custom-control-label">@{{language.o}}</label>
 			</div>
 		</div>
