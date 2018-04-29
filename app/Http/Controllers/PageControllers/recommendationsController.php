3ece58a6d662e706ffc4457d71b15735e0fd375a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-30 01:14:59 +0300

    logo

--- app/Http/Controllers/PageControllers/recommendationsController.php
+++ app/Http/Controllers/PageControllers/recommendationsController.php
@@ -46,7 +46,7 @@ class recommendationsController extends Controller
 
 
     public function get_top_rateds(Request $request)
-    {return $request->f_add_watched;
+    {return 2;
         $start = microtime(true);
 
         if(auth::check()){
