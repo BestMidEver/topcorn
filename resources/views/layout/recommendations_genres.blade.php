c3119be03ec23078ae6c486c5d1aab79cf38ca9b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-04-30 00:33:58 +0300

    logo

--- resources/views/layout/recommendations_genres.blade.php
+++ resources/views/layout/recommendations_genres.blade.php
@@ -2,10 +2,14 @@
 	<p class="h6 text-muted">{{ __('general.genre') }}</p>
 	<div class="d-flex flex-wrap">
 		<div class="col-6 col-sm-4 col-md-3 col-lg-2" ng-repeat="genre in genres">
-			<label class="form-check-label">
+			<!--<label class="form-check-label">
 				<input type="checkbox" class="form-check-input" ng-model="f_genre_model['id_'+genre.i]" ng-change="get_first_page_data()">
 				@{{genre.o}}
-			</label>
+			</label>-->
+			<div class="custom-control custom-checkbox">
+			  <input type="checkbox" class="custom-control-input" ng-attr-id="customCheckGenre@{{$index}}" ng-model="f_genre_model['id_'+genre.i]" ng-change="get_first_page_data()">
+			  <label class="custom-control-label" for="customCheckGenre@{{$index}}">@{{genre.o}}</label>
+			</div>
 		</div>
 	</div>
 </div>
