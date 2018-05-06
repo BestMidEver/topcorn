2dac0e84fb5cbbacef42121a9feae27e7f456563
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-05-06 17:17:44 +0300

    logo

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -325,7 +325,7 @@
 <!--Lists Section-->
 <div class="container-fluid px-0 pt-5">
 	<div class="h5 px-3 px-md-0">
-		<span class="mb-0 pr-2 pb-3">{{ __('general.movie_lists_title') }}</span>	
+		<span class="mb-0 pr-2">{{ __('general.movie_lists_title') }}</span>	
 		<a href="/createlist/new" class="btn btn-outline-success btn-sm" target="{{$target}}">
 			<i class="fas fa-plus"></i> {{ __('general.create_list') }}
 		</a>
