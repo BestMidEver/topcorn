dfb9d487b8ffa2315e4e93b653b5bb3a2b1ac479
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-06-21 21:47:26 +0300

    logo

--- resources/views/whatmovieshouldiwatch.blade.php
+++ resources/views/whatmovieshouldiwatch.blade.php
@@ -4,4 +4,34 @@
 
 @section('body')
 <h1 class="text-center text-md-left col mt-3 mt-md-4">What Movie Should I Watch</h1>
+
+<div class="p-3">
+	<p class="lead">
+	Humankind started to make films at the end of the 19th centuary. Since then the film industry grew rapidly. Nowadays we have access to hundreds of thousands of movies. Because we are mortals and we don’t have infinite time, we can’t possibly watch all of them. That makes you ask the question “What movie should I watch?”
+
+	If you came to here to get some movie recommendations from somebody who may or may not share the same movie taste with you, you may scroll down to the bottom of the page for that. Even so I didn’t create this page to announce my “Top 100 Greatest Movies of All Time”. 
+
+	This page is a hook so you can reach this website. If you want to get the answer of the question immediately, then you can simply skip reading this long post and start using this personalized movie recommendation engine. 
+
+	You can create an account with your facebook account or with your email address easily and quickly. You don’t even have to confirm your e-mail address. If it is still too much work to do you can just go to the recommendations page and define your mood with several movies and get your movie suggestions according to that mood. However it can be told that, creating a strong profile and filtering the results afterwards is more effective than defining the mood with several movies.
+
+	There are several ways of strengthening your profile. Firstly, you can use quick vote feature. Through the quick vote button you can sequential vote the most frequently voted movies by other users. Secondly, you can search&vote. In addition to them you can go to the page of your favourite actors or directors to vote their movies. 
+
+	I don’t really have to tell you to how to use this website because it is quite easy to use; however like I said before, this is a hook page for the search results so you can find the real product. And you found the tool to answer your question. You will not have to overthink or oversearch the movie that you should watch tonight anymore. There are 19070 of movies in the database currently and it is increasing everyday. So I invite you to use topcorn.io. It is completely free and it will stay like this. On the other hand, I quit my job and I need to take some advertisements when the right time comes. 
+
+	Apparently you are still reading despite to my warnings, so lets continue.
+
+	After you create and strengthen your profile, now you can get your customized movie recommendations. If you entered enough movies to the system, you will have lots of movies in your list. You can use the filter to narrow the list down. If that filter is not enough complex for you and if you want to see more choices there, you can go to interface settings to enable advanced recommendations. You can also customize your profile&background pictures and many other things from there too. I am spending a lot of time on developing topcorn.io. Every time when I have new users who uses topcorn, that makes me really happy. I am glad if I can help you to find some good movies to watch.
+
+	I will tell about an another use of topcorn.io. Topcorn.io is created to give customized movie recommendation lists however you can create custom lists for any reason and you can share it with other users too. In this way; at the bottom section of the related movie detail pages, your lists will be shown. Thanks to that people can reach to your lists and your profile. In addition to this, you may share links to promote your social media accounts or your youtube channel or even your blog to get some extra traffic. 
+
+	When I was creating this website; no one ever told me the difficulties of promoting it, will be harder than creating it. Now I see that I didn’t make enough plans or I don’t know enough to reach people who needs free customized movie recommendations from every language not only in english.
+
+	I don’t have any problem who love american cinema and actually I watch hollywood movies more than other cinemas however I really enjoy other cinemas too. I love to hear and listen the stories from different cultures in different languages. So much essence is hidden inside those movies. Some spanish crime mistery movies, almost all of the good scandinavian movies without endings, quality indian movies, old time japanese movies, weird romantic korean movies and more that I can’t remember right now. 
+
+	When I tried to find a customized movie recommendation engine which can also give this kind of movies, I couldn’t find it. That really makes topcorn.io different from other personalized movie suggestion systems and that’s why I am developing it.
+
+	It is not rocket science or something magical by the way. When I was creating this website, I planned users to spend as least time as possible on the site. Users come to the website, find the best movie for their taste quickly and then they can spend their time on watching them. All of my plans were according to it. It is a very simple page and offers you to find a good movie to watch, nothing else.
+	</p>
+</div>
 @endsection
