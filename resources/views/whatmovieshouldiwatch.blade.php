263dcaafcdc8372d8cd561647a2ec064da4dae25
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-06-22 18:55:36 +0300

    logo

--- resources/views/whatmovieshouldiwatch.blade.php
+++ resources/views/whatmovieshouldiwatch.blade.php
@@ -9,6 +9,9 @@
 	<div class="p-3">
 		<p>Humankind started to make films at the end of the 19th centuary. Since then the film industry grew rapidly. Nowadays we have access to hundreds of thousands of movies. Because we are mortals and we don’t have infinite time, we can’t possibly watch all of them. That makes you ask the question <strong>What movie should I watch?</strong>
 		</p>
+		<div class="jumbotron">
+			<p class="lead">Topcorn.io is the answer of this question!</p>
+		</div>
 	</div>
 </div>
 @endsection
