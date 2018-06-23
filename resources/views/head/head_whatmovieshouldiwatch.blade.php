be3b81778bdf0fa3d631d061c7e9e6e19352d52d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-06-23 22:39:33 +0300

    logo

--- resources/views/head/head_whatmovieshouldiwatch.blade.php
+++ resources/views/head/head_whatmovieshouldiwatch.blade.php
@@ -1,3 +1,6 @@
 @section('title')
-What Movie Should I Watch (2018)
+What Movie Should I Watch? (2018)
 @endsection
+
+@section('meta_description')
+Get movie recommendations based on your taste. Filter the movies with their original languages, years, genres and vote counts. Use watch together feature to find the best movie for your movie party.@endsection
