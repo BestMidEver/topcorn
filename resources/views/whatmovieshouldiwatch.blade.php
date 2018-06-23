8819b82980044edbb081c5bef91c7c940bd1b254
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-06-23 22:14:03 +0300

    logo

--- resources/views/whatmovieshouldiwatch.blade.php
+++ resources/views/whatmovieshouldiwatch.blade.php
@@ -137,17 +137,19 @@
 		<p>If you are asking yourself "What movie should I watch?", then you came to the right place. Because I am asking the same question to myself every night. If you have any ideas or critics to improve Topcorn.io, please contact us. I am waiting your merciless critics.</p>
 	</div>
 
-	<div class="col-12 col-lg-4 pl-0 pl-lg-4 pt-0 pt-lg-5 quote-line">
-		<div class="text-muted"><strong>POPULAR LISTS</strong></div>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Picture</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Director</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">My favourite spanish movies</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Best Date Movies</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Foreign Language Film</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Costume Design</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Sound Editing</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Visual Effects</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Best movies about artists & painters</a></p>
+	<div class="col-12 col-lg-4 pl-0 pl-lg-4 pt-0 pt-lg-5">
+		<div class="quote-line">
+			<div class="text-muted"><strong>POPULAR LISTS</strong></div>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Picture</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Director</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">My favourite spanish movies</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Best Date Movies</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Foreign Language Film</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Costume Design</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Sound Editing</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Oscar Winner Movies of All Times - Best Visual Effects</a></p>
+			<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture" class="text-muted">Best movies about artists & painters</a></p>
+		</div>
 	</div>
 </div>
 @endsection
