93a95497bd4e55e7ff8b6b05fe9d9e65a8b86411
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-06-23 22:08:45 +0300

    logo

--- resources/views/whatmovieshouldiwatch.blade.php
+++ resources/views/whatmovieshouldiwatch.blade.php
@@ -137,10 +137,17 @@
 		<p>If you are asking yourself "What movie should I watch?", then you came to the right place. Because I am asking the same question to myself every night. If you have any ideas or critics to improve Topcorn.io, please contact us. I am waiting your merciless critics.</p>
 	</div>
 
-	<div class="col-12 col-lg-4 pl-0 pl-lg-3 pt-0 pt-lg-4">
-		<div class="text-muted"><strong>POPULAR LISTS</strong></div>
+	<div class="col-12 col-lg-4 pl-0 pl-lg-3 pt-0 pt-lg-4 text-muted">
+		<div><strong>POPULAR LISTS</strong></div>
 		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Picture</a></p>
-		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Picture</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Director</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">My favourite spanish movies</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Best Date Movies</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Foreign Language Film</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Costume Design</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Sound Editing</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Oscar Winner Movies of All Times - Best Visual Effects</a></p>
+		<p><a href="/list/11-Oscar-Winner-Movies-of-All-Times---Best-Picture">Best movies about artists & painters</a></p>
 	</div>
 </div>
 @endsection
