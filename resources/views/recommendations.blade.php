f616c3d17e38d55fe904e6484221a3bce1cb8136
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-14 19:28:30 +0300

    logo

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -5,12 +5,12 @@
 @section('body')
 <!-- H1 +AddPerson +AddMode row  -->
 <div class="col mb-2 mt-3 mt-md-4">
-	<h1 class="h5 d-inline align-middle my-2 mr-3 pb-2">{{ __('navbar.recommendations') }}</h1>
+	<h1 class="h5 d-inline align-middle my-2 mr-3">{{ __('navbar.recommendations') }}</h1>
 	@if(auth::check())
 	<div class="d-inline" ng-show="active_tab!='mood_pick'">
 		<button class="btn btn-outline-secondary my-2" ng-show="party_members.length>0" type="button" disabled>{{ Auth::user()->name }}</button>
 		<button class="btn btn-outline-secondary mr-2 my-2" type="button" ng-repeat="user in party_members" ng-click="remove_from_party(user.user_id);">@{{user.name}} <i class="fa fa-times"></i></button>
-		<button id="addperson_button" class="btn btn-outline-secondary my-2" type="button" data-toggle="collapse" data-target="#collapseAdd" ng-click="setFocus('input_user')"><i class="fas fa-user-plus"></i> {{ __('general.add_person') }}</button>
+		<button id="addperson_button" class="btn btn-outline-secondary my-2 border-circle" type="button" data-toggle="collapse" data-target="#collapseAdd" ng-click="setFocus('input_user')"><i class="fas fa-user-plus"></i></button>
 	</div>
 	@endif
 	<div class="d-inline" ng-show="active_tab=='mood_pick'">
