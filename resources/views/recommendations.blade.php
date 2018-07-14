ac953c0dcd96d7aaf17dd92a409cb26f4fa22066
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-14 19:25:05 +0300

    logo

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -5,7 +5,7 @@
 @section('body')
 <!-- H1 +AddPerson +AddMode row  -->
 <div class="col mb-2 mt-3 mt-md-4">
-	<h1 class="h5 d-inline align-middle my-2 mr-3">{{ __('navbar.recommendations') }}</h1>
+	<h1 class="h5 d-inline align-middle my-2 mr-3 pb-2">{{ __('navbar.recommendations') }}</h1>
 	@if(auth::check())
 	<div class="d-inline" ng-show="active_tab!='mood_pick'">
 		<button class="btn btn-outline-secondary my-2" ng-show="party_members.length>0" type="button" disabled>{{ Auth::user()->name }}</button>
