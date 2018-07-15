ca183fe3836208f8320871c1f36730cfb7443b65
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-15 12:20:04 +0300

    logo

--- app/Jobs/RefreshSiteMapJob.php
+++ app/Jobs/RefreshSiteMapJob.php
@@ -37,7 +37,8 @@ class RefreshSiteMapJob implements ShouldQueue
         $xml = '<?xml version="1.0" encoding="utf-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
 <url> <loc>https://topcorn.io/recommendations</loc> <lastmod>'.substr(Carbon::now(), 0, 10).'</lastmod> <changefreq>weekly</changefreq> <priority>1</priority> </url>
-<url> <loc>https://topcorn.io/home</loc> <lastmod>2018-05-11</lastmod> <changefreq>monthly</changefreq> <priority>1</priority> </url>
+<url> <loc>https://topcorn.io</loc> <lastmod>'.substr(Carbon::now(), 0, 10).'</lastmod> <changefreq>weekly</changefreq> <priority>1</priority> </url>
+<url> <loc>https://topcorn.io/home</loc> <lastmod>2018-05-11</lastmod> <changefreq>monthly</changefreq> <priority>0.80</priority> </url>
 <url> <loc>https://topcorn.io/register</loc> <lastmod>2018-05-11</lastmod> <changefreq>monthly</changefreq> <priority>0.50</priority> </url>
 <url> <loc>https://topcorn.io/login</loc> <lastmod>2018-05-11</lastmod> <changefreq>monthly</changefreq> <priority>0.50</priority> </url>
 <url> <loc>https://topcorn.io/faq</loc> <lastmod>2018-05-11</lastmod> <changefreq>monthly</changefreq> <priority>0.80</priority> </url>
