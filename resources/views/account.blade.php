ef5f66b4cf4f7ddd5dbc4257865173fbc42c400d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-15 03:36:20 +0300

    logo

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -26,6 +26,7 @@
 <!-- Tabs Button Mobile -->
 <div class="scrollmenu my-3 d-md-none tab2">
 	<button class="btn btn-link border-no-radius text-sm-center text-muted text-no-decoration active" href="/account">{{ __('general.profile') }}</button>
+	<button class="btn btn-link border-no-radius text-sm-center text-muted text-no-decoration active" href="/account/password">{{ __('general.password') }}</button>
 </div>
 <!-- Tabs Button Mobile -->
 
