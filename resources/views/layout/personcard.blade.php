baf5407d5cd390412b6830a01e3c5496c276d5ad
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-15 12:16:31 +0300

    logo

--- resources/views/layout/personcard.blade.php
+++ resources/views/layout/personcard.blade.php
@@ -1,5 +1,5 @@
 <div class="card-group no-gutters">
-	<div class="col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-4" ng-repeat="person in people">
+	<div class="col-6 col-md-4 col-lg-3 col-xl-2 mt-4" ng-repeat="person in people">
 		<div class="card mx-sm-2">
 			<a href="/person/@{{person.id}}" target={{$target}}>
 				<img class="card-img-top" ng-src="{{config('constants.image.movie_card')[$image_quality]}}@{{person.profile_path}}" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
