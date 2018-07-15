541d76b71377b7ac7eb775b97a7cd1f8edaf19f1
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-15 03:35:15 +0300

    logo

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -25,7 +25,7 @@
 
 <!-- Tabs Button Mobile -->
 <div class="scrollmenu my-3 d-md-none tab2">
-	<button class="btn btn-link border-no-radius text-sm-center text-muted text-no-decoration active" href="/account">{{ __('general.seen_movies') }}</button>
+	<button class="btn btn-link border-no-radius text-sm-center text-muted text-no-decoration active" href="/account">{{ __('general.profile') }}</button>
 </div>
 <!-- Tabs Button Mobile -->
 
