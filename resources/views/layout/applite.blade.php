bb4ce5b00c9108edd88ee48cd4e2aab2765ef0cb
Uygar Yılmaz <uygary@anadolu.edu.tr>
2018-07-21 14:09:56 +0300

    logo

--- resources/views/layout/applite.blade.php
+++ resources/views/layout/applite.blade.php
@@ -51,7 +51,7 @@
 
 
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-night px-md-0 py-md-0 z-1041">
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-night px-md-0 py-md-0 z-1041">
         <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
--- resources/views/layout/appnew.blade.php
+++ resources/views/layout/appnew.blade.php
@@ -88,7 +88,7 @@
 
 
 
-    <nav class="navbar navbar-expand-md navbar-dark bg-night py-md-0">
+    <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-night py-md-0">
         <div class="container">
             <a class="navbar-brand" href="/">
                 <img src="/images/topcorn_logo.png" class="d-none d-md-inline" width="66" height="66">
