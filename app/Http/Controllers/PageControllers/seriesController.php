f73a43a3da402d2ae3da2d46af0c5839ff25995f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-11 19:29:11 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -11,7 +11,9 @@ class seriesController extends Controller
 {
     public function series($id, $lang = '', $secondary_lang='')
     {
-    	if(Auth::check()){
+    	$image_quality = Auth::check() ? Auth::User()->image_quality : 1;
+
+        if(Auth::check()){
             $target = Auth::User()->open_new_tab == 1 ? '_blank' : '_self';
             $watched_movie_number = Rated::where('user_id', Auth::id())->where('rate', '<>', 0)->count();
         }else{
