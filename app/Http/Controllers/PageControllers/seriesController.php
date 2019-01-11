5d398cee69a6dabdc8e11f702d91d6a9e215365f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-11 19:09:16 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -4,11 +4,20 @@ namespace App\Http\Controllers\PageControllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class seriesController extends Controller
 {
     public function series($id, $lang = '', $secondary_lang='')
     {
+    	if(Auth::check()){
+            $target = Auth::User()->open_new_tab == 1 ? '_blank' : '_self';
+            $watched_movie_number = Rated::where('user_id', Auth::id())->where('rate', '<>', 0)->count();
+        }else{
+            $target = '_self';
+            $watched_movie_number = null;
+        }
+
     	return view('series', compact('id'));
     }
 }
