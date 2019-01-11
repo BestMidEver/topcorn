8b7f1cbb4f0beb56a95c12240cf5fcca944b1703
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-11 17:29:10 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -9,6 +9,6 @@ class seriesController extends Controller
 {
     public function series($id, $lang = '', $secondary_lang='')
     {
-    	return view('series');
+    	return view('series', compact('id'));
     }
 }
