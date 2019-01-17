9c44806b21d9d36a272cc0420c7760607b2755ae
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-17 17:00:29 +0100

    series

--- app/Http/Controllers/ApiControllers/SeriesBanController.php
+++ app/Http/Controllers/ApiControllers/SeriesBanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Series_ban;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class SeriesBanController extends Controller
 {
