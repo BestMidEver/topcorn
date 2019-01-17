4cb29fecdf7698dba60b7fa0d6759f7967540cd9
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-17 20:56:44 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -329,7 +329,7 @@
 
 <!--People Who Liked Also Liked Section-->
 <div class="container-fluid px-0 mt-5" id="guest_stars" ng-hide="is_waiting">
-    <div class="dropdown d-inline">
+    <div class="dropdown d-inline mr-2">
         <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle px-3 px-md-0 border-0 background-inherit" type="button" id="peopleWhoLikedAlsoLikedDropdownButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         <span class="h5" ng-if="page_variables.active_tab_3==0">People Who Liked This Also Liked</span>
         <span class="h5" ng-if="page_variables.active_tab_3==1">Similar Movies</span>
@@ -341,8 +341,8 @@
             <button class="dropdown-item" ng-click="page_variables.active_tab_3=2;">{{ __('general.movie_lists_title') }}</button>
         </div>
     </div>
-    <span class="text-muted pl-2" ng-if="page_variables.active_tab_3<2"><small>Sorted by relevance</small></span>
-	<a href="/createlist/new" class="btn btn-outline-success pl-2" ng-if="page_variables.active_tab_3==2" target="{{$target}}">
+    <span class="text-muted" ng-if="page_variables.active_tab_3<2"><small>Sorted by relevance</small></span>
+	<a href="/createlist/new" class="btn btn-outline-success" ng-if="page_variables.active_tab_3==2" target="{{$target}}">
 		<i class="fas fa-plus"></i> {{ __('general.create_list') }}
 	</a>
 	<div ng-if="page_variables.active_tab_3<2">
