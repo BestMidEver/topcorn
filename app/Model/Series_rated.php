ba363b26fd4a05737559b1138abf3825e0135a39
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-18 17:36:40 +0100

    series

--- app/Model/Series_rated.php
+++ app/Model/Series_rated.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Series_rated extends Model
 {
-    //
+    protected $fillable = [
+    	'user_id',
+    	'series_id',
+    	'rate',
+    ];
+
+    /*public function movie(){
+    	return $this->belongsTo(Movie::class);
+    }*/
 }
