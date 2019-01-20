bc332c3ecee2c47bf2d4801427abd7334e08ac6a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-20 13:44:00 +0100

    series

--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -116,7 +116,7 @@
             <button class="btn btn-link nav-link text-muted" ng-class="{'active':page_variables.active_tab_1==-1}" ng-click="page_variables.active_tab_1=-1;pull_data()">General Info</button>
         </li>
         <li class="nav-item" ng-repeat="season in page_variables.seasons">
-            <button class="btn btn-link nav-link" ng-class="{'active':page_variables.active_tab_1==season.season_number, season.season_number==page_variables.last_seen_season?'text-warning':'text-muted'}" ng-click="page_variables.active_tab_1=season.season_number;page_variables.active_tab_2=-1;pull_data()"><span ng-if="season.season_number != 0">S@{{season.season_number>9?season.season_number:'0'+season.season_number}}</span><span ng-if="season.season_number == 0">Specials</span></button>
+            <button class="btn btn-link nav-link" ng-class="{season.season_number==page_variables.last_seen_season?'text-warning':'text-muted'}" ng-click="page_variables.active_tab_1=season.season_number;page_variables.active_tab_2=-1;pull_data()"><span ng-if="season.season_number != 0">S@{{season.season_number>9?season.season_number:'0'+season.season_number}}</span><span ng-if="season.season_number == 0">Specials</span></button>
         </li>
     </ul>
 </div>
