d612166fbc34174130049b5e03111db557fbee4d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-21 21:32:45 +0100

    series

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -50,7 +50,7 @@
 
 <!-- Tabs Button -->
 <div class="container-fluid mt-3 pb-3 d-none d-md-inline">
-	<ul class="nav justify-content-md-center tab1" ng-init=active_tab='vote_count'>
+	<ul class="nav justify-content-md-center tab1" ng-init="active_tab='vote_count'">
 		<li class="nav-item">
 			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab=='vote_count'}" ng-click="active_tab='vote_count';filter('vote_count')">{{ __('general.most_rated') }}</button>
 		</li>
@@ -83,6 +83,17 @@
 
 
 <div class="container-fluid">
+	<div class="dropdown d-inline mr-2" ng-init="active_tab='vote_count'">
+		<button class="btn btn-outline-secondary dropdown-toggle mt-3" type="button" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+			<i class="fas fa-sort-amount-down"></i> <span ng-if="active_tab=='vote_count">Most Popular</span><span ng-if="active_tab=='vote_average">Top Rated</span><span ng-if="active_tab=='release_date">Newest</span><span ng-if="active_tab=='title">A -> Z</span>
+		</button>
+		<div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+			<button class="dropdown-item" ng-click="active_tab='vote_count';filter('vote_count')">Most Popular</button>
+			<button class="dropdown-item" ng-click="active_tab='vote_average';filter('vote_average')">Top Rated</button>
+			<button class="dropdown-item" ng-click="active_tab='release_date';filter('release_date')">Newest</button>
+			<button class="dropdown-item" ng-click="active_tab='title';filter('title')">A -> Z</button>
+		</div>
+	</div>
 	<div class="dropdown" ng-init="cast_or_crew='{{ __('general.all') }}'">
 		<button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 			@{{cast_or_crew}}
