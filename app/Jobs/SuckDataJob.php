fcaab508fa70f78f3b7003bf1891dfc3eab7eaa5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-21 17:52:14 +0100

    series

--- app/Jobs/SuckDataJob.php
+++ app/Jobs/SuckDataJob.php
@@ -89,13 +89,13 @@ class SuckDataJob implements ShouldQueue
 
 
 
-        foreach(Series_rated::All()->pluck('movie_id')->unique() as $id){
+        foreach(Series_rated::All()->pluck('series_id')->unique() as $id){
             SuckSeriesJob::dispatch($id, true)->onQueue("low");
         }
-        foreach(Series_later::All()->pluck('movie_id')->unique() as $id){
+        foreach(Series_later::All()->pluck('series_id')->unique() as $id){
             SuckSeriesJob::dispatch($id, false)->onQueue("low");
         }
-        foreach(Series_ban::All()->pluck('movie_id')->unique() as $id){
+        foreach(Series_ban::All()->pluck('series_id')->unique() as $id){
             SuckSeriesJob::dispatch($id, false)->onQueue("low");
         }
 
