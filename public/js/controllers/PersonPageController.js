572b9bdf33a6bbb3576a5dcc1451d658110afbc5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-22 19:20:30 +0100

    series

--- public/js/controllers/PersonPageController.js
+++ public/js/controllers/PersonPageController.js
@@ -162,7 +162,7 @@ MyApp.controller('PersonPageController', function($scope, $http, $anchorScroll, 
 	};
 
 	$scope.set_full_screen_images = function(image){
-		$scope.page_variables.current_image.poster_path = image.file_path;
+		$scope.page_variables.current_image_poster_path = image.file_path;
 	}
 //////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////// RETRIEVE MOVIECARD DATA //////////////////////////////
