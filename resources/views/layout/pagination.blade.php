db84d10d73e6c3e6ce9a42bf746be1a6fc9fdb6f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-22 18:20:06 +0100

    series

--- resources/views/layout/pagination.blade.php
+++ resources/views/layout/pagination.blade.php
@@ -1,5 +1,5 @@
 <nav aria-label="Page navigation" class="mt-5" ng-show="pagination{{ $suffix }} > 1">
-  <ul class="pagination justify-content-center mb-0" ng-init="current_page{{ $suffix }}=1">
+  <ul class="pagination justify-content-center mb-0">
     <li class="page-item" ng-class="{'active':$index+1==current_page{{ $suffix }}}" ng-repeat="p in [] | range:pagination{{ $suffix }}" ng-show="$index+1==1 || ($index+1>current_page{{ $suffix }}-3 && $index+1<current_page{{ $suffix }}+3) || $index+1==pagination{{ $suffix }} || (current_page{{ $suffix }}<5 && $index+1<7) || (current_page{{ $suffix }}>pagination{{ $suffix }}-4 && $index+1>pagination{{ $suffix }}-6)">
       <button type="button" class="page-link" ng-click="paginate{{ $suffix }}($index+1)">@{{$index+1}}</button>
     </li>
