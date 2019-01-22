191143ba99cfb52351b1bf1b6cd82987e4c38259
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-22 22:20:42 +0100

    series

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -154,6 +154,8 @@
 	</div>
 </div>
 <div class="modal fade" id="image_modal" tabindex="-1" role="dialog" aria-labelledby="image_modal">
-	<img class="mh-100 mw-100" ng-src="{{config('constants.image.original')}}@{{page_variables.current_image_poster_path}}" on-error-src="{{config('constants.image.rate_modal_error')}}" alt="Card image">
+	<div class="votecard modal-dialog modal-dialog-centered" role="document">
+		<img class="mh-100 mw-100" ng-src="{{config('constants.image.original')}}@{{page_variables.current_image_poster_path}}" on-error-src="{{config('constants.image.rate_modal_error')}}" alt="Card image">
+	</div>
 </div>
 @endsection
