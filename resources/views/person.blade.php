0424e623e86c8230805fb75b1d3413f1e3cb2ac7
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-22 19:42:16 +0100

    series

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -158,7 +158,7 @@
 	<div class="modal-dialog modal-lg modal-dialog-centered" role="document">
 		<div class="modal-content">
 			<div class="card">
-				<img class="card-img" ng-style="{height: iframeHeight + 'px!important'}" ng-src="{{config('constants.image.original')}}@{{page_variables.current_image_poster_path}}" on-error-src="{{config('constants.image.rate_modal_error')}}" alt="Card image">
+				<img class="card-img" ng-style="{height: '200px!important'}" ng-src="{{config('constants.image.original')}}@{{page_variables.current_image_poster_path}}" on-error-src="{{config('constants.image.rate_modal_error')}}" alt="Card image">
 				<div class="card-img-overlay p-2">
 					<div class="text-center h-100 d-flex flex-column justify-content-between">
 						<div class="d-flex flex-row justify-content-between">
