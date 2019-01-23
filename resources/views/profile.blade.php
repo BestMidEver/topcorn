89ce3fe0572e5d883ab0d1e4132ac59656122ae6
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-23 18:03:44 +0100

    series

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -103,12 +103,14 @@
 
 <!-- Tabs Button -->
 <div class="container-fluid mt-3 d-none d-md-inline">
+	<div class="justify-content-md-center">
 
-			<div class="btn-group btn-group justify-content-md-center" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
+			<div class="btn-group btn-group" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series=='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='movies'">Movies</button>
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series!='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='series'">Series</button>
 			</div>
 
+	</div>
 </div>
 <div class="container-fluid mt-3 pb-3 d-none d-md-inline">
 	<ul class="nav justify-content-md-center tab1">
