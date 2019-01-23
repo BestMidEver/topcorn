cc1dd9264730e8b3658092ca594d83a33c0bd9e4
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-23 17:45:45 +0100

    series

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -105,7 +105,7 @@
 <div class="container-fluid mt-3 pb-3 d-none d-md-inline">
 	<ul class="nav justify-content-md-center tab1">
 		<li class="nav-item">
-			<div class="btn-group btn-group mr-4" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
+			<div class="btn-group btn-group mr-5" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series=='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='movies'">Movies</button>
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series!='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='series'">Series</button>
 			</div>
