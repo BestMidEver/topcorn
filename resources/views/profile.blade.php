773969c3cf7fe6d9a6099c2246a21c34bfdc5206
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-23 17:51:43 +0100

    series

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -103,12 +103,12 @@
 
 <!-- Tabs Button -->
 <div class="container-fluid mt-3 pb-3 d-none d-md-inline">
-	<ul class="nav justify-content-md-center tab1">
-		<li class="nav-item d-block">
-			<div class="btn-group btn-group mr-5" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
+			<div class="btn-group btn-group mr-5 text-center" role="group" aria-label="Movies or Series" ng-init="page_variables.movies_or_series = 'movies'">
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series=='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='movies'">Movies</button>
 				<button type="button" class="btn" ng-class="page_variables.movies_or_series!='movies'?'btn-tab':'btn-outline-tab'" ng-click="page_variables.movies_or_series='series'">Series</button>
 			</div>
+	<ul class="nav justify-content-md-center tab1">
+		<li class="nav-item">
 		</li>
 		<li class="nav-item">
 			<button class="btn btn-link nav-link text-muted" ng-class="{'active':active_tab!='get_laters' && active_tab!='get_bans' && active_tab!='get_lists'}" ng-click="mod_title='{{ __('general.definitely_recommend') }}';active_tab='get_rateds/5';get_first_page_data()">{{ __('general.seen_movies') }}</button>
