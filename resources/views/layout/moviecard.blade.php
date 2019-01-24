fd2eb9d88ee54e4d719fe2a286bec0d11053446a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-24 19:42:07 +0100

    series

--- resources/views/layout/moviecard.blade.php
+++ resources/views/layout/moviecard.blade.php
@@ -18,7 +18,7 @@
 								<small ng-if="active_tab=='mood_pick'"><span class="h5 text-warning">@{{movie.point}}</span>/@{{movie.count*5}} {{ __('general.point') }}</small>
 							</div>
 						</div>
-						<div class="d-flex flex-row justify-content-center" ng-if="movie.day_difference_last">
+						<div class="d-flex flex-row justify-content-center" ng-if="!isNaN(movie.day_difference_last)">
 							<div class="text-white" ng-if="movie.day_difference_last<1">
 								<small>New Episode</small>
 								<span class="d-block"><span class="h5 text-warning">
