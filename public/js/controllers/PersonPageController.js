f3f504fce8915efa69150817be6c7c92cf72d3d4
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-24 15:26:24 +0100

    series

--- public/js/controllers/PersonPageController.js
+++ public/js/controllers/PersonPageController.js
@@ -40,7 +40,7 @@ MyApp.controller('PersonPageController', function($scope, $http, $anchorScroll, 
 			$scope.set_moviecard_data('movies');
 			temp = _.find($scope.person.tagged_images.results, function(num){ return num.aspect_ratio>1; })
 			console.log(temp)
-			if(temp) $scope.cover=temp.file_path;
+			if(temp>0) $scope.cover=temp.file_path;
 			else if($scope.person.tagged_images.results.length>0) $scope.cover=$scope.person.tagged_images.results[0].file_path;
 			else $scope.cover=$scope.movies[0].backdrop_path;
 			console.log($scope.cover, $scope.person.tagged_images.results)
