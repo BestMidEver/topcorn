549edef900d1ea9133f316e6ff3c621b65eb9057
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-25 08:15:42 +0100

    series

--- public/js/controllers/SeriesPageController.js
+++ public/js/controllers/SeriesPageController.js
@@ -459,7 +459,7 @@ MyApp.controller('MoviePageController', function($scope, $http, $sce, $anchorScr
 				if(temp.length>0){
 					episode=temp[0].episode_number;
 					season=temp[0].season_number;
-				}else if($scope.movie.in_production){
+				}else if($scope.movie.in_production || $scope.page_variables.active_tab_1!=$scope.page_variables.seasons[$scope.page_variables.seasons.length-1].season_number){
 					temp=_.where($scope.page_variables.seasons, {season_number:$scope.page_variables.active_tab_1+1});
 					episode=1;
 					season=temp[0].season_number;
