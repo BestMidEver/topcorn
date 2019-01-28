3d04db3669ccd91f79a0af0acc470c8367023170
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-28 23:55:45 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -77,7 +77,8 @@ class seriesController extends Controller
             DB::raw('sum(IF(r2.rate > 0, 1, 0)) as count'),
             DB::raw('sum(IF(r2.rate > 0, r2.rate-1, 0))*25 DIV sum(IF(r2.rate > 0, 1, 0)) as percent')
         )
-        ->groupBy('series.id');
+        ->groupBy('series.id')
+        ->get();
 
         $user_series_record = json_decode( json_encode($user_series_record), true);
 
