abea6f8e40d769423b3cc42d3e3d68757935a655
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-28 14:07:40 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Rated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class seriesController extends Controller
 {
