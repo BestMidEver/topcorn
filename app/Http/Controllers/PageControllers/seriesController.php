ad157c2b1be29ec5d3bc5bdba9c190041dcb938e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-29 00:23:22 +0100

    series

--- app/Http/Controllers/PageControllers/seriesController.php
+++ app/Http/Controllers/PageControllers/seriesController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\PageControllers;
 
 use App\Http\Controllers\Controller;
 use App\Model\Rated;
-use GuzzleHttp\json_decode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -46,45 +45,59 @@ class seriesController extends Controller
                 $series_year = '';
                 $poster_path = '';
             }
+            $temp = DB::table('series')
+            ->where('series.id', '=', $id)
+            ->leftjoin('series_rateds', function ($join) {
+                $join->on('series_rateds.series_id', '=', 'series.id')
+                ->where('series_rateds.user_id', '=', Auth::user()->id);
+            })
+            ->leftjoin('series_laters', function ($join) {
+                $join->on('series_laters.series_id', '=', 'series.id')
+                ->where('series_laters.user_id', '=', Auth::user()->id);
+            })
+            ->leftjoin('series_bans', function ($join) {
+                $join->on('series_bans.series_id', '=', 'series.id')
+                ->where('series_bans.user_id', '=', Auth::user()->id);
+            })
+            ->leftjoin('series_recommendations', 'series_recommendations.this_id', '=', 'series.id')
+            ->leftjoin('series_rateds as r2', function ($join) {
+                $join->on('r2.series_id', '=', 'series_recommendations.series_id')
+                ->where('r2.user_id', Auth::user()->id);
+            })
+            ->select(
+                'series.id as series_id',
+                'series_rateds.id as rated_id',
+                'series_rateds.rate as rate_code',
+                'series_laters.id as later_id',
+                'series_bans.id as ban_id',
+                DB::raw('sum(IF(r2.rate > 0, ABS(r2.rate-3)*(r2.rate-3)*series_recommendations.rank, 0)) AS point'),
+                DB::raw('sum(IF(r2.rate > 0, 4*series_recommendations.rank, 0)) as p2'),
+                DB::raw('sum(IF(r2.rate > 0, 1, 0)) as count'),
+                DB::raw('sum(IF(r2.rate > 0, r2.rate-1, 0))*25 DIV sum(IF(r2.rate > 0, 1, 0)) as percent')
+            )
+            ->groupBy('series.id');
+            if($temp->count() > 0){
+                $temp = $temp->first();
+                $rated_id = $temp->rated_id;
+                $rate_code = $temp->rate_code;
+                $later_id = $temp->later_id;
+                $ban_id = $temp->ban_id;
+                $point = $temp->point;
+                $p2 = $temp->p2;
+                $count = $temp->count;
+                $percent = $temp->percent;
+            }else{
+                $rated_id = '';
+                $rate_code = '';
+                $later_id = '';
+                $ban_id = '';
+                $point = '';
+                $p2 = '';
+                $count = '';
+                $percent = '';
+            }
         }
 
-        $user_series_record = DB::table('series')
-        ->where('series.id', '=', $id)
-        ->leftjoin('series_rateds', function ($join) {
-            $join->on('series_rateds.series_id', '=', 'series.id')
-            ->where('series_rateds.user_id', '=', Auth::user()->id);
-        })
-        ->leftjoin('series_laters', function ($join) {
-            $join->on('series_laters.series_id', '=', 'series.id')
-            ->where('series_laters.user_id', '=', Auth::user()->id);
-        })
-        ->leftjoin('series_bans', function ($join) {
-            $join->on('series_bans.series_id', '=', 'series.id')
-            ->where('series_bans.user_id', '=', Auth::user()->id);
-        })
-        ->leftjoin('series_recommendations', 'series_recommendations.this_id', '=', 'series.id')
-        ->leftjoin('series_rateds as r2', function ($join) {
-            $join->on('r2.series_id', '=', 'series_recommendations.series_id')
-            ->where('r2.user_id', Auth::user()->id);
-        })
-        ->select(
-            'series.id as series_id',
-            'series_rateds.id as rated_id',
-            'series_rateds.rate as rate_code',
-            'series_laters.id as later_id',
-            'series_bans.id as ban_id',
-            DB::raw('sum(IF(r2.rate > 0, ABS(r2.rate-3)*(r2.rate-3)*series_recommendations.rank, 0)) AS point'),
-            DB::raw('sum(IF(r2.rate > 0, 4*series_recommendations.rank, 0)) as p2'),
-            DB::raw('sum(IF(r2.rate > 0, 1, 0)) as count'),
-            DB::raw('sum(IF(r2.rate > 0, r2.rate-1, 0))*25 DIV sum(IF(r2.rate > 0, 1, 0)) as percent')
-        )
-        ->groupBy('series.id')
-        ->first();
-
-        $user_series_record -> json_decode( json_encode($user_series_record), true);
-
-        //return response()->json($user_series_record->first());
-
-    	return view('series', compact('id', 'id_dash_name', 'image_quality', 'target', 'watched_movie_number', 'series_name', 'series_en_name', 'series_tr_name', 'series_hu_name', 'series_year', 'series_path', 'user_series_record'));
+    	return view('series', compact('id', 'id_dash_name', 'image_quality', 'target', 'watched_movie_number', 'series_name', 'series_en_name', 'series_tr_name', 'series_hu_name', 'series_year', 'series_path', 'rated_id', 'rate_code', 'later_id', 'ban_id', 'point', 'p2', 'count', 'percent'));
     }
 }
