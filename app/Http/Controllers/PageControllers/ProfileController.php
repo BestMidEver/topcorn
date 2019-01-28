21e387482a971e23376d589fbcf2d6b71d21d34f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-28 12:03:50 +0100

    series

--- app/Http/Controllers/PageControllers/ProfileController.php
+++ app/Http/Controllers/PageControllers/ProfileController.php
@@ -364,7 +364,7 @@ class ProfileController extends Controller
         }else if($mode == 'available'){
             $return_val = $return_val
             ->whereRaw('(DATEDIFF(series.last_episode_air_date, series_seens.air_date) > 0) OR (DATEDIFF(series.next_episode_air_date, NOW()) < 0)')
-            ->orderByRaw('ISNULL(series_seens.updated_at), series_seens.updated_at asc')
+            ->orderByRaw('ISNULL(series_seens.updated_at), series_seens.updated_at desc')
             ->orderBy('series_laters.updated_at', 'desc');
         }else if($mode == 'awaited'){
             $return_val = $return_val
@@ -372,7 +372,7 @@ class ProfileController extends Controller
             ->orderByRaw('ISNULL(day_difference_next), day_difference_next asc');
         }else{
             $return_val = $return_val
-            ->orderByRaw('ISNULL(series_seens.updated_at), series_seens.updated_at asc')
+            ->orderByRaw('ISNULL(series_seens.updated_at), series_seens.updated_at desc')
             ->orderBy('series_laters.updated_at', 'desc');
         }
 
