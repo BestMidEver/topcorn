118162a85f2bbf32349370e61891e828b87aea04
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-29 10:31:07 +0100

    series

--- public/js/controllers/SeriesPageController.js
+++ public/js/controllers/SeriesPageController.js
@@ -14,53 +14,7 @@ MyApp.controller('MoviePageController', function($scope, $http, $sce, $anchorScr
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////// SCROLL TO TOP ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
-	console.log(pass)
 	$scope.user_movie_record=pass.user_movie_record;
-	console.log($scope.user_movie_record);
-	/*if(pass.is_auth == 1){
-		$http({
-			method: 'GET',
-			url: '/api/series_laters/'+pass.seriesid
-		}).then(function successCallback(response) {
-			if(response.data.hasOwnProperty('series_id')){
-				$scope.user_movie_record.later_id = response.data.id;
-			}
-		}, function errorCallback(response) {
-		});
-
-		$http({
-			method: 'GET',
-			url: '/api/series_rateds/'+pass.seriesid
-		}).then(function successCallback(response) {
-			if(response.data.hasOwnProperty('series_id')){
-				$scope.user_movie_record.rated_id = response.data.id;
-				$scope.user_movie_record.rate_code = response.data.rate;
-			}
-		}, function errorCallback(response) {
-		});
-
-		$http({
-			method: 'GET',
-			url: '/api/series_bans/'+pass.seriesid
-		}).then(function successCallback(response) {
-			if(response.data.hasOwnProperty('series_id')){
-				$scope.user_movie_record.ban_id = response.data.id;
-			}
-		}, function errorCallback(response) {
-		});
-
-		$http({
-			method: 'GET',
-			url: '/api/series_seens/'+pass.seriesid
-		}).then(function successCallback(response) {
-			if(response.data.hasOwnProperty('series_id')){
-				$scope.user_movie_record.last_seen_id = response.data.id;
-				$scope.user_movie_record.last_seen_season = response.data.season_number;
-				$scope.user_movie_record.last_seen_episode = response.data.episode_number;
-			}
-		}, function errorCallback(response) {
-		});
-	}*/
 
 	///////////////////////////////////////////////////// YENİ YENİ YENİ YENİ //////////////////////////////////////////////////
 	$scope.page_variables={};
