e19cae5ddfa108c47601840c2ad6785ed34a1b18
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-29 10:32:08 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -19,7 +19,7 @@
 								<div ng-if="user_movie_record.percent > 0">
 									<small class="text-white">{{ __("general.according_to_your_taste") }}</small>
 									<div>
-										<span class="text-warning display-4 d-none d-md-inline">{!! __('general.moviecard_percent', ['user_movie_record.percent' => $suffix]) !!}</span><span class="text-warning h5 d-md-none">{!! __('general.moviecard_percent', ['user_movie_record.percent' => $suffix]) !!}</span><span class="text-white"> <small>{{ __("general.match") }}</small></span>
+										<span class="text-warning display-4 d-none d-md-inline">{!! __('general.moviecard_percent', ['user_movie_record.percent' => 'suffix']) !!}</span><span class="text-warning h5 d-md-none">{!! __('general.moviecard_percent', ['user_movie_record.percent' => 'suffix']) !!}</span><span class="text-white"> <small>{{ __("general.match") }}</small></span>
 									</div>
 									@if(Auth::check())
 										@if(Auth::User()->advanced_filter)
@@ -54,7 +54,7 @@
 						<div class="col pb-2 pr-2 text-right">
 							<div ng-if="user_movie_record.percent > 0">
 								<div>
-									<small class="text-white">{{ __("general.according_to_your_taste") }} </small><span class="text-warning h4 d-none d-md-inline">{!! __('general.moviecard_percent', ['user_movie_record.percent' => $suffix]) !!}</span><span class="text-warning h5 d-md-none">{!! __('general.moviecard_percent', ['user_movie_record.percent' => $suffix]) !!}</span><span class="text-white"> <small>{{ __("general.match") }}</small></span>
+									<small class="text-white">{{ __("general.according_to_your_taste") }} </small><span class="text-warning h4 d-none d-md-inline">{!! __('general.moviecard_percent', ['user_movie_record.percent' => 'suffix']) !!}</span><span class="text-warning h5 d-md-none">{!! __('general.moviecard_percent', ['user_movie_record.percent' => 'suffix']) !!}</span><span class="text-white"> <small>{{ __("general.match") }}</small></span>
 								</div>
 								@if(Auth::check())
 									@if(Auth::User()->advanced_filter)
