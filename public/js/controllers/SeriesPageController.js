88f29c10810665a8b19a078d1f4be93ba6da985e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-01-30 10:26:30 +0100

    series

--- public/js/controllers/SeriesPageController.js
+++ public/js/controllers/SeriesPageController.js
@@ -14,6 +14,15 @@ MyApp.controller('SeriesPageController', function($scope, $http, $sce, $anchorSc
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////// SCROLL TO TOP ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
+	$('#collapseMovies').on('show.bs.collapse', function () {
+		console.log('A')
+		angular.element( document.querySelector( '#top_moviecard_6' ) ).addClass('d-none');
+	});
+	$('#collapseMovies').on('hide.bs.collapse', function () {
+		console.log('B')
+		angular.element( document.querySelector( '#top_moviecard_6' ) ).removeClass('d-none');
+	});
+	
 	$scope.user_movie_record=pass.user_movie_record;
 
 	///////////////////////////////////////////////////// YENİ YENİ YENİ YENİ //////////////////////////////////////////////////
@@ -778,13 +787,4 @@ MyApp.controller('SeriesPageController', function($scope, $http, $sce, $anchorSc
 //////////////////////////////////////// TUTORIAL ////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 	}
-
-	$('#collapseMovies').on('show.bs.collapse', function () {
-		console.log('A')
-		angular.element( document.querySelector( '#top_moviecard_6' ) ).addClass('d-none');
-	});
-	$('#collapseMovies').on('hide.bs.collapse', function () {
-		console.log('B')
-		angular.element( document.querySelector( '#top_moviecard_6' ) ).removeClass('d-none');
-	});
 });
