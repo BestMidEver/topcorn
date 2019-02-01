8c8c1d7b8076daaac00ba285c7bc4ff4e09dda2c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-01 16:27:50 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -296,7 +296,7 @@
 
 <!--Review Section-->
 <div class="container-fluid px-0 mt-5">	
-	<div class="h5 px-3 px-md-0 d-flex justify-content-between">
+	<div class="h5 px-3 px-md-0 mb-0 d-flex justify-content-between">
 		<span class="mb-0 pr-2">{{ __('general.reviews') }}</span>
 		<button ng-click="" class="btn btn-outline-secondary addblack border-0"
 			@if(Auth::check())
