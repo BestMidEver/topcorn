9050f0a05ebf0686ff9dc918d7ca0d3ee86abb77
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-01 18:45:16 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -347,7 +347,7 @@
 	</div>
 </div>
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
+  <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">{{ __('general.add_review') }}</h5>
