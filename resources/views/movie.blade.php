2258f9be5ad9eec7916792baf1c53a45051ef69b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-01 16:16:26 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -298,7 +298,7 @@
 <div class="container-fluid px-0 mt-5">	
 	<div class="h5 px-3 px-md-0 d-flex justify-content-between">
 		<span class="mb-0 pr-2">{{ __('general.reviews') }}</span>
-		<button ng-click="" class="btn btn-outline-primary addseen border-0"
+		<button ng-click="" class="btn btn-outline-secondary addseen border-0"
 			@if(Auth::check())
 				@if(Auth::User()->tt_movie < 50)
             data-toggle="popover" data-placement="bottom" title='{{ __("tutorial.hint") }}<a class="close tooltip-x" href="#close-tooltip">&times;</a>' id="review"
