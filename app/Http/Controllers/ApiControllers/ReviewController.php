68564bfad0ac422245f735c0e4c6b2e688dfdbb4
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-02 21:00:42 +0100

    series

--- app/Http/Controllers/ApiControllers/ReviewController.php
+++ app/Http/Controllers/ApiControllers/ReviewController.php
@@ -64,7 +64,7 @@ class ReviewController extends Controller
             'users.name as name',
             'users.id as user_id',
             DB::raw('COUNT(review_likes.id) as count'),
-            DB::raw('COUNT(IF(review_likes.user_id = '.Auth::id().', 1, 0)) as is_liked')
+            DB::raw('COUNT(IF(review_likes.user_id = '.Auth::id().', 0, 1)) as is_liked')
             //'movies.'.Auth::User()->lang.'_title as title',
         );
 
