9859058fa8f6cb825e7f7ebbf837a7a9de85c3a6
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-03 11:54:00 +0100

    series

--- app/Http/Controllers/ApiControllers/ReviewController.php
+++ app/Http/Controllers/ApiControllers/ReviewController.php
@@ -41,7 +41,7 @@ class ReviewController extends Controller
     {
         $review = Review::updateOrCreate(
             array('user_id' => Auth::id(), 'movie_series_id' => $request->movie_series_id),
-            array('review' => $request->review, 'mode' => 1, 'lang' => Auth::User()->lang)
+            array('review' => strip_tags($request->review), 'mode' => 1, 'lang' => Auth::User()->lang)
         );
         return Response([
             'data' => $review,
