d3d3b624064d320b1a0bfb4971246a2a746cb96e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-03 15:20:13 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -314,17 +314,11 @@
 				<div class="h6 pb-2">@{{review.author}}@{{review.name}}</div>
 				<div class="h6">
 					<button class="btn btn-outline-secondary btn-sm border-0 mt-0 addseen opacity-1" ng-disabled="review.is_mine==1" ng-click="like_review($index)"
-					@if(Auth::guest())
+					@if(!Auth::check())
 					disabled
 					@endif
 					>
-						<div ng-class="{'text-success':review.count>0}"><i class="fas fa-heart" ng-show="review.is_liked==1"></i><i class="far fa-heart" ng-show="review.is_liked==0||
-						@if(Auth::check())
-						'false'
-						@else
-						'true'
-						@endif
-						"></i><span ng-if="review.count>0"> @{{review.count}}</span></div>
+						<div ng-class="{'text-success':review.count>0}"><i class="fas fa-heart" ng-show="review.is_liked==1"></i><i class="far fa-heart" ng-show="review.is_liked==0"></i><span ng-if="review.count>0"> @{{review.count}}</span></div>
 					</button>
 				</div>
 			</div>
