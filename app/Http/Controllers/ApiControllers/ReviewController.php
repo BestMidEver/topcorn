1d5dce797412aab290b610a2d8a849656ce44cf7
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-03 11:39:28 +0100

    series

--- app/Http/Controllers/ApiControllers/ReviewController.php
+++ app/Http/Controllers/ApiControllers/ReviewController.php
@@ -73,7 +73,8 @@ class ReviewController extends Controller
             DB::raw('sum(IF(review_likes.user_id = '.Auth::id().', 1, 0)) as is_liked'),
             DB::raw('sum(IF(reviews.user_id = '.Auth::id().', 1, 0)) as is_mine')
             //'movies.'.Auth::User()->lang.'_title as title',
-        );
+        )
+        ->orderBy('count', 'desc');;
 
         return $review->paginate(10);
     }
