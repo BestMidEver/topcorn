56857d28e30d1eb9a3f1672d164465fcdc2384db
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-03 13:40:45 +0100

    series

--- public/js/controllers/MoviePageController.js
+++ public/js/controllers/MoviePageController.js
@@ -595,6 +595,7 @@ MyApp.controller('MoviePageController', function($scope, $http, $sce, $anchorScr
 				if(response.status == 201){
 					$scope.prepeare_reviews(response.data.data.data);
 					$scope.page_variables.reviews=response.data.data.data;
+					if($scope.page_variables.reviews.length>0)	if($scope.page_variables.reviews[0].is_mine==1)	$scope.page_variables.review_textarea=$scope.page_variables.reviews[0].content;
 				}
 			});
 		}
