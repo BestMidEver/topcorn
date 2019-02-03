815cf1a228f35bb0578796394e17292249581d47
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-03 11:23:29 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -311,8 +311,8 @@
 			<div class="d-flex justify-content-between">
 				<div class="h6 pb-2">@{{review.author}}@{{review.name}}</div>
 				<div class="h6">
-					<button class="btn btn-outline-secondary btn-sm border-0 mt-0 addseen" ng-click="like_review($index)">
-						<div ng-class="{'text-success':review.count>0, 'disabled':review.is_mine==1}"><i class="fas fa-heart" ng-show="review.is_liked==1"></i><i class="far fa-heart" ng-show="review.is_liked==0"></i><span ng-if="review.count>0"> @{{review.count}}</span></div>
+					<button class="btn btn-outline-secondary btn-sm border-0 mt-0 addseen" ng-class="{'disabled':review.is_mine==1}" ng-click="like_review($index)">
+						<div ng-class="{'text-success':review.count>0}"><i class="fas fa-heart" ng-show="review.is_liked==1"></i><i class="far fa-heart" ng-show="review.is_liked==0"></i><span ng-if="review.count>0"> @{{review.count}}</span></div>
 					</button>
 				</div>
 			</div>
