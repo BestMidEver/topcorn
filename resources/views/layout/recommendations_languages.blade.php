035b90b2c1efb4976fac37dbef8d8b17eb9fdd22
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 19:10:16 +0100

    series

--- resources/views/layout/recommendations_languages.blade.php
+++ resources/views/layout/recommendations_languages.blade.php
@@ -11,5 +11,30 @@
 			  <label class="custom-control-label" for="customCheck@{{$index}}">@{{language.o}}</label>
 			</div>
 		</div>
-	</div>	
+	</div>
+	<button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal">Choose</button>
+</div>
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h6 class="modal-title" id="exampleModalLabel"><span ng-if="!page_variables.is_with_review">{{ __('general.add_review') }}</span><span ng-if="page_variables.is_with_review">{{ __('general.edit_review') }}</span></h6>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body" ng-repeat="language in languages">
+				<div class="btn-group-toggle" data-toggle="buttons">
+					<label class="btn btn-secondary active">
+						<input type="checkbox" checked autocomplete="off"> @{{language.o}}
+					</label>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-outline-secondary" data-dismiss="modal">{{ __('general.close') }}</button>
+				<button type="button" class="btn btn-outline-primary" data-dismiss="modal">Apply</button>
+			</div>
+		</div>
+	</div>
 </div>
