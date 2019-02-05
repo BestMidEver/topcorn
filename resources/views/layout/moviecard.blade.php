f1406494707f6b3a77a0611fe9aa82e7cfa74d49
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 15:02:11 +0100

    series

--- resources/views/layout/moviecard.blade.php
+++ resources/views/layout/moviecard.blade.php
@@ -22,6 +22,7 @@
 							<div ng-hide="page_variables.is_guest">
 								<div class="text-white" ng-if="movie.next_episode>0">
 									<div class="h5 text-warning mb-1"> S@{{movie.next_season>9?movie.next_season:'0'+movie.next_season}}E@{{movie.next_episode>9?movie.next_episode:'0'+movie.next_episode}}</div>
+									<span ng-if="movie.day_difference_next==null" class="h5 text-warning">{{  ucfirst(__('general.unknown')) }}</span>
 									<span ng-if="movie.day_difference_next>1">{!! __('general.airs_days_later') !!}</span>
 									<span ng-if="movie.day_difference_next==1">{!! __('general.airs_tomorrow') !!}</span>
 									<span ng-if="movie.day_difference_next==0">{!! __('general.airs_today') !!}</span>
