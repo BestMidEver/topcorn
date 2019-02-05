d98753bbb3a5fef3f797d532884b7096c3c080a3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 19:48:02 +0100

    series

--- resources/views/layout/recommendations_languages.blade.php
+++ resources/views/layout/recommendations_languages.blade.php
@@ -10,8 +10,8 @@
 			  <input type="checkbox" class="custom-control-input" ng-attr-id="customCheck@{{$index}}" ng-model="f_lang_model[language.i]" ng-change="get_first_page_data()">
 			  <label class="custom-control-label" for="customCheck@{{$index}}">@{{language.o}}</label>
 			</div>-->
-			<div class="btn-group-toggle" ng-repeat="language in languages">
-				<label class="btn btn-outline-secondary m-1 border-0">
+			<div class="btn-group-toggle">
+				<label class="btn btn-outline-secondary m-1 border-0" ng-repeat="language in languages">
 					<input type="checkbox" ng-attr-id="customCheck@{{$index}}" ng-model="f_lang_model[language.i]" ng-change="get_first_page_data()"> @{{language.o}}
 				</label>
 			</div>
