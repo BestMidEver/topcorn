77c7de1287b2d30b8a7924a8d7e7410d8190a302
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 19:54:20 +0100

    series

--- resources/views/layout/recommendations_languages.blade.php
+++ resources/views/layout/recommendations_languages.blade.php
@@ -1,7 +1,7 @@
 <div class="">
 	<p class="h6 text-muted pt-3">{{ __('general.language') }}</p>
 	<div class="">
-		<div class="" >
+		<div class="">
 			<!--<label class="form-check-label nowrap">col-6 col-sm-4 col-md-3 col-lg-2 
 				<input type="checkbox" class="form-check-input" ng-model="f_lang_model[language.i]" ng-change="get_first_page_data()">
 				@{{language.o}}
