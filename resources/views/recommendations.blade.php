8e054c7c022867fb90faff75a3bb2c5130cd1337
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 23:47:48 +0100

    series

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -249,7 +249,7 @@
 	<div class="mt-3">
 		<div class="btn-group-toggle">
 			<label class="btn btn-outline-secondary border-0" ng-class="{'active':f_add_watched}">
-				<input type="checkbox" ng-attr-id="customCheck@{{$index}}" ng-model="f_add_watched" ng-change="get_first_page_data()"><span class="h6">{{ __('general.show_watched_movies') }}</span>
+				<input type="checkbox" ng-attr-id="customCheck@{{$index}}" ng-model="f_add_watched" ng-change="get_first_page_data()"><span class="h6">{{ strtoupper (__('general.show_watched_movies')) }}</span>
 			</label>
 		</div>
 	</div>
