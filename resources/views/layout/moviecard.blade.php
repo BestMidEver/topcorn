0fdff6e6f5bb020f8c7d93e0642aa5b09cddf577
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-05 14:26:11 +0100

    series

--- resources/views/layout/moviecard.blade.php
+++ resources/views/layout/moviecard.blade.php
@@ -20,7 +20,29 @@
 						</div>
 						<div class="d-flex flex-row justify-content-center" ng-if="!isNaN(movie.day_difference_last)">
 							<div ng-hide="page_variables.is_guest">
-								<div class="text-white" ng-if="movie.day_difference_last<1 && movie.last_seen_air_date.length>0">
+								<div class="text-white" ng-if="movie.next_episode>0">
+									<div class="h5 text-warning mb-1"> S@{{movie.next_season>9?movie.next_season:'0'+movie.next_season}}E@{{movie.next_episode>9?movie.next_episode:'0'+movie.next_episode}}</div>
+									<span ng-if="movie.day_difference_next==null"><span class="h5 text-warning">{{  ucfirst(__('general.unknown')) }}</span></span>
+										<span ng-if="movie.day_difference_next>1">{!! __('general.airs_days_later') !!}</span>
+										<span ng-if="movie.day_difference_next==1">{!! __('general.airs_tomorrow') !!}</span>
+										<span ng-if="movie.day_difference_next==0">{!! __('general.airs_today') !!}</span>
+										<span ng-if="movie.day_difference_next<0" class="h5 text-warning">{{ __('general.available') }}</span>
+								</div>
+								<div class="text-white" ng-if="!movie.next_episode>0">
+									<span class="h5 text-warning" ng-if="movie.status=='Ended'">{{ __('general.ended') }}</span>
+									<span class="h5 text-warning" ng-if="movie.status=='Canceled'">{{ __('general.canceled') }}</span>
+									<span class="h5 text-warning" ng-if="movie.status=='Returning Series'">{{ ucfirst(__('general.unknown')) }}</span>
+								</div>
+								<div class="text-white" ng-if="!movie.last_seen_air_date.length>0 && movie.last_episode_air_date.length>0">
+									<div>
+										<span class="h5 text-warning">{{ __('general.unseen') }}</span>
+									</div>
+								</div>
+
+
+
+
+								<!--<div class="text-white" ng-if="movie.day_difference_last<1 && movie.last_seen_air_date.length>0">
 									<small ng-if="movie.status=='Returning Series' || movie.next_episode>0">{{ __('general.new_episode') }}</small>
 									<div ng-if="movie.next_episode>0" class="h5 text-warning mb-1"> S@{{movie.next_season>9?movie.next_season:'0'+movie.next_season}}E@{{movie.next_episode>9?movie.next_episode:'0'+movie.next_episode}}</div>
 									<div>
@@ -44,7 +66,7 @@
 									<div>
 										<span class="h5 text-warning">{{ __('general.unseen') }}</span>
 									</div>
-								</div>
+								</div>-->
 							</div>
 						</div>
 					</div>
