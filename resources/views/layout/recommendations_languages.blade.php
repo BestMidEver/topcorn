e414827247e1b45e5237cbf3e483a1c8204ff427
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-06 17:11:00 +0100

    series

--- resources/views/layout/recommendations_languages.blade.php
+++ resources/views/layout/recommendations_languages.blade.php
@@ -1,5 +1,5 @@
 <div class="pt-2">
-	<button id="languages_button" class="btn btn-outline-secondary dropdown-toggle h6 m-0 border-0" type="button" data-toggle="collapse" data-target="#collapseLanguages"><span class="h6">{{ __('general.language') }}</span></button>
+	<button id="languages_button" class="btn btn-link dropdown-toggle h6 m-0 border-0" type="button" data-toggle="collapse" data-target="#collapseLanguages"><span class="h6">{{ __('general.language') }}</span></button>
 	<div class="collapse" id="collapseLanguages">
 		<div class="">
 			<!--<label class="form-check-label nowrap">col-6 col-sm-4 col-md-3 col-lg-2 
