95ffb9c30b677d75f12e0d1125da7fd1f251fbdd
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-06 14:42:44 +0100

    series

--- resources/views/layout/recommendations_genres.blade.php
+++ resources/views/layout/recommendations_genres.blade.php
@@ -1,5 +1,5 @@
 <div class="">
-	<button id="genres_button" class="btn btn-outline-secondary dropdown-toggle h6 mt-3 border-0" type="button" data-toggle="collapse" data-target="#collapseGenres"><span class="h6">{{ __('general.genre') }}</span></button>
+	<button id="genres_button" class="btn btn-outline-secondary dropdown-toggle h6 m-0 border-0" type="button" data-toggle="collapse" data-target="#collapseGenres"><span class="h6">{{ __('general.genre') }}</span></button>
 	<div class="collapse" id="collapseGenres">
 		<div>
 			<!--<label class="form-check-label">col-6 col-sm-4 col-md-3 col-lg-2 ng-repeat="genre in genres" class="d-flex flex-wrap"
