8c55787a0475eb2057f4bee95e23fb7efceb78f5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-07 13:10:16 +0100

    series

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/RedirectsUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/RedirectsUsers.php
@@ -15,6 +15,7 @@ trait RedirectsUsers
             return $this->redirectTo();
         }
 
-        return property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
+        //return property_exists($this, 'redirectTo') ? $this->redirectTo : '/home';
+        return session()->has('links') ? session('links') : '/home';
     }
 }
