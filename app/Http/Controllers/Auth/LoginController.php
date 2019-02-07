02c1d476d41cf4f7ef94a76242fee382a0d0c2ec
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-07 11:22:52 +0100

    series

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,7 +39,7 @@ class LoginController extends Controller
      * @return void
      */
     public function __construct()
-    {
+    {$this->redirectTo = url()->previous();
         $this->middleware('guest')->except('logout');
     }
 
