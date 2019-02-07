32a7f0c8ef9d3a7f21697c76b3153448837f7878
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-07 13:06:12 +0100

    series

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = session()->has('links')?session('links'):'/recommendations';
-    Session::delete('links')
+    Session::delete('links');
     /**
      * Create a new controller instance.
      *
