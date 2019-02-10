2a24a42ebefefa67a31757198927d7251c54e068
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-10 22:38:11 +0100

    series

--- resources/views/head/head_notifications.blade.php
+++ resources/views/head/head_notifications.blade.php
@@ -6,7 +6,7 @@ pass={
 	"api_key":"{{config('constants.api_key')}}",
 	"tt_navbar":{{ Auth::User()->tt_navbar }},
 	"watched_movie_number":{{ $watched_movie_number }},
-	"notifications":{!! print($notifications) !!}
+	"notifications":{!! json_encode($notifications) !!}
 };
 </script>
 @endsection
