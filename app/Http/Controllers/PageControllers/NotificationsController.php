ba4981e0a9e97e8ea3719dc94f57321a7a589b3b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-11 22:07:21 +0100

    series

--- app/Http/Controllers/PageControllers/NotificationsController.php
+++ app/Http/Controllers/PageControllers/NotificationsController.php
@@ -8,6 +8,7 @@ use App\Model\Rated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Response;
 
 class NotificationsController extends Controller
 {
