bdf95df2977ad0cb42a6b77b1f0b8883006056e2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-11 14:36:21 +0100

    series

--- resources/views/notifications.blade.php
+++ resources/views/notifications.blade.php
@@ -8,12 +8,12 @@
 <div class="mt-3">
     <div ng-repeat="notification in page_variables.notifications">
         <div class="d-flex py-2 pl-2 align-items-center background-lightgrey">
-            <div class="flex-grow-1" ng-if="notification.mode==0 || notification.mode==1">
+            <div class="flex-grow-1" ng-if="notification.data[0].mode==0 || notification.data[0].mode==1">
                 <span class="text-success"><i class="fas fa-heart"></i></span>
                 <span class="ml-2">@{{notification.total}} users liked your <a ng-href="@{{notification.data[0].review_mode==1?'movie':'series'}}/@{{notification.data[0].movie_id}}" class="text-dark">@{{notification.data[0].title}} @{{notification.data[0].release_date.length>0?'('+notification.data[0].release_date.substring(0, 4)+')':''}} review</a>. @{{notification.total>3?'Last 3':notification.total}} likes: <span ng-repeat="item in notification.data"><span ng-hide="$index==0">, </span>@{{item.user_name}}</span></span>
             </div>
             <div class="ml-2">
-                <button class="btn addban"><i class="fa-circle" ng-class="notification.is_seen?'fas':'far'"></i></button>
+                <button class="btn addban"><i class="fa-circle" ng-class="notification.data[0].is_seen?'fas':'far'"></i></button>
             </div>
         </div>
         <hr class="m-0">
