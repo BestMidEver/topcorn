85489cb59acb50fbcf2733f4f442d02bd090e474
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-12 12:53:36 +0100

    series

--- resources/views/layout/share_modal.blade.php
+++ resources/views/layout/share_modal.blade.php
@@ -13,11 +13,6 @@
 					</div>
 				</form>
 			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-outline-danger" data-dismiss="modal" ng-if="page_variables.is_with_review" ng-click="delete_review(page_variables.this_review_id)">{{ __('general.delete') }}</button>
-				<button type="button" class="btn btn-outline-secondary" data-dismiss="modal">{{ __('general.close') }}</button>
-				<button type="button" class="btn btn-outline-primary" data-dismiss="modal" ng-click="save_review()">{{ __('general.save') }}</button>
-			</div>
 		</div>
 	</div>
 </div>
