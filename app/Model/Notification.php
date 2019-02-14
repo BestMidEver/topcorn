9d723031e08e084479fc08dd49ceb05c66256309
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-14 14:42:41 +0100

    series

--- app/Model/Notification.php
+++ app/Model/Notification.php
@@ -14,4 +14,18 @@ class Notification extends Model
     	'multi_id',
     	'is_seen'
     ];
+
+    
+    public static function get_notification_button(){
+        if(Auth::check()){
+            $notifications = DB::table('notifications')
+            ->where('notifications.is_seen', '=', 0);
+
+            if(Auth::id()!=7) $notifications = $notifications->where('notifications.user_id', Auth::id());
+
+            $notifications = $notifications->count();
+
+            return $notifications;
+        }else return 0;
+    }
 }
