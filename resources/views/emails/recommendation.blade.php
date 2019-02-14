0bd676e9fcb0da3b849ef85215e74bdcb37d757c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-14 20:39:34 +0100

    series

--- resources/views/emails/recommendation.blade.php
+++ resources/views/emails/recommendation.blade.php
@@ -3,11 +3,11 @@
 
 Someone recommended {{$title}} to you.
 
-@component('mail::button', ['url' => '{{ URL::to("/") }}{{$mode}}/{{$movie_id}}', 'color' => 'green'])
+@component('mail::button', ['url' => 'https://topcorn.io/{{$mode}}/{{$movie_id}}', 'color' => 'green'])
 Check it
 @endcomponent
 
-<small>If you don't want to receive emails from us, please click <a href="https://topcorn.io"></a> and change your settings.</small>
+<small>If you don't want to receive emails from us, please click <a href="https://topcorn.io/account"></a> and change your settings.</small>
 
 Thanks,<br>
 {{ config('app.name') }}
