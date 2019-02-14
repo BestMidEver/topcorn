59dc1c3cc9637e8928331b6f2227f4f6196c5f88
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-14 19:23:57 +0100

    series

--- resources/views/emails/deneme.blade.php
+++ resources/views/emails/deneme.blade.php
@@ -1,11 +1,13 @@
 @component('mail::message')
-# Introduction
+# Hello!
 
 mekk mesterr "watch together" with you. You can use share button to recommend movies and series to this user.
 
 @component('mail::button', ['url' => 'https://laravel.com/docs/5.5/mail#generating-markdown-mailables', 'color' => 'green'])
 Button Text
 @endcomponent
+
+mekk mesterr "watch together" with you. You can use share button to recommend movies and series to this user.
 
 Thanks,<br>
 {{ config('app.name') }}
