caaf216729644926958fc8382bf1e77cba9e5db5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-14 18:54:40 +0100

    series

--- resources/views/email/notification.blade.php
+++ resources/views/email/notification.blade.php
@@ -1,4 +1,14 @@
-:D aldın mı?
-<div class="alert alert-danger">
-{{$var}}
-</div>
+<html>
+    <head>
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @section('sidebar')
+            This is the master sidebar.
+        @show
+        
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
