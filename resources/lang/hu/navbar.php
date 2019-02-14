a08c39fde654e39b1cd3232077f99fc8f4a30953
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-14 15:39:33 +0100

    series

--- resources/lang/hu/navbar.php
+++ resources/lang/hu/navbar.php
@@ -3,10 +3,10 @@
 return [
     'home' => 'Főoldal',
     'search' => 'Keresés',
-    'movie_person_user_search' => 'Film/Személy/Felhasználó Keresés',
+    'movie_person_user_search' => 'Keresés',
     'sequentialvote' => 'Gyors Szavazás',
     'recommendations' => 'Ajánlások',
-    'movie_recommendations' => 'Film Ajánlások',
+    'movie_recommendations' => 'Ajánlások',
     'watchlater' => 'Később megnézem',
     'profile' => 'Profilom',
     'register' => 'Regisztráció',
@@ -16,6 +16,7 @@ return [
     'faq_long' => 'Gyakran Ismételt Kérdések',
     'login' => 'Bejelentkezés',
     'logout' => 'Kijelentkezés',
+    'notifications' => 'Notifications',////////////////
     'donation' => 'Adomány',
     'privacy' => 'Adatvédelem',
     'like_us_on_facebook' => 'Kedvelj minket Facebook-on!', ////////////////////
