ccb6e43dce11ed4383b684e1abd38eff0bd4571c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-16 11:04:48 +0100

    series

--- resources/views/head/head_series.blade.php
+++ resources/views/head/head_series.blade.php
@@ -67,5 +67,11 @@ Watch trailer of series {{$series_name}} {{$series_year}}. Read summary, reviews
 @endsection
 
 @section('adsense')
-
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5818851352711866",
+    enable_page_level_ads: true
+  });
+</script>
 @endsection
