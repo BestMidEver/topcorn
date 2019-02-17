56e26a3d4b457512c8c9687f718a0a3345004a43
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-17 22:08:47 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -149,11 +149,11 @@
 	<div class="col-12 col-md-3 col-lg-3">
 		<img ng-src="{{config('constants.image.movie_card')[$image_quality]}}@{{movie.poster_path}}" on-error-src="{{config('constants.image.movie_card_error')}}" class="card-img-top d-none d-md-inline" alt="Responsive image">
 	</div>
+	<input type="hidden" name="IL_IN_ARTICLE">
 	<div class="col-12 col-md-9 col-lg-6">
 		<div class="container-fluid">
 			<p class="h6 pt-3 pt-md-0"><span data-toggle="tooltip" data-placement="top" data-original-title="@{{movie.release_date}}" ng-if="movie.release_date.length>0">@{{movie.release_date.substring(0, 4)}}</span> <span class="text-muted" ng-if="movie.genres.length > 0 && movie.release_date.length>0">•</span> <span ng-repeat="genre in movie.genres"><span ng-if="$index!=0">, </span>@{{genre.name}}</span></p>
 			<div class="pt-2" ng-if="movie.overview.length > 0 && movie.overview != 'No overview found.'"><p>@{{movie.overview}}</p></div>
-			<input type="hidden" name="IL_IN_ARTICLE">
 			<div ng-if="directors.length > 0">
 				<div class="h6 pt-1"><span ng-if="directors.length == 1">{{ __('general.director') }}</span><span ng-if="directors.length > 1">{{ __('general.directors') }}</span></div>
 				<p><span class="d-inline" ng-repeat="director in directors"><span ng-if="$index!=0">, </span><a href="/person/@{{director.id}}" target={{$target}} class="text-dark">@{{director.name}}</a></span></p>
