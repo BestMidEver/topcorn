55e0322c00945e49f3e6124fc41c639230d819b3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-21 19:28:24 +0100

    series

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -125,6 +125,7 @@
 	<div class="d-flex flex-column mt-1 mt-md-1 px-0 col-12 col-md-auto fa22 ml-auto">
 		<div class="d-flex flex-row justify-content-between text-center">
 			@if(Auth::check())
+			<button type="button" class="btn btn-outline-secondary btn-sm btn-block border-0 mt-0 px-lg-4 addlater" ng-class="{'text-warning':false}" ng-click=""><div><span ng-show="true"><i class="fa fa-plus"></i></span><span ng-show="false"><i class="far fa-clock"></i></span></div><span class="scrollmenu">{{ __('general.watch_together') }}</span></button>
 			<button type="button" class="btn btn-outline-secondary btn-sm btn-block border-0 mt-0 px-lg-4 addlater" ng-class="{'text-warning':false}" ng-click=""><div><span ng-show="true"><i class="fas fa-user-friends"></i></span><span ng-show="false"><i class="far fa-clock"></i></span></div><span class="scrollmenu">{{ __('general.follow') }}</span></button>
 			@endif
 		</div>
