428523fe319f20f4f9ae5f95d1438cdaf717186b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-21 19:26:31 +0100

    series

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -126,17 +126,7 @@
 		<div class="d-flex flex-row justify-content-between text-center">
 			@if(Auth::check())
 			<button type="button" class="btn btn-outline-secondary btn-sm btn-block border-0 mt-0 px-lg-4 addlater" ng-class="{'text-warning':false}" ng-click=""><div><span ng-show="true"><i class="fas fa-user-friends"></i></span><span ng-show="false"><i class="far fa-clock"></i></span></div><span class="scrollmenu">{{ __('general.follow') }}</span></button>
-			<button type="button" class="btn btn-outline-secondary btn-sm btn-block border-0 mt-0 px-lg-4 addban" ng-click="open_share_modal()"><div><i class="fas fa-share"></i></div>{{ __('general.share') }}</button>
 			@endif
-			@if(Auth::guest())
-			<a ng-href="{{config('constants.facebook.share_website')}}/movie/{{$id}}" target="_blank" class="btn btn-outline-secondary btn-sm btn-block border-0 mt-0 px-lg-4 addfacebook"
-			@if(Auth::check())
-			@if(Auth::User()->tt_movie < 50)
-            data-toggle="popover" data-placement="bottom" title='{{ __("tutorial.hint") }}<a class="close tooltip-x" href="#close-tooltip">&times;</a>' id="share"
-            @endif
-            @endif
-            ><div><i class="fas fa-share"></i></div>{{ __('general.share') }}</a>
-            @endif
 		</div>
 	</div>
 </div>
