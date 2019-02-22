f9c858b0ebc2d90b2404697e226866e879200950
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-22 16:24:11 +0100

    series

--- app/Http/Controllers/ApiControllers/FollowController.php
+++ app/Http/Controllers/ApiControllers/FollowController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Jobs\SendNotificationEmailJob;
 use App\Model\Follow;
 use App\Model\Notification;
+use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
