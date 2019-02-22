9c93c94205b3026b0d0827e0fd11c42a8cc8ef37
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-22 10:05:56 +0100

    series

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -319,10 +319,12 @@ else $theme='';
     </div>
 </div>
 @endif
+@if(Auth::id()>7)
 <script type="text/javascript">
 var infolinks_pid = 3165314;
 var infolinks_wsid = 0;
 </script>
 <script type="text/javascript" src="//resources.infolinks.com/js/infolinks_main.js"></script>
+@endif
 </body>
 </html>
