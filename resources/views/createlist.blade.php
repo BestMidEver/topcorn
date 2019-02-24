8246b5ce65dbf9f7e0d3db377e0417fc3f8b83a3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-24 13:18:41 +0100

    series

--- resources/views/createlist.blade.php
+++ resources/views/createlist.blade.php
@@ -171,8 +171,8 @@
 							
 							<div class="input-group col mt-3 mt-md-0">
 								<div class="input-group-prepend pl-md-3">
-									<button class="btn btn-outline-secondary" ng-class="{'active':movie.mode==0}" ng-click="movie.mode=0;search_movie(movie)" type="button">Movies</button>
-									<button class="btn btn-outline-secondary" ng-class="{'active':movie.mode!=0}" ng-click="movie.mode=1;search_movie(movie)" type="button">Series</button>
+									<button class="btn btn-outline-secondary" ng-class="{'active':movie.mode==0}" ng-click="movie.mode=0;search_movie(movie);set_focus($index)" type="button">Movies</button>
+									<button class="btn btn-outline-secondary" ng-class="{'active':movie.mode!=0}" ng-click="movie.mode=1;search_movie(movie);set_focus($index)" type="button">Series</button>
 								</div>
 								<input ng-attr-id="vitrin_@{{$index}}" ng-show="!movie.searchmode" type="text" class="form-control" ng-click="movie.searchmode=true;set_focus($index)" ng-init="movie.movie_title=movie.movie_title+(movie.release_date.length > 0 ? ' ('+movie.release_date.substring(0, 4)+')' : '')" ng-model="movie.movie_title" placeholder="{{ __('general.enter_movie_name') }}">
 								<input ng-attr-id="movie_id_@{{$index}}" ng-show="false" type="text" class="form-control" ng-model="movie.movie_id" name="items[@{{$index}}][1]">
