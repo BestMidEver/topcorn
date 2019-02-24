7bb171421c86894d1ca57201ba1b535eee383071
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-24 22:11:37 +0100

    series

--- public/js/controllers/ListPageController.js
+++ public/js/controllers/ListPageController.js
@@ -469,17 +469,25 @@ MyApp.controller('ListPageController', function($scope, $http, rate, $window)
 
 	$scope.ban=function(index)
 	{
-		console.log(index)
+		if($scope.movies[index].mode == 0){
+			f1 = 'add_ban';
+			f2 = 'un_ban';
+			v1 = 'ban_id';
+		}else{
+			f1 = 'series_add_ban';
+			f2 = 'series_un_ban';
+			v1 = 'id';
+		}
 		if($scope.movies[index].ban_id == null){
-			rate.add_ban($scope.movies[index].id)
+			rate[f1]($scope.movies[index].id)
 			.then(function(response){
 				console.log(response);
 				if(response.status == 201){
-					$scope.movies[index].ban_id=response.data.data.ban_id;
+					$scope.movies[index].ban_id=response.data.data[v1];
 				}
 			});
 		}else{
-			rate.un_ban($scope.movies[index].ban_id)
+			rate[f2]($scope.movies[index].ban_id)
 			.then(function(response){
 				console.log(response);
 				if(response.status == 204 || response.status == 404){
