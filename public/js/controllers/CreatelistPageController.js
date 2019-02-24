aad5de6004eae24a1a2d9985d75c964dba5476d4
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-02-24 13:32:33 +0100

    series

--- public/js/controllers/CreatelistPageController.js
+++ public/js/controllers/CreatelistPageController.js
@@ -27,7 +27,7 @@ MyApp.controller('CreatelistPageController', function($scope, $http, $timeout, r
 
 	$scope.choose_movie = function(index, movie){
 		console.log(index, movie);
-		rate.suck_movie(movie.id, movie.mode);
+		rate.suck_movie_series(movie.id, movie.mode);
 		$scope.movies[index].movie_id=movie.id;
 		$scope.movies[index].searchmode=false;
 		$scope.movies[index].overview=movie.overview;
