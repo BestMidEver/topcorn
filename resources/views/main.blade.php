3e9ef2fd16f54f0e4c349900b78074301dadb96b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-01 16:16:26 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,12 +13,12 @@
 </div>
 <div class="dropdown d-inline pl-3">
     <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap mr-2 mt-3 mt-md-4 py-0 px-md-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
-    <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Newest</span>
+    <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Now Playing</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Legendary</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Garbage</span>
     </button>
     <div class="dropdown-menu">
-        <button class="dropdown-item" ng-click="page_variables.active_tab_1=0;">Newest</button>
+        <button class="dropdown-item" ng-click="page_variables.active_tab_1=0;">Now Playing</button>
         <button class="dropdown-item" ng-click="page_variables.active_tab_1=1;">Legendary</button>
         <button class="dropdown-item" ng-click="page_variables.active_tab_1=2;">Garbage</button>
     </div>
