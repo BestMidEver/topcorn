00aa1f4bf2b0e4bffb28223502203ba46d0a5494
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-01 15:38:15 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,13 +12,13 @@
 	Lists: most liked | last added | last liked
 </div>
 <div class="dropdown d-inline mr-2">
-    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle px-3 px-md-0 border-0 background-inherit nowrap" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle px-3 px-md-0 border-0 background-inherit nowrap" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
     <span class="h5" ng-show="page_variables.active_tab_1==0">now playing</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">most green</span>
     </button>
     <div class="dropdown-menu">
-        <button class="dropdown-item" ng-click="page_variables.active_tab_1=3;set_recommendations();" ng-if="movie.belongs_to_collection">now playing</button>
-        <button class="dropdown-item" ng-click="page_variables.active_tab_1=0;set_recommendations();" ng-show="page_variables.has_recommendation">most green</button>
+        <button class="dropdown-item" ng-click="page_variables.active_tab_1=0;set_recommendations();">now playing</button>
+        <button class="dropdown-item" ng-click="page_variables.active_tab_1=1;set_recommendations();">most green</button>
     </div>
 </div>
 @endsection
