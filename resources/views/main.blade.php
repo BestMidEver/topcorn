2b02fd3380d20fcbf5c090a44b62fe9bb77df1fa
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-01 16:11:00 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,7 +11,7 @@
 	Reviews: most liked | last added | last liked
 	Lists: most liked | last added | last liked
 </div>
-<div class="dropdown d-inline pl-2">
+<div class="dropdown d-inline pl-3">
     <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap mr-2 mt-3 mt-md-4 py-0 px-md-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
     <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Newest</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Legendary</span>
