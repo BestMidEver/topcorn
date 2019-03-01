b10fa48531dc8d6bdecee3bb5596356a4a9e2305
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-01 16:07:05 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -12,7 +12,7 @@
 	Lists: most liked | last added | last liked
 </div>
 <div class="dropdown d-inline">
-    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap mr-2 mt-3 mt-md-4 col h5" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
+    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap mr-2 mt-3 mt-md-4 h5" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
     <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Newest</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Legendary</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Garbage</span>
