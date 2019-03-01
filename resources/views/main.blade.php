1208ad4fcc642993470024672f610161d6e8bfac
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-01 16:03:19 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,8 +11,8 @@
 	Reviews: most liked | last added | last liked
 	Lists: most liked | last added | last liked
 </div>
-<div class="dropdown d-inline mr-2">
-    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle px-3 px-md-0 border-0 background-inherit nowrap" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
+<div class="dropdown d-inline mr-2 mt-3 mt-md-4">
+    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
     <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Newest</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Legendary</span>
     <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Garbage</span>
