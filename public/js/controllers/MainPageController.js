15ca88dbe4057ed442f481521af2fde8bcf755d2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-02 14:57:10 +0100

    series

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -20,7 +20,7 @@ MyApp.controller('MainPageController', function($scope, $http, $anchorScroll, ra
 		switch(mode) {
 			case 1:
 					console.log(mode);
-				rate.get_now_playing(pass.constants_api_key, pass.lang, pass.lang=='en'?'us':(pass.lang=='tr'?'tr':'hu'), $scope.page_1)
+				rate.get_now_playing(pass.api_key, pass.lang, pass.lang=='en'?'us':(pass.lang=='tr'?'tr':'hu'), $scope.page_1)
 				.then(function(response){
 					console.log(response);
 				});
