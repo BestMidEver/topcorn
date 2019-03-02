873ad614fdd0178b1e3dd89a69a0cfb45a119183
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-02 21:04:29 +0100

    series

--- public/js/functions/rate_by_index.js
+++ public/js/functions/rate_by_index.js
@@ -126,7 +126,7 @@ MyApp.factory('rate', function($http) {
     {
         return $http({
 			method: 'GET',
-			url: '/api/get_notifications/'+page_mode+'/'+page,
+			url: '/api/get_notifications/'+page_mode+'?page='+page,
 			headers: {
 				'Content-Type': 'application/json',
 				'Accept' : 'application/json'
