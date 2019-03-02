31d62191bf33daab22653340421d24bb4f18ebbf
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-02 19:25:39 +0100

    series

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -12,7 +12,7 @@ MyApp.controller('MainPageController', function($scope, $http, $anchorScroll, ra
 //////////////////////////////////////////////////////////////////////////////////////////
 
 	console.log(pass.users)
-	$scope.users = pass.users;
+	$scope.users = pass.users.data;
 	$scope.page_variables={};
 	$scope.page_1=0;
 	$scope.page_2=0;
