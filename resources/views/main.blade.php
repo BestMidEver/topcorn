c26a955a2cab957d5634fb99fcd8264fd3a9236e
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-02 15:22:55 +0100

    series

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -16,7 +16,7 @@
 	    <button class="btn btn-lg btn-outline-dark text-dark dropdown-toggle border-0 background-inherit nowrap mr-2 mt-3 mt-md-4 py-0 px-md-0" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" ng-init="page_variables.active_tab_1=0">
 	    <span class="h5" ng-show="page_variables.active_tab_1==0">Movies: Now Playing</span>
 	    <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Legendary</span>
-	    <span class="h5" ng-if="page_variables.active_tab_1==1">Movies: Garbage</span>
+	    <span class="h5" ng-if="page_variables.active_tab_1==2">Movies: Garbage</span>
 	    </button>
 	    <div class="dropdown-menu">
 	        <button class="dropdown-item" ng-click="page_variables.active_tab_1=0;">Now Playing</button>
