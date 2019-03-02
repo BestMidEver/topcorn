fa2156fa81b51d57c55183d282a3b20ae988aaa1
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-02 16:29:06 +0100

    series

--- resources/views/layout/peoplecard_6.blade.php
+++ resources/views/layout/peoplecard_6.blade.php
@@ -22,21 +22,21 @@
 						<a href="/person/@{{person.id}}" target={{$target}}>
 							<img class="card-img-top" ng-src="{{config('constants.image.movie_card')[$image_quality]}}@{{person.profile_path}}" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
 							<div class="card-block text-center">
-								<h6 class="card-title px-1 pt-1 text-dark" ng-if="person.name.length > 0">@{{people{{  $suffix  }}.length}}</h6>
+								<h6 class="card-title px-1 pt-1 text-dark" ng-if="person.name.length > 0">@{{person.name}}</h6>
 							</div>
 						</a>
-						<div class="card-title px-1 text-muted text-center mb-0"><small ng-if="person.character.length > 0">@{{person.character}}</small></div>
+						<!-- <div class="card-title px-1 text-muted text-center mb-0"><small ng-if="person.character.length > 0">@{{person.character}}</small></div> -->
 					</div>
 				</div>
 			</div>
 		</div>
-		<div ng-if="people{{  $suffix  }}.length > 6">
-			<div class="text-center pt-1" ng-hide="iscast{{  $suffix  }}">
-				<button class="btn btn-outline-secondary border-0 text-muted hover-white" ng-click="iscast{{  $suffix  }} = true;" data-toggle="collapse" data-target="#collapseCast{{  $suffix  }}"><small>{{ __('general.show_everyone') }}</small></button>
-			</div>
-			<div class="text-center pt-1" ng-show="iscast{{  $suffix  }}">
-				<button class="btn btn-outline-secondary btn-lg fa40 border-0 text-muted hover-white" ng-click="iscast{{  $suffix  }} = false;" data-toggle="collapse" data-target="#collapseCast{{  $suffix  }}"><i class="fa fa-angle-up"></i></button>
-			</div>
+	</div>
+	<div ng-if="people{{  $suffix  }}.length > 6">
+		<div class="text-center pt-1" ng-hide="iscast{{  $suffix  }}">
+			<button class="btn btn-outline-secondary border-0 text-muted hover-white" ng-click="iscast{{  $suffix  }} = true;" data-toggle="collapse" data-target="#collapseCast{{  $suffix  }}"><small>{{ __('general.show_everyone') }}</small></button>
+		</div>
+		<div class="text-center pt-1" ng-show="iscast{{  $suffix  }}">
+			<button class="btn btn-outline-secondary btn-lg fa40 border-0 text-muted hover-white" ng-click="iscast{{  $suffix  }} = false;" data-toggle="collapse" data-target="#collapseCast{{  $suffix  }}"><i class="fa fa-angle-up"></i></button>
 		</div>
 	</div>
 </div>
