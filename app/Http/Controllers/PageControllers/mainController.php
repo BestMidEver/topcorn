e4408d12e40315223c8201429acf5e30c0b7a2ce
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-03 21:03:40 +0100

    series

--- app/Http/Controllers/PageControllers/mainController.php
+++ app/Http/Controllers/PageControllers/mainController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Rated;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
