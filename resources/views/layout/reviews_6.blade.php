2d437956189ae06496d28ca9b6befe69179c460d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-03 18:43:22 +0100

    series

--- resources/views/layout/reviews_6.blade.php
+++ resources/views/layout/reviews_6.blade.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-	<div class="py-4" ng-repeat="review in reviews{{ $suffix }} | limitTo:6">
+	<div class="py-4" ng-repeat="review in reviews{{ $suffix }} | limitTo:3">
         <div class="d-flex justify-content-between">
             <div class="h6 pb-2" ng-if="review.author.length>0">@{{review.author}}</div>
             <div class="d-inline" ng-if="review.name.length>0"><a class="h6 pb-2 text-dark d-inline" ng-href="/@{{review.mode==1?'movie':'series'}}/@{{review.movie_series_id}}" data-toggle="tooltip" data-placement="top" title="@{{review.original_title}}">@{{review.movie_title}} <small class="text-muted" ng-if="review.release_date.length > 0"><em>(@{{review.release_date.substring(0, 4)}})</em></small></a> • <a class="h6 pb-2 text-dark d-inline" ng-href="/profile/@{{review.user_id}}">@{{review.name}}</a> <span class="ml-2" ng-if="review.rate>0"><i class="fas fa-star" ng-class="{1:'text-danger', 2:'text-warning', 3:'text-secondary', 4:'text-info', 5:'text-success'}[review.rate]" ng-repeat="n in [] | range:review.rate"></i><i class="far fa-star text-muted" ng-repeat="n in [] | range:(5-review.rate)"></i></span></div>
@@ -36,8 +36,8 @@
 	</div>
 </div>
 <div class="collapse container-fluid" id="collapseMovies{{ $suffix }}">
-	<div ng-if="reviews{{ $suffix }}.length > 6">
-    	<div class="py-4" ng-repeat="review in reviews{{ $suffix }} | limitTo:100:6">
+	<div ng-if="reviews{{ $suffix }}.length > 3">
+    	<div class="py-4" ng-repeat="review in reviews{{ $suffix }} | limitTo:100:3">
             <div class="d-flex justify-content-between">
                 <div class="h6 pb-2" ng-if="review.author.length>0">@{{review.author}}</div>
                 <div class="d-inline" ng-if="review.name.length>0"><a class="h6 pb-2 text-dark d-inline" ng-href="/@{{review.mode==1?'movie':'series'}}/@{{review.movie_series_id}}" data-toggle="tooltip" data-placement="top" title="@{{review.original_title}}">@{{review.movie_title}} <small class="text-muted" ng-if="review.release_date.length > 0"><em>(@{{review.release_date.substring(0, 4)}})</em></small></a> • <a class="h6 pb-2 text-dark d-inline" ng-href="/profile/@{{review.user_id}}">@{{review.name}}</a> <span class="ml-2" ng-if="review.rate>0"><i class="fas fa-star" ng-class="{1:'text-danger', 2:'text-warning', 3:'text-secondary', 4:'text-info', 5:'text-success'}[review.rate]" ng-repeat="n in [] | range:review.rate"></i><i class="far fa-star text-muted" ng-repeat="n in [] | range:(5-review.rate)"></i></span></div>
@@ -47,9 +47,9 @@
                     </button>
                 </div>
             </div>
-            <div id="@{{'accordion'+($index+6)}}">
+            <div id="@{{'accordion'+($index+3)}}">
                 <div ng-if="review.id == 'long'">
-                    <div id="@{{'collapse'+($index+6)+'a'}}" data-parent="@{{'#accordion'+($index+6)}}" class="lead lead-small collapse">
+                    <div id="@{{'collapse'+($index+3)+'a'}}" data-parent="@{{'#accordion'+($index+3)}}" class="lead lead-small collapse">
                         <div>
                             <div ng-bind-html="review.content"></div>
                         </div>
@@ -59,13 +59,13 @@
                     </div>
                 </div>
                 <div>
-                    <div id="@{{'collapse'+($index+6)+'b'}}" data-parent="@{{'#accordion'+($index+6)}}" class="lead lead-small collapse show">
+                    <div id="@{{'collapse'+($index+3)+'b'}}" data-parent="@{{'#accordion'+($index+3)}}" class="lead lead-small collapse show">
                         <div>
                             <div ng-bind-html="review.url"></div>
                         </div>
                         <div ng-if="review.id == 'long'">
                             <div class="text-center pt-1">
-                                <button class="btn btn-outline-secondary border-0 text-muted hover-white showreview" data-toggle="collapse" data-target="@{{'#collapse'+($index+6)+'a'}}" aria-expanded="false"><small>{{ __('general.read_all') }}</small></button>
+                                <button class="btn btn-outline-secondary border-0 text-muted hover-white showreview" data-toggle="collapse" data-target="@{{'#collapse'+($index+3)+'a'}}" aria-expanded="false"><small>{{ __('general.read_all') }}</small></button>
                             </div>
                         </div>
                     </div>
@@ -77,9 +77,9 @@
 <div ng-show="iscast_movies{{  $suffix  }}">
 @include('layout.pagination', ['suffix' => '_'.$suffix])
 </div>
-<div class="text-center pt-1" ng-hide="iscast_movies{{ $suffix }} || !(reviews{{ $suffix }}.length>6)">
+<div class="text-center pt-1" ng-hide="iscast_movies{{ $suffix }} || !(reviews{{ $suffix }}.length>3)">
     <button class="btn btn-outline-secondary border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = true;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><small>{{__('general.show_all')}}</small></button>
 </div>
-<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && reviews{{ $suffix }}.length>6">
+<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && reviews{{ $suffix }}.length>3">
     <button class="btn btn-outline-secondary btn-lg fa40 border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = false;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><i class="fa fa-angle-up"></i></button>
 </div>
