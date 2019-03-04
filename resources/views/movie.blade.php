fc08fc1b171e4291b21fcdc983a4f976f7111f93
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-04 13:07:28 +0100

    series

--- resources/views/movie.blade.php
+++ resources/views/movie.blade.php
@@ -317,9 +317,15 @@
         </div>
     </div>
     <span class="text-muted scrollmenu px-3 px-md-0" ng-if="page_variables.active_tab_3!=2 && page_variables.active_tab_3!=3"><small>Sorted by relevance</small></span>
-	<a href="/createlist/new" class="btn btn-outline-success" ng-if="page_variables.active_tab_3==2" target="{{$target}}">
+	<a href="/createlist/new" class="btn btn-outline-success d-none" ng-if="page_variables.active_tab_3==2" target="{{$target}}">
 		<i class="fas fa-plus"></i> {{ __('general.create_list') }}
 	</a>
+	<div class="h5 px-3 px-md-0 mb-0 d-flex justify-content-between">
+		<span class="mb-0 pr-2 align-middle mt-3 d-none">{{ __('general.reviews') }}</span>
+		@if(Auth::check())
+		<button href="/createlist/new" class="btn btn-outline-secondary addblack border-0" ng-if="page_variables.active_tab_3==2" target="{{$target}}"><div><i class="fas fa-plus"></i></div> <span>{{ __('general.create_list') }}</span></button>
+        @endif
+	</div>
 	<div ng-show="page_variables.active_tab_3!=2">
     @include('layout.moviecard_6', ['suffix' => ''])
 	</div>
