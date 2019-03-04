ad248375e05e142b85ff0587711f5af90587f44d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-04 18:49:48 +0100

    series

--- app/Http/Controllers/PageControllers/mainController.php
+++ app/Http/Controllers/PageControllers/mainController.php
@@ -24,14 +24,7 @@ class mainController extends Controller
         ->leftjoin('users', 'users.id', '=', 'rateds.user_id')
         ->select(
             'movies.id',
-            'movies.original_title as original_title',
-            'movies.vote_average',
-            'movies.vote_count',
-            'movies.release_date',
-            'movies.'.App::getlocale().'_title as title',
-            'movies.'.App::getlocale().'_poster_path as poster_path',
             DB::raw('MAX(rateds.updated_at) as updated_at'),
-            //'rateds.updated_at',
             'rateds.rate',
             DB::raw('LEFT(users.name , 25) AS last_voter_name')
         )
@@ -57,6 +50,18 @@ class mainController extends Controller
                 $join->on('movies.id', '=', 'ss.id')
                 ->addBinding($subq->getBindings());  
             }
+        )
+        ->select(
+            'ss.id',
+            'movies.original_title as original_title',
+            'movies.vote_average',
+            'movies.vote_count',
+            'movies.release_date',
+            'movies.'.App::getlocale().'_title as title',
+            'movies.'.App::getlocale().'_poster_path as poster_path',
+            'ss.updated_at',
+            'ss.rate',
+            'ss.last_voter_name',
         )
         ->groupBy('ss.id');
 
