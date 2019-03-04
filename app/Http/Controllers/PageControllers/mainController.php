843b64a69af5ec5989e5c0b12b75a0b2512da7a5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-04 19:02:42 +0100

    series

--- app/Http/Controllers/PageControllers/mainController.php
+++ app/Http/Controllers/PageControllers/mainController.php
@@ -30,23 +30,23 @@ class mainController extends Controller
             'movies.release_date',
             'movies.'.App::getlocale().'_title as title',
             'movies.'.App::getlocale().'_poster_path as poster_path',
-            DB::raw('MAX(rateds.updated_at) as updated_at'),
-            //'rateds.updated_at',
+            //DB::raw('MAX(rateds.updated_at) as updated_at'),
+            'rateds.updated_at',
             'rateds.rate',
             DB::raw('LEFT(users.name , 25) AS last_voter_name')
         )
-        ->groupBy('movies.id')
+        //->groupBy('movies.id')
         ->orderBy('rateds.updated_at', 'desc');
 
         $qqSql = $subq->toSql();
 
         /////////////////////////////////////////////////////////
 
-        $movies = DB::table('rateds')
+        $movies = DB::table('movies')
         ->join(
             DB::raw('(' . $qqSql. ') as ss'),
             function($join) use ($subq) {
-                $join->on('rateds.updated_at', '=', 'ss.updated_at')
+                $join->on('movies.id', '=', 'ss.id')
                 ->addBinding($subq->getBindings());  
             }
         )
