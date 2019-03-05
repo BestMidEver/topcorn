46b6c203658840fa4a2b4015d57eb72e1c9c21f2
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 12:48:22 +0100

    series

--- resources/views/layout/moviecard_6.blade.php
+++ resources/views/layout/moviecard_6.blade.php
@@ -18,6 +18,6 @@
 <div class="text-center pt-1" ng-hide="iscast_movies{{ $suffix }} || !(similar_movies{{ $suffix }}.length>6) || is_expanded{{ $suffix }}">
     <button class="btn btn-outline-secondary border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = true;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><small>{{__('general.show_all')}}</small></button>
 </div>
-<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && similar_movies{{ $suffix }}.length>6" ng-hide="is_expanded{{ $suffix }}">
+<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && similar_movies{{ $suffix }}.length>6" ng-hide="false">
     <button class="btn btn-outline-secondary btn-lg fa40 border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = false;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><i class="fa fa-angle-up"></i></button>
 </div>
