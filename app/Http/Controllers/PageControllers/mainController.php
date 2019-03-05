d206ba2944c0887dee71e81bda12829bbaf48cd3
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 20:01:04 +0100

    series

--- app/Http/Controllers/PageControllers/mainController.php
+++ app/Http/Controllers/PageControllers/mainController.php
@@ -434,7 +434,7 @@ class mainController extends Controller
         ->leftjoin('rateds', function ($join) {
             $join->on('rateds.user_id', '=', 'follows.object_id');
         })
-        ->where('rateds.rate', '=', 5)
+        ->where('rateds.rate', '=', 6)
         ->count();
 
         $is_following2 = DB::table('follows')
