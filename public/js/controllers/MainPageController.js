2f37d4849b58f2d95c8849a14a9838a177655fd5
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 22:35:41 +0100

    series

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -463,7 +463,6 @@ MyApp.controller('MainPageController', function($scope, $http, $anchorScroll, ra
 
 	$scope.votemodal=function(index, movie)
 	{
-		console.log(index, movie)
 		$scope.active_tab = movie.title.length>0?'movie':'series';
 		$scope.modalmovie = movie;
 		$scope.modalmovie.index = index;
