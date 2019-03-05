fa674642012d2df8be32d24367b0d7ac744098db
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 19:55:47 +0100

    series

--- app/Http/Controllers/PageControllers/mainController.php
+++ app/Http/Controllers/PageControllers/mainController.php
@@ -445,8 +445,8 @@ class mainController extends Controller
         })
         ->count();
         
-        $movies = $this->get_legendary_garbage_movies(5, $is_following>0?'following':'all', 'newest');
-        $series = $this->get_legendary_garbage_series(5, $is_following>0?'following':'all', 'newest');
+        $movies = $this->get_legendary_garbage_movies(5, $is_following1>0?'following':'all', 'newest');
+        $series = $this->get_legendary_garbage_series(5, $is_following2>0?'following':'all', 'newest');
         $people = $this->get_popular_people('born today');
         $users = $this->get_popular_users('comment');
         $reviews = $this->get_popular_reviews('newest');
