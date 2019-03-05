9e335f6d0a59bdb9e6a8bb7c56a0e9057b769fba
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 12:46:27 +0100

    series

--- resources/views/layout/moviecard_6.blade.php
+++ resources/views/layout/moviecard_6.blade.php
@@ -15,9 +15,9 @@
 <div ng-show="iscast_movies{{  $suffix  }}">
 @include('layout.pagination', ['suffix' => '_'.$suffix])
 </div>
-<div class="text-center pt-1" ng-hide="iscast_movies{{ $suffix }} || !(similar_movies{{ $suffix }}.length>6)">
+<div class="text-center pt-1" ng-hide="iscast_movies{{ $suffix }} || !(similar_movies{{ $suffix }}.length>6) || is_expanded{{ $suffix }}">
     <button class="btn btn-outline-secondary border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = true;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><small>{{__('general.show_all')}}</small></button>
 </div>
-<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && similar_movies{{ $suffix }}.length>6">
+<div class="text-center pt-1" ng-show="iscast_movies{{ $suffix }} && similar_movies{{ $suffix }}.length>6" ng-hide="is_expanded{{ $suffix }}">
     <button class="btn btn-outline-secondary btn-lg fa40 border-0 text-muted hover-white" ng-click="iscast_movies{{ $suffix }} = false;" data-toggle="collapse" data-target="#collapseMovies{{ $suffix }}"><i class="fa fa-angle-up"></i></button>
 </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -19,8 +19,8 @@
 			    </div>
 			</div>
 		</div>
-		<button class="btn btn-outline-secondary addblack border-0" ng-click="page_variables.expanded=1;iscast_movies1=true;toggle_collapse('collapseMovies1', 'expand');" ng-show="page_variables.expanded==-1"><div><i class="fas fa-arrows-alt"></i></div></button>
-		<button class="btn btn-outline-secondary addblack border-0" data-toggle="tooltip" data-placement="top" title="{{ __('general.compress') }}" ng-click="page_variables.expanded=-1;iscast_movies1=false;toggle_collapse('collapseMovies1', 'collapse');" ng-show="page_variables.expanded!=-1"><div><i class="fas fa-compress-arrows-alt"></i></div></button>
+		<button class="btn btn-outline-secondary addblack border-0" ng-click="page_variables.expanded=1;iscast_movies1=true;is_expanded=true;toggle_collapse('collapseMovies1', 'expand');" ng-show="page_variables.expanded==-1"><div><i class="fas fa-arrows-alt"></i></div></button>
+		<button class="btn btn-outline-secondary addblack border-0" data-toggle="tooltip" data-placement="top" title="{{ __('general.compress') }}" ng-click="page_variables.expanded=-1;is_expanded=false;iscast_movies1=false;toggle_collapse('collapseMovies1', 'collapse');" ng-show="page_variables.expanded!=-1"><div><i class="fas fa-compress-arrows-alt"></i></div></button>
 	</div>
 	<div ng-show="similar_movies1.length>0">
     @include('layout.moviecard_6', ['suffix' => '1'])
