9f526c3406ab672eb3dcae06f93842ab5cad0c12
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-05 20:04:56 +0100

    series

--- public/js/controllers/MainPageController.js
+++ public/js/controllers/MainPageController.js
@@ -77,7 +77,7 @@ MyApp.controller('MainPageController', function($scope, $http, $anchorScroll, ra
 
 
 	$scope.constants_image_thumb_nail = pass.constants_image_thumb_nail;
-	$scope.page_variables={expanded:-1, f_following1:(parseInt(pass.following1)>0?'following':'all'), f_sort1:'newest', f_following2:(parseInt(pass.following2)>0?'following':'all'), f_sort2:'newest'};
+	$scope.page_variables={expanded:-1, f_following1:(pass.is_following1>0?'following':'all'), f_sort1:'newest', f_following2:(pass.is_following2>0?'following':'all'), f_sort2:'newest'};
 	console.log($scope.page_variables)
 	$scope.page_1=0;
 	$scope.page_2=0;
