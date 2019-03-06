360d8aca99ead88ff97ea8ddab6dab4d7f0c61d6
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-06 16:23:55 +0100

    series

--- resources/views/layout/peoplecard_6.blade.php
+++ resources/views/layout/peoplecard_6.blade.php
@@ -38,9 +38,27 @@
 				<div class="col-4 col-md-2 mt-4 px-2" ng-repeat="person in people{{  $suffix  }} | limitTo:100:6">
 					<div class="card moviecard h-100 d-flex flex-column justify-content-between">
 						<a href="/person/@{{person.id}}" target={{$target}}>
-							<img class="card-img-top" ng-src="{{config('constants.image.movie_card')[$image_quality]}}@{{person.profile_path}}" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
-							<div class="card-block text-center">
-								<h6 class="card-title px-1 pt-1 text-dark" ng-if="person.name.length > 0">@{{person.name}}</h6>
+							<div class="position-relative text-center min-height-200">
+								<img class="card-img-top darken-cover" ng-src="{{config('constants.image.movie_card')[$image_quality]}}@{{person.profile_path}}" on-error-src="{{config('constants.image.movie_card_error')}}" alt="Card image cap">
+								<div class="custom-over-layer h-50 d-flex flex-column justify-content-center">
+									<div class="d-flex flex-row justify-content-center" ng-if="person.age>0">
+										<div class="text-white">
+											<small ng-if="person.died_age>0">If lived</small>
+											<span class="d-block"><span class="h5 text-warning">@{{person.age}}</span> <small>years old</small></span>
+										</div>
+									</div>
+								</div>
+								<div class="custom-over-layer-bottom h-50 d-flex flex-column justify-content-center">
+									<div class="d-flex flex-row justify-content-center">
+										<div class="text-white" ng-if="person.died_age>0">
+											<small>When died</small>
+											<span class="d-block"><span class="h5 text-warning">@{{person.died_age}}</span> <small>years old</small></span>
+										</div>
+									</div>
+								</div>
+								<div class="card-block text-center">
+									<h6 class="card-title px-1 pt-1 text-dark">@{{person.name}}</h6>
+								</div>
 							</div>
 						</a>
 						<!-- <div class="card-title px-1 text-muted text-center mb-0"><small ng-if="person.character.length > 0">@{{person.character}}</small></div> -->
