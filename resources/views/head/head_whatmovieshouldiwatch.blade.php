366452f6981cea361e4f1a2a1d8b46f20da2247a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-08 10:00:40 +0100

    series

--- resources/views/head/head_whatmovieshouldiwatch.blade.php
+++ resources/views/head/head_whatmovieshouldiwatch.blade.php
@@ -5,3 +5,13 @@ What Movie Should I Watch? (2019)
 @section('meta_description')
 Learn what to watch tonight, based on your taste. Filter movies with original language, release year, genre and vote count. Completely free
 @endsection
+
+@section('adsense')
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-5818851352711866",
+    enable_page_level_ads: true
+  });
+</script>
+@endsection
