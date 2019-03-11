a6ee9da5ce6477e76869cc26193838081380d8df
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-11 13:19:03 +0100

    series

--- resources/views/head/head_whatmovieshouldiwatch.blade.php
+++ resources/views/head/head_whatmovieshouldiwatch.blade.php
@@ -6,6 +6,15 @@ What Movie Should I Watch? (2019)
 Learn what to watch tonight, based on your taste. Filter movies with original language, release year, genre and vote count. Completely free
 @endsection
 
+@section('og_tags')
+<meta property="og:url" content="{{url('/')}}/home"/>
+<meta property="og:title" content="What movie should I watch tonight?"/>
+<meta property="og:description" content="Learn what to watch tonight, based on your taste. Filter movies with original language, release year, genre and vote count. Completely free"/>
+<meta property="og:type" content="website"/>
+<meta property="og:image" content="/images/godfather_feeling.png"/>
+<meta property="fb:app_id" content="{{config('constants.facebook.app_id')}}"/>
+@endsection
+
 @section('adsense')
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
