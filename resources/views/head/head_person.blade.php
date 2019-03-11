cd3ad43e94e951aa55a5f6ef74d2391a56bf7413
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-11 14:21:39 +0100

    series

--- resources/views/head/head_person.blade.php
+++ resources/views/head/head_person.blade.php
@@ -36,3 +36,16 @@ pass={
 @{{page_variables.notification_count>0?'('+page_variables.notification_count+') ':''}}
 @{{person.name}}{{ __('title.person') }}
 @endsection
+
+@section('meta_description')
+{{ __('long_texts.home.description') }}
+@endsection
+
+@section('og_tags')
+<meta property="og:url" content="{{url('/')}}/person/{{$id}}"/>
+<meta property="og:title" content="@{{person.name}} @{{age}}"/>
+<meta property="og:description" content="@{{person.place_of_birth}} - @{{person.birthday.length>0?person.birthday:''}}"/>
+<meta property="og:type" content="website"/>
+<meta property="og:image" content="{{config('constants.image.thumb_nail')[$image_quality]}}@{{person.profile_path}}"/>
+<meta property="fb:app_id" content="{{config('constants.facebook.app_id')}}"/>
+@endsection
