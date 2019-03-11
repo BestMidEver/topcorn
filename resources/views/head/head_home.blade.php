b2d18386f302b7e7517b1550a80e64eb61124598
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-11 13:25:57 +0100

    series

--- resources/views/head/head_home.blade.php
+++ resources/views/head/head_home.blade.php
@@ -1,3 +1,16 @@
 @section('title')
 {{ __('title.home') }}
 @endsection
+
+@section('meta_description')
+Learn what to watch tonight, based on your taste. Filter movies with original language, release year, genre and vote count. Completely free
+@endsection
+
+@section('og_tags')
+<meta property="og:url" content="{{url('/')}}"/>
+<meta property="og:title" content="{{$series_name}} ({{$series_year}})"/>
+<meta property="og:description" content="{{$series_plot}}"/>
+<meta property="og:type" content="website"/>
+<meta property="og:image" content="{{config('constants.image.fb_https')}}{{$poster_path}}"/>
+<meta property="fb:app_id" content="{{config('constants.facebook.app_id')}}"/>
+@endsection
