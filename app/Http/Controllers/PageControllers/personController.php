893c0b5e2ca837a6a948140d0005d4b64899f492
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-11 16:17:01 +0100

    series

--- app/Http/Controllers/PageControllers/personController.php
+++ app/Http/Controllers/PageControllers/personController.php
@@ -7,6 +7,7 @@ use App\Model\Rated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class personController extends Controller
 {    
