3d8527d98cebd1e5d17843b3347966216e513d99
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-14 19:26:46 +0100

    series

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -63,6 +63,22 @@
     </script>
 
     @yield('adsense')
+
+    <script type="text/javascript">
+amzn_assoc_placement = "adunit0";
+amzn_assoc_search_bar = "true";
+amzn_assoc_tracking_id = "topcornio-20";
+amzn_assoc_search_bar_position = "bottom";
+amzn_assoc_ad_mode = "search";
+amzn_assoc_ad_type = "smart";
+amzn_assoc_marketplace = "amazon";
+amzn_assoc_region = "US";
+amzn_assoc_title = "Shop Related Products";
+amzn_assoc_default_search_phrase = "one punch man";
+amzn_assoc_default_category = "All";
+amzn_assoc_linkid = "9fa35a0684981b57cbea8dc44a110ce7";
+</script>
+<script src="//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US"></script>
     
 </head>
 
