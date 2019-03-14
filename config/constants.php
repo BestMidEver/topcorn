0cb0fe103bad4145f95a29820a942ac44e011cb8
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-14 10:10:07 +0100

    series

--- config/constants.php
+++ config/constants.php
@@ -25,14 +25,14 @@ return array(
     'cover_error' => '/images/cover.png',
 
     'movie_card' => array(
-      'https://image.tmdb.org/t/p/w92_and_h138_bestv2', 
+      'https://image.tmdb.org/t/p/w300_and_h450_bestv2', 
       'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
       'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
     ),
     'movie_card_error' => '/images/noimage15_1.png',
 
     'rate_modal' => array(
-      'https://image.tmdb.org/t/p/w92_and_h138_bestv2',
+      'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
       'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
       'https://image.tmdb.org/t/p/w300_and_h450_bestv2',
     ),
