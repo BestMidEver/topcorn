1fe06403ece2a498f4e1b6d4baec04e1daac950a
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-15 11:41:26 +0100

    series

--- resources/views/head/head_movie.blade.php
+++ resources/views/head/head_movie.blade.php
@@ -69,7 +69,7 @@ pass={
 @endsection
 
 @section('amazon_affiliate')
-<div class="my-5">
+<div class="mt-4 mb-5">
 <script type="text/javascript">
 amzn_assoc_placement = "adunit0";
 amzn_assoc_tracking_id = "topcornio-20";
