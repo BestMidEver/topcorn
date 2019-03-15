7971fba5aef51f72de79f0bdb110437cabf76c2f
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-15 15:11:49 +0100

    series

--- app/Http/Controllers/PageControllers/movieController.php
+++ app/Http/Controllers/PageControllers/movieController.php
@@ -71,7 +71,7 @@ class movieController extends Controller
 
         $amazon_variables_general = amazon_variables_general();
 
-    	return view('movie', compact('id', 'id_dash_title', 'image_quality', 'target', 'watched_movie_number', 'movie_title', 'movie_en_title', 'movie_plot', 'movie_year', 'poster_path', 'amazon_variables'), 'amazon_variables_general'))->with('watch_togethers', $watch_togethers);
+    	return view('movie', compact('id', 'id_dash_title', 'image_quality', 'target', 'watched_movie_number', 'movie_title', 'movie_en_title', 'movie_plot', 'movie_year', 'poster_path', 'amazon_variables', 'amazon_variables_general'))->with('watch_togethers', $watch_togethers);
     }
 
 
