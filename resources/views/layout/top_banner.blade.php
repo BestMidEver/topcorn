46e49501c0eddd600715ad8791277a502a78bd93
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-18 16:21:03 +0100

    series

--- resources/views/layout/top_banner.blade.php
+++ resources/views/layout/top_banner.blade.php
@@ -1,8 +1,9 @@
 @if(Auth::check())
 	@if(Auth::id()==7)
 <div class="p-5">
-	<div class="h6">An offer that you can't refuse.</div>
-	<div>If you liked using Topcorn.io contribute us by signing up Amazon Prime 30-day free trial.</div>
+	<p class="h6">An offer that you can't refuse.</p>
+	<p>If you liked using Topcorn.io contribute us by signing up Amazon Prime 30-day free trial.</p>
+	<p>It is completely free and you can cancel it any time. We will get benefit even if you don't complete the full 30 days.</p>
 </div>
 	@endif
 @endif
