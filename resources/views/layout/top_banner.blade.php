aa06fc36d2b701eee8f9f16790d4ff4bb677251b
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-18 16:13:06 +0100

    series

--- resources/views/layout/top_banner.blade.php
+++ resources/views/layout/top_banner.blade.php
@@ -2,6 +2,7 @@
 	@if(Auth::id()==7)
 <div class="p-5">
 	<div class="h6">An offer that you can't refuse.</div>
+	<div>If you liked using Topcorn.io contribute us by signing up Amazon Prime 30-day free trial.</div>
 </div>
 	@endif
 @endif
