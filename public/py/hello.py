bdfb5954c42e8f933f4590b543c51f8ef754a5e0
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 17:18:56 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -20,13 +20,9 @@ y = df['rate']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-model = MLPClassifier(hidden_layer_sizes=(15,15,15))
+model = MLPClassifier(hidden_layer_sizes=(4,8,15,10,4))
 model.fit(X, y)
-predictions = model.predict([[7,550],[7,389],[7,77],[7,603],[7,27205]
-,[7,1124],[7,180864],[7,640],[7,8392],[7,11194]
-,[7,9741],[7,203801],[7,9477],[7,115],[7,369972]
-,[7,152603],[7,33909],[7,89584],[7,509335],[7,401981]
-,[7,7345],[7,348],[7,449262],[7,102651],[7,131634]])#X_test)
+predictions = model.predict([[7,228967],[7,195589],[7,109414],[7,262543],[7,131634]])#X_test)
 
 #score = accuracy_score(y_test, predictions)
 print(predictions)
