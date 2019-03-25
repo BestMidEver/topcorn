f4c439825d965ff12ca884a0db5db5e4c1f0d29d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 15:28:11 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -17,7 +17,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 model = DecisionTreeClassifier()
 model.fit(X_train, y_train)
-predictions = model.predict([ [7, 603] ])
+predictions = model.predict(X_test)
 
 score = accuracy_score(y_test, predictions)
 print(score)
