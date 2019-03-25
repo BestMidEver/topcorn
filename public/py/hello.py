ec8d98e544a81e82550bd186e3c2bbe6e258618c
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 15:07:49 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
+import pymysql
 import pandas as pd
 
-db_connection = 'mysql://root:S1freyokki@localhost/laravel'
+db_connection = 'mysql+pymysql://root:S1freyokki@localhost/laravel'
 conn = create_engine(db_connection)
 
 df = pd.read_sql("SELECT rateds.user_id, rateds.movie_id, rateds.rate FROM rateds WHERE rateds.rate > 0", conn)
