8f13b241088f188ab491005460f82f8599a573ac
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 18:08:43 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report,confusion_matrix
 
 db_connection = 'mysql+pymysql://root:S1freyokki@localhost/laravel'
 conn = create_engine(db_connection)
@@ -24,5 +25,5 @@ model = MLPClassifier(hidden_layer_sizes=(8,8))
 model.fit(X, y)
 predictions = model.predict(X_test)
 
-score = accuracy_score(y_test, predictions)
+score = confusion_matrix(y_test, predictions)
 print(score)
