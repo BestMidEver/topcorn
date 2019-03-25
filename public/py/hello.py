31e18d279376a2d876bdcd2ba73bebcb442ebb77
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 18:23:55 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -27,7 +27,7 @@ scaler.fit(X_train)
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
 
-model = MLPClassifier(hidden_layer_sizes=(13,13,13),max_iter=500)
+model = MLPClassifier(hidden_layer_sizes=(13,13),max_iter=500)
 model.fit(X_train,y_train)
 
 predictions = model.predict(X_test)
