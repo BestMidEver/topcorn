7b85e727ad6900d4e2efdaab05aabe2f0dad7297
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 15:11:25 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -8,5 +8,6 @@ conn = create_engine(db_connection)
 df = pd.read_sql("SELECT rateds.user_id, rateds.movie_id, rateds.rate FROM rateds WHERE rateds.rate > 0", conn)
 
 X = df.drop(columns=['rate'])
+y = df['rate']
 
-print(X)
+print(y)
