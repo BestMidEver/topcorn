648f321e8b97388053014e1c74d92c94cc0c695d
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 15:24:56 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 import pymysql
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
 
 db_connection = 'mysql+pymysql://root:S1freyokki@localhost/laravel'
 conn = create_engine(db_connection)
@@ -11,9 +12,11 @@ df = pd.read_sql("SELECT rateds.user_id, rateds.movie_id, (rateds.rate - 1)*25 a
 X = df.drop(columns=['rate'])
 y = df['rate']
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
 model = DecisionTreeClassifier()
 model.fit(X, y)
 predictions = model.predict([ [7, 603] ])
 
-
-print(predictions)
+score = accuracy_score(y_test, predictions)
+print(score)
