7cc56ee0c3169b5889c32d6a7f851dd790ace842
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 19:44:48 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -12,16 +12,12 @@ conn = create_engine(db_connection)
 df = pd.read_sql("""
 	SELECT rateds.user_id, rateds.movie_id, IF(rateds.rate=1 OR rateds.rate=2, 0, IF(rateds.rate=2, 1, 2)) as rate
 	FROM rateds
-
-
-INNER JOIN
-    (SELECT user_id, Count(1) As count
-    FROM rateds
-    WHERE rate > 0
-    GROUP BY user_id) NG
-ON rateds.user_id = NG.user_id
-
-
+	INNER JOIN
+	    (SELECT user_id, Count(1) As count
+	    FROM rateds
+	    WHERE rate > 0
+	    GROUP BY user_id) NG
+	ON rateds.user_id = NG.user_id
 	LEFT JOIN movies ON rateds.movie_id = movies.id 
 	WHERE rateds.rate <> 0 AND NG.count > 100
 	""", conn)
@@ -31,7 +27,7 @@ y = df['rate']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-model = MLPClassifier(hidden_layer_sizes=(4,4,4,4,4,4,4))#,max_iter=500)
+model = MLPClassifier(hidden_layer_sizes=(4,4))#,max_iter=500)
 model.fit(X_train,y_train)
 
 predictions = model.predict(X_test)
