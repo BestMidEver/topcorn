9f6c3b1be0bbd894c1cd1d15cb4db76f88abc9ac
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 19:28:56 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -30,7 +30,7 @@ y = df['rate']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-model = MLPClassifier(hidden_layer_sizes=(4,4,4,4,4,4,4)#,max_iter=500)
+model = MLPClassifier(hidden_layer_sizes=(4,4,4,4,4,4,4))#,max_iter=500)
 model.fit(X_train,y_train)
 
 predictions = model.predict(X_test)
