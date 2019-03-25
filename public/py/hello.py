e4f28979bb3381b015e83faae29e272af068de66
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-25 17:13:16 +0100

    series

--- public/py/hello.py
+++ public/py/hello.py
@@ -9,7 +9,7 @@ db_connection = 'mysql+pymysql://root:S1freyokki@localhost/laravel'
 conn = create_engine(db_connection)
 
 df = pd.read_sql("""
-	SELECT rateds.user_id, rateds.movie_id, rateds.rate
+	SELECT rateds.user_id, rateds.movie_id, rateds.rate, ROUND(movies.vote_average, 0) as vote_average
 	FROM rateds 
 	LEFT JOIN movies ON rateds.movie_id = movies.id 
 	WHERE rateds.rate > 0
