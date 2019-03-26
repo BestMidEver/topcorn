0d0c6bfb638fac7ec9cac075993cf8e3b450f9c8
Uygar Yılmaz <uygary@anadolu.edu.tr>
2019-03-26 16:00:28 +0100

    machine learning

--- public/py/hello.py
+++ public/py/hello.py
@@ -28,9 +28,10 @@ y = df['rate']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
 model = MLPClassifier(hidden_layer_sizes=(8,100,8))#,max_iter=250)
-model.fit(X,y)
+model.fit(X_train,y_train)
 
-predictions = model.predict([[7,102651, 7878, 7]])
+#predictions = model.predict([[7,102651, 7878, 7]])
+predictions = model.predict(X_test)
 
-#score = accuracy_score(y_test, predictions)
-print(predictions)
+score = accuracy_score(y_test, predictions)
+print(score)
