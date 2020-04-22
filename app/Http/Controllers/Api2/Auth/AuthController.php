f52753c9d4cf5e74d303897bc23a9e9be94686aa
Uygar Yılmaz <me20@mediasales.hu>
2020-04-22 17:39:53 +0200

    'login'

--- app/Http/Controllers/Api2/Auth/AuthController.php
+++ app/Http/Controllers/Api2/Auth/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users|confirmed',
             'password' => 'required|string|min:6|confirmed',
         ]);
 
