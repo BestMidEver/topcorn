630220ea98031d869f9fa14a14312322eae8eabc
Uygar Yılmaz <me20@mediasales.hu>
2020-04-22 17:19:18 +0200

    'login'

--- app/Http/Controllers/Api2/Auth/AuthController.php
+++ app/Http/Controllers/Api2/Auth/AuthController.php
@@ -42,7 +42,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:6|same:password_confirmation',
         ]);
 
         return User::create([
