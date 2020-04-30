0f2cb51ededa5e74116b8d163fccb4db11c8b92f
Uygar Yılmaz <me20@mediasales.hu>
2020-04-30 12:48:25 +0200

    'default1'

--- app/Http/Middleware/id_dash_moviename.php
+++ app/Http/Middleware/id_dash_moviename.php
@@ -14,7 +14,7 @@ class id_dash_moviename
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next, $type, $pi, $s)
+    public function handle($request, Closure $next)
     {
         $movie = Movie::where(['id' => $request->id]);
 
