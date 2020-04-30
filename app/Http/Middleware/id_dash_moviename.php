0bd59260dfc11377e7504911ea91891a0546c4ed
Uygar Yılmaz <me20@mediasales.hu>
2020-04-30 12:49:05 +0200

    'default1'

--- app/Http/Middleware/id_dash_moviename.php
+++ app/Http/Middleware/id_dash_moviename.php
@@ -16,7 +16,6 @@ class id_dash_moviename
      */
     public function handle($request, Closure $next)
     {
-        return $request;
         $movie = Movie::where(['id' => $request->id]);
 
         if($movie->count() > 0) $movie = $movie->first();
