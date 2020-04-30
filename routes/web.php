e4820496fc4fabd02208b2e7e6a04d6dc9b3081e
Uygar Yılmaz <me20@mediasales.hu>
2020-04-30 22:00:11 +0200

    'default'

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use App\Jobs\SuckDataJob;
+use App\Model\Recent_movie;
 use App\Jobs\RefreshSiteMapJob;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
@@ -276,6 +277,11 @@ Route::apiResource('api/review_like','ApiControllers\ReviewLikeController');
 ///////////////////////////////// SUCK DATA (ONLY ARCHITECT) /////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 Route::get('suckData', function(){
+	//$recent = Recent_movie::updateOrCreate(array('user_id' => $this->userId, 'movie_id' => $this->objId));
+	//$recent->touch();
+	$keep = Recent_movie::where('user_id', $this->userId)->latest('updated_at')->take(config('constants.recently_viewed.latest_n'))->pluck('id');
+	Recent_movie::where('user_id', $this->userId)->whereNotIn('id', $keep)->delete();
+	return Recent_movie::where('user_id', $this->userId)->latest('updated_at');
 	//SuckDataJob::dispatch()->onQueue("low");
 	return 'not sucking data.';
 });
