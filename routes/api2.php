e4219bb49a565a0cafe81ebc2beb9260b069ade9
Uygar Yılmaz <me20@mediasales.hu>
2020-05-01 05:09:56 +0200

    'default'

--- routes/api2.php
+++ routes/api2.php
@@ -14,7 +14,6 @@ Route::middleware('auth:api')->group(function () {
     });
     Route::get('/pluckId/interactedMoviesSeries', 'Api2\MergeController@pluckId');
     Route::get('/recenltyVisiteds', 'Api2\SearchController@recenltyVisiteds');
-    Route::get('/recenltyVisited/ww', 'Api2\MergeController@pluckId');
 
     Route::post('/logout', 'Api2\Auth\AuthController@logout');
 });
