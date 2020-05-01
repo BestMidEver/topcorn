abb8e8a33d993c6e5d37602e2b067bab64696188
Uygar Yılmaz <me20@mediasales.hu>
2020-05-01 04:57:43 +0200

    'default'

--- app/Http/Controllers/Api2/SearchController.php
+++ app/Http/Controllers/Api2/SearchController.php
@@ -13,7 +13,7 @@ class SearchController extends Controller
     {
         $movies = DB::table('recent_movies')
         ->where('recent_movies.id', Auth::user()->id)
-        ->join('movies', 'movies.id', '=', 'recent_movies.movie_id')
+        -/* >join('movies', 'movies.id', '=', 'recent_movies.movie_id')
         ->leftjoin('rateds', function ($join) {
             $join->on('rateds.movie_id', '=', 'recent_movies.movie_id')
             ->where('rateds.user_id', '=', Auth::user()->id);
@@ -38,7 +38,7 @@ class SearchController extends Controller
             'rateds.rate as rate_code',
             'laters.id as later_id',
             'bans.id as ban_id'
-        )
+        ) */
         ->orderBy('recent_movies.updated_at', 'desc');
 
 
