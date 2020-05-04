6bdb9cb79b09452f368576108ff9e5174f269f76
Uygar Yılmaz <me20@mediasales.hu>
2020-05-04 16:42:44 +0200

    'default'

--- app/Http/Controllers/Api2/SearchController.php
+++ app/Http/Controllers/Api2/SearchController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class SearchController extends Controller
 {
