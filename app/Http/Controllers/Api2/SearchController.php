295da9432769549d983423344a94cc1b3bfc8274
Uygar Yılmaz <me20@mediasales.hu>
2020-05-04 15:29:51 +0200

    'default'

--- app/Http/Controllers/Api2/SearchController.php
+++ app/Http/Controllers/Api2/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api2;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\App;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
