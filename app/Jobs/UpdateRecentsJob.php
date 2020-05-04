56191cdec022fa3cb3e2fea7be181343645a64b8
Uygar Yılmaz <me20@mediasales.hu>
2020-05-04 12:37:55 +0200

    'default'

--- app/Jobs/UpdateRecentsJob.php
+++ app/Jobs/UpdateRecentsJob.php
@@ -54,7 +54,7 @@ class UpdateRecentsJob implements ShouldQueue
             $recent->touch();
             Recent_person::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
         } else if($this->type === 'user') {
-            $user = User::where(id, $this->objId);
+            $user = User::where('id', $this->objId);
             if(!$user->count() > 0) return;
             $recent = Recent_user::updateOrCreate(array('user_id' => $this->userId, 'subject_id' => $this->objId));
             $recent->touch();
