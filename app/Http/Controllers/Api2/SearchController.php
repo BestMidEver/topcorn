2b1656e5a71ed87b6dfdb3b14d7c60e469929aea
Uygar Yılmaz <me20@mediasales.hu>
2020-05-04 15:59:25 +0200

    'default'

--- app/Http/Controllers/Api2/SearchController.php
+++ app/Http/Controllers/Api2/SearchController.php
@@ -142,12 +142,12 @@ class SearchController extends Controller
             'listes.updated_at',
             DB::raw('LEFT(listes.entry_1 , 50) AS entry_1'),
             DB::raw('LEFT(listes.entry_1 , 51) AS entry_1_raw'),
-            'm1.'.App::getlocale().'_poster_path as m1_poster_path',
-            'm2.'.App::getlocale().'_poster_path as m2_poster_path',
-            'm3.'.App::getlocale().'_poster_path as m3_poster_path',
-            'm4.'.App::getlocale().'_poster_path as m4_poster_path',
-            'm5.'.App::getlocale().'_poster_path as m5_poster_path',
-            'm6.'.App::getlocale().'_poster_path as m6_poster_path'
+            'm1.'.App::getlocale().'_cover_path as m1_cover_path',
+            'm2.'.App::getlocale().'_cover_path as m2_cover_path',
+            'm3.'.App::getlocale().'_cover_path as m3_cover_path',
+            'm4.'.App::getlocale().'_cover_path as m4_cover_path',
+            'm5.'.App::getlocale().'_cover_path as m5_cover_path',
+            'm6.'.App::getlocale().'_cover_path as m6_cover_path'
         )
         ->groupBy('listes.id')
         ->orderBy('listes.updated_at', 'desc');
