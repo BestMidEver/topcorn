903882eb6139fea3855bb23719201133783d15f8
Uygar Yılmaz <me20@mediasales.hu>
2020-05-08 10:22:21 +0200

    'default'

--- app/Http/Controllers/Api2/QuickVoteController.php
+++ app/Http/Controllers/Api2/QuickVoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api2;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class QuickVoteController extends Controller
 {
