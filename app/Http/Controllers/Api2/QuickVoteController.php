093792a2f6f2a6eb1f4b1773243930a2f7c16ee1
Uygar Yılmaz <me20@mediasales.hu>
2020-05-08 11:26:06 +0200

    'default'

--- app/Http/Controllers/Api2/QuickVoteController.php
+++ app/Http/Controllers/Api2/QuickVoteController.php
@@ -49,14 +49,13 @@ class QuickVoteController extends Controller
             'movies.release_date',
             'movies.original_title as original_title',
             'movies.'.Auth::User()->lang.'_title as title',
-            //'movies.'.Auth::User()->lang.'_poster_path as poster_path',
             'movies.'.Auth::User()->lang.'_cover_path as cover_path',
             'rateds.rate as rate_code',
             'laters.id as later_id',
             'bans.id as ban_id'
         );
 
-        return response()->json($movies->get());
+        return response()->json($movies->take(5)->get());
     }
 
     private function getQuickVoteMovies($objId)
@@ -158,13 +157,12 @@ class QuickVoteController extends Controller
             'series.first_air_date',
             'series.original_name',
             'series.'.Auth::User()->lang.'_name as name',
-            //'series.'.Auth::User()->lang.'_poster_path as poster_path',
             'series.'.Auth::User()->lang.'_backdrop_path as backdrop_path',
             'series_rateds.rate as rate_code',
             'series_laters.id as later_id',
             'series_bans.id as ban_id'
         );
 
-        return response()->json($series->take(2)->get());
+        return response()->json($series->take(5)->get());
     }
 }
