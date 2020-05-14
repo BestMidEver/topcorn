6cf7327c5c11a48116ddb2d6c15f89af7aacc1c7
Uygar Yılmaz <me20@mediasales.hu>
2020-05-14 18:30:13 +0200

    'default'

--- app/Http/Controllers/Api2/RateController.php
+++ app/Http/Controllers/Api2/RateController.php
@@ -78,8 +78,12 @@ class RateController extends Controller
     private function sendMovieSeriesReview($mode, $request)
     {
         $review = strip_tags($request->review);
-        if($review == '') Review::where(array('user_id' => Auth::id(), 'movie_series_id' => $request->obj_id, 'mode' => $mode, 'season_number' => null, 'episode_number' => null))->delete();
-        else Review::updateOrCreate(
+        if($review == ''){
+            $reviewToDelete = Review::where(array('user_id' => Auth::id(), 'movie_series_id' => $request->obj_id, 'mode' => $mode, 'season_number' => null, 'episode_number' => null));
+            return $reviewToDelete->first()->id;
+            $reviewToDelete->delete();
+        }
+        else $data = Review::updateOrCreate(
             array('user_id' => Auth::id(), 'movie_series_id' => $request->obj_id, 'mode' => $mode, 'season_number' => null, 'episode_number' => null),
             array('review' => $review, 'lang' => Auth::User()->lang)
         );
