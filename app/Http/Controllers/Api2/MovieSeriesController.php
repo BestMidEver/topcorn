53fd41b9be87ff8a1f766be79f17057f28504420
Uygar Yılmaz <me20@mediasales.hu>
2020-05-21 14:20:14 +0200

    'default'

--- app/Http/Controllers/Api2/MovieSeriesController.php
+++ app/Http/Controllers/Api2/MovieSeriesController.php
@@ -124,7 +124,7 @@ class MovieSeriesController extends Controller
     {
         $return_val = DB::table('series')
         ->where('series.id', $objId)
-        ->leftjoin('series_seens', function ($join) use ($season) {
+        ->leftjoin('series_seens', function ($join) use ($season, $episode) {
             $join->on('series_seens.series_id', '=', 'series.id')
             ->where('series_seens.user_id', Auth::id())
             ->where('series_seens.season_number', $season)
