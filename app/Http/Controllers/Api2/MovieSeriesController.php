408f2dae1d17c0beb7bfac36daa3eaca5c2c8de6
Uygar Yılmaz <me20@mediasales.hu>
2020-05-21 14:12:55 +0200

    'default'

--- app/Http/Controllers/Api2/MovieSeriesController.php
+++ app/Http/Controllers/Api2/MovieSeriesController.php
@@ -127,10 +127,10 @@ class MovieSeriesController extends Controller
         ->where('series_seens.user_id', Auth::id())
         ->where('series_seens.season_number', $season)
         ->where('series_seens.episode_number', $episode)
-        /* ->select(
+        ->select(
             'series_seens.series_id',
             'series_seens.id as seen_id'
-        ) */;
+        );
 
         return response()->json($return_val->first());
     }
