4a64e00def3614248b52e67522ac93f8c943415b
Uygar Yılmaz <me20@mediasales.hu>
2020-05-21 15:28:16 +0200

    'default'

--- app/Http/Controllers/Api2/MovieSeriesController.php
+++ app/Http/Controllers/Api2/MovieSeriesController.php
@@ -133,7 +133,7 @@ class MovieSeriesController extends Controller
         ->select(
             'series.id as series_id',
             'series_seens.id as seen_id',
-            '1 as asd'
+            DB::raw('1 as p2'),
         );
 
         return response()->json($return_val->first());
