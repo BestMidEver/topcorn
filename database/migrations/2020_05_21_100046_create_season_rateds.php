9c4d4b1cfd1f382d3261f7943707bcc3cb2a595d
Uygar Yılmaz <me20@mediasales.hu>
2020-05-21 12:11:27 +0200

    'default'

--- database/migrations/2020_05_21_100046_create_season_rateds.php
+++ database/migrations/2020_05_21_100046_create_season_rateds.php
@@ -17,11 +17,11 @@ class CreateSeasonRateds extends Migration
             $table->increments('id');
             $table->integer('series_id');
             $table->integer('user_id');
-            $table->integer('season_number');
-            $table->integer('episode_number');
+            $table->integer('season');
+            $table->integer('episode');
             $table->tinyInteger('rate');
             $table->timestamps();
-            $table->unique(['series_id', 'user_id', 'season_number', 'episode_number']);
+            $table->unique(['series_id', 'user_id', 'season', 'episode']);
         });
     }
 
