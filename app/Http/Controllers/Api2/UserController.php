f15b9c74aa5f5d7b39304238801313691c9e5021
Uygar Yılmaz <me20@mediasales.hu>
2020-06-04 14:13:52 +0200

    'default'

--- app/Http/Controllers/Api2/UserController.php
+++ app/Http/Controllers/Api2/UserController.php
@@ -45,8 +45,8 @@ class UserController extends Controller
             'rateds.rate as user_rate_code',
             'laters.id as user_later_id',
             'bans.id as user_ban_id',
-            DB::raw('IF((rateds.updated_at OR 0)>(laters.updated_at OR 0), rateds.updated_at, laters.updated_at) as updated_at)'),
-            DB::raw('(rateds.updated_at OR 0)>(laters.updated_at OR 0) as zzzz)'),
+            DB::raw('IF(rateds.updated_at>laters.updated_at, rateds.updated_at, laters.updated_at) as updated_at)'),
+            DB::raw('rateds.updated_at>laters.updated_at as zzzz)'),
             'rateds.updated_at as rupdated',
             'laters.updated_at as lupdated'
             /* 'rateds.updated_at as updated_at' */
