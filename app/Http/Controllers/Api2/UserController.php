3126fea8e7798c1cfd8404867efd3e667cad379d
Uygar Yılmaz <me20@mediasales.hu>
2020-06-04 16:02:58 +0200

    'default'

--- app/Http/Controllers/Api2/UserController.php
+++ app/Http/Controllers/Api2/UserController.php
@@ -23,14 +23,10 @@ class UserController extends Controller
         ->leftjoin('rateds', function ($join) use ($userId) { $join->on('rateds.movie_id', '=', 'movies.id')->where('rateds.user_id', $userId); })
         ->leftjoin('laters', function ($join) use ($userId) { $join->on('laters.movie_id', '=', 'movies.id')->where('laters.user_id', '=', $userId); })
         ->leftjoin('bans', function ($join) use ($userId) { $join->on('bans.movie_id', '=', 'movies.id')->where('bans.user_id', '=', $userId); })
+        ->where(function ($query) { $query->where('rateds.rate', '>', 0)->orWhereNotNull('bans.id')->orWhereNotNull('laters.id'); })
         ->leftjoin('rateds as r2', function ($join) { $join->on('r2.movie_id', '=', 'movies.id')->where('r2.user_id', Auth::id()); })
         ->leftjoin('laters as l2', function ($join) { $join->on('l2.movie_id', '=', 'movies.id')->where('l2.user_id', '=', Auth::id()); })
         ->leftjoin('bans as b2', function ($join) { $join->on('b2.movie_id', '=', 'movies.id')->where('b2.user_id', '=', Auth::id()); })
-        ->where(function ($query) {
-            $query->where('rateds.rate', '>', 0)
-                  ->orWhereNotNull('bans.id')
-                  ->orWhereNotNull('laters.id');
-        })
         ->select(
             'movies.id as id',
             'movies.en_title as title',
@@ -49,32 +45,24 @@ class UserController extends Controller
             'rateds.updated_at as rupdated',
             'laters.updated_at as lupdated'
         );
-        
+        // Profile User Interaction Filter
         if($request->interaction == 'Watch Later') $return_val = $return_val->whereNotNull('laters.id');
         elseif(strpos($request->interaction, 'Rate-') !== false) $return_val = $return_val->where('rateds.rate', explode('-', $request->interaction)[1]);
         elseif($request->interaction == 'Hidden') $return_val = $return_val->whereNotNull('bans.id');
-
+        // Vote Average Filter
         if($request->min_vote_average > 0) $return_val = $return_val->where('movies.vote_average', '>', $request->min_vote_average);
-        
+        // Vote Count Filter
         if($request->min_vote_count > 0) $return_val = $return_val->where('movies.vote_count', '>', $request->min_vote_count);
-        
+        // User Hide Filter
         if(strpos($request->hide, 'Watch Later') !== false) $return_val = $return_val->whereNull('l2.id');
         if(strpos($request->hide, 'Already Seen') !== false) $return_val = $return_val->where(function ($query) { $query->where('r2.rate', '=', 0)->orWhereNull('r2.rate'); });
         if(strpos($request->hide, 'Hidden') !== false) $return_val = $return_val->whereNull('b2.id');
-
+        // Sorting
         if($request->sort == 'Most Popular') $return_val = $return_val->orderBy('popularity', 'desc');
         elseif($request->sort == 'Top Rated') $return_val = $return_val->orderBy('vote_average', 'desc');
         elseif($request->sort == 'Newest') $return_val = $return_val->orderBy('release_date', 'desc');
         elseif($request->sort == 'Alphabetical Order') $return_val = $return_val->orderBy('en_title', 'asc');
         else $return_val = $return_val->orderBy('updated_at', 'desc');
-
-        /* if($rate=='all'){
-            $return_val = $return_val->where('rateds.rate', '<>', 0)
-            ->paginate($pagin);
-        }else{
-            $return_val = $return_val->where('rateds.rate', $rate)
-            ->paginate($pagin);
-        } */
 
         /* foreach ($return_val as $row) {
             $row->updated_at = timeAgo(explode(' ', Carbon::createFromTimeStamp(strtotime($row->updated_at))->diffForHumans()));
