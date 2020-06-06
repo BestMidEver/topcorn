d420ca30a84f65361dc4aa980eb91358914f10fa
Uygar Yılmaz <me20@mediasales.hu>
2020-06-06 19:20:37 +0200

    'default'

--- app/Http/Controllers/Api2/UserController.php
+++ app/Http/Controllers/Api2/UserController.php
@@ -26,7 +26,16 @@ class UserController extends Controller
     public function getUserDetails(Request $request) {
         $userId = $request->id == -1 ? Auth::id() : $request->id;
         $user = User::where('id', $userId)->first();
-        return gettype ($user);
+        return gettype ([
+            'rated_movies' => $this->rateGrouped($userId, 'rateds'),
+            'rated_movie_count' => DB::table('rateds')->where('user_id', $userId)->where('rate', '>', 0)->count(),
+            'rated_series' => $this->rateGrouped($userId, 'series_rateds'),
+            'rated_series_count' => DB::table('series_rateds')->where('user_id', $userId)->where('rate', '>', 0)->count(),
+            'follower_count' => DB::table('follows')->where('follows.object_id', $userId)->count(),
+            'following_count' => DB::table('follows')->where('follows.subject_id', $userId)->count(),
+            'review_like_count' => DB::table('reviews')->where('reviews.user_id', '=', $userId)->leftjoin('review_likes', 'review_likes.review_id', '=', 'reviews.id')->where('review_likes.is_deleted', '=', 0)->whereNotNull('review_likes.id')->count(),
+            'review_count' => DB::table('reviews')->where('reviews.user_id', $userId)->count()
+        ]);
         return (object) array_merge((array) $user, (array) [
             'rated_movies' => $this->rateGrouped($userId, 'rateds'),
             'rated_movie_count' => DB::table('rateds')->where('user_id', $userId)->where('rate', '>', 0)->count(),
