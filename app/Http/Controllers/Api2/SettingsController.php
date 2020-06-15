cdc65b3b1e67f728a7a824940be28b0105fe3c4b
Uygar Yılmaz <me20@mediasales.hu>
2020-06-15 13:10:13 +0200

    'default'

--- app/Http/Controllers/Api2/SettingsController.php
+++ app/Http/Controllers/Api2/SettingsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
 
 class SettingsController extends Controller
 {
