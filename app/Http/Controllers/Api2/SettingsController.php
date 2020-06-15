d35218f8643af4ea04be5e4f7c974ab8841a61bc
Uygar Yılmaz <me20@mediasales.hu>
2020-06-15 10:34:31 +0200

    'default'

--- app/Http/Controllers/Api2/SettingsController.php
+++ app/Http/Controllers/Api2/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api2;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class SettingsController extends Controller
 {
