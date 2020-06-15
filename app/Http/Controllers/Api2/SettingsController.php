70ed26e2da5566641b2741fd9c6a3a0c4beb7ad3
Uygar Yılmaz <me20@mediasales.hu>
2020-06-15 13:29:33 +0200

    'default'

--- app/Http/Controllers/Api2/SettingsController.php
+++ app/Http/Controllers/Api2/SettingsController.php
@@ -55,6 +55,6 @@ class SettingsController extends Controller
 			$user->save();
 			return Response::make("", 204);
 		}
-		return response()->json(['errors' => ['current_password' => 'Current password is not correct.']], 401);
+		return response()->json(['errors' => ['current_password' => ['Current password is not correct.']]], 401);
 	}
 }
