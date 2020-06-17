84f08a219463c29c651ea7f3cf5756c1d2af0ba8
Uygar Yılmaz <me20@mediasales.hu>
2020-06-17 14:17:22 +0200

    'default'

--- app/Http/Controllers/Api2/UserController.php
+++ app/Http/Controllers/Api2/UserController.php
@@ -75,7 +75,7 @@ class UserController extends Controller
     }
 
     public function getUserMovies(Request $request)
-    {return 1;
+    {return $request->hide;
         $userId = $request->id;
         $hide = $request->hide ? implode(',', $request->hide) : 'Hidden';
         $return_val = DB::table('movies')
