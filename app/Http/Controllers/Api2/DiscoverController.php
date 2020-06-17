b826e6927447c2970383226e339b75be6266ca1a
Uygar Yılmaz <me20@mediasales.hu>
2020-06-17 11:51:52 +0200

    'default'

--- app/Http/Controllers/Api2/DiscoverController.php
+++ app/Http/Controllers/Api2/DiscoverController.php
@@ -21,7 +21,7 @@ class DiscoverController extends Controller
     }
 
     private function getPemosuMovies($request)
-    {return explode(',', $request->languages);
+    {return explode(',', $request->languages) ? 1 : 2;
         $subq = DB::table('rateds')
         ->where('rateds.user_id', Auth::id())
         ->where('rateds.rate', '>', 0)
