17ed3f1d24acc7fabacc502baaf58cc4b8e04a0d
Uygar Yılmaz <me20@mediasales.hu>
2020-06-21 20:32:31 +0200

    'default'

--- app/Http/Controllers/Api2/NotificationController.php
+++ app/Http/Controllers/Api2/NotificationController.php
@@ -10,14 +10,10 @@ use Illuminate\Support\Facades\Auth;
 class NotificationController extends Controller
 {
     public static function getNotificationButton() {
-        $notifications = DB::table('notifications')
-        ->where('notifications.is_seen', '=', 0);
-
-        $notifications = $notifications->where('notifications.user_id', Auth::id());
-
-        $notifications = $notifications->count();
-
-        return $notifications;
+        return DB::table('notifications')
+        ->where('notifications.is_seen', '=', 0)
+        ->where('notifications.user_id', Auth::id())
+        ->count();
     }
 
     public static function getNotifications(Request $request) {
