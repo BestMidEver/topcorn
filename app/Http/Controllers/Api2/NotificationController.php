51067411de3d72a8711f1f8fdc1d7e5d03487880
Uygar Yılmaz <me20@mediasales.hu>
2020-06-22 08:31:18 +0200

    'default'

--- app/Http/Controllers/Api2/NotificationController.php
+++ app/Http/Controllers/Api2/NotificationController.php
@@ -188,7 +188,7 @@ class NotificationController extends Controller
             }
 			array_push($return_val, $temp);
         }
-        $notifications->data = []];
+        $notifications->data = [];
         return [$notifications, $return_val];
     }
 }
