c6e07050382736093d131537b8edfde339507270
Uygar Yılmaz <me20@mediasales.hu>
2020-06-22 13:26:05 +0200

    'default'

--- app/Http/Controllers/Api2/NotificationController.php
+++ app/Http/Controllers/Api2/NotificationController.php
@@ -30,7 +30,7 @@ class NotificationController extends Controller
 				->where('reviews.id', $notification->multi_id)
         		->leftjoin('review_likes', 'review_likes.review_id', 'reviews.id')
                 ->where('review_likes.is_deleted', 0);
-				if($temp->first()->mode == 1) {
+				if($temp->get()[0]->mode == 1) {
 					$temp = $temp->join('movies', 'movies.id', 'reviews.movie_series_id')
                     ->join('users', 'users.id', 'review_likes.user_id')
             		->select(
