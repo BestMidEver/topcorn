962cf3ef328dfb9c351d70ec7cce71d5f0c86a4d
Uygar Yılmaz <me20@mediasales.hu>
2020-06-22 17:31:44 +0200

    'default'

--- app/Http/Controllers/Api2/NotificationController.php
+++ app/Http/Controllers/Api2/NotificationController.php
@@ -174,7 +174,7 @@ class NotificationController extends Controller
                 );
             }
             $notification->notification = $temp->paginate(1, ['*'], 'page', 1);
-            $notification->time_ago = Carbon::createFromTimeStamp(strtotime($notification->updated_at))->diffForHumans();
+            $notification->time_ago = Carbon::createFromTimeStamp(strtotime($notification->updated_at))->diffForHumans(true, true);
         }
         return $notifications;
     }
