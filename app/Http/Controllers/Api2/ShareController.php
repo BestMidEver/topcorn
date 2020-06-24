666134ef03d725779755c16544b1327cc60871a1
Uygar Yılmaz <me20@mediasales.hu>
2020-06-24 15:08:35 +0200

    'default'

--- app/Http/Controllers/Api2/ShareController.php
+++ app/Http/Controllers/Api2/ShareController.php
@@ -3,12 +3,25 @@
 namespace App\Http\Controllers\Api2;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class ShareController extends Controller
 {
     public function getShareObjectModalUsers($type, $objId)
     {
-        return $type;
+        $return_val = DB::table('follows')
+        ->where('follows.object_id', Auth::id())
+        ->where('is_deleted', 0)
+        ->join('users', 'users.id', 'follows.subject_id')
+        ->select(
+            'users.id as user_id',
+            'users.name as user_name'
+        )
+        ->orderBy('users.name', 'desc')
+        ->get();
+
+        return $return_val;
     }
 }
