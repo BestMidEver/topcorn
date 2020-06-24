4f65648a58c72c40683845d050c4bcd9994ce782
Uygar Yılmaz <me20@mediasales.hu>
2020-06-24 15:25:09 +0200

    'default'

--- app/Http/Controllers/Api2/ShareController.php
+++ app/Http/Controllers/Api2/ShareController.php
@@ -15,7 +15,7 @@ class ShareController extends Controller
         ->where('follows.object_id', Auth::id())
         ->where('is_deleted', 0)
         ->join('users', 'users.id', 'follows.subject_id')
-        ->leftjoin('sent_items', function ($join) {
+        ->leftjoin('sent_items', function ($join) use ($type, $objId) {
             $join->on('sent_items.receiver_user_id', 'users.id')
             ->where('sent_items.sender_user_id', Auth::id())
             ->where('sent_items.multi_id', $objId)
