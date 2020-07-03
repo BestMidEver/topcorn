7a5b51cd4bb50502e6692fa2938e0133984e3fc7
Uygar Yılmaz <me20@mediasales.hu>
2020-07-03 10:13:14 +0200

    'default'

---
+++ app/Http/Controllers/Api2/Auth/ForgotPasswordController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api2\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ForgotPasswordController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Api2/Auth/ResetPasswordController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api2\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ResetPasswordController extends Controller
+{
+    public function sendEmail(Request $request)
+    {return $request;
+        $this->validateEmail($request);
+
+        // We will send the password reset link to this user. Once we have attempted
+        // to send the link, we will examine the response then see the message we
+        // need to show to the user. Finally, we'll send out a proper response.
+        $response = $this->broker()->sendResetLink(
+            $request->only('email')
+        );
+
+        return $response == Password::RESET_LINK_SENT
+                    ? $this->sendResetLinkResponse($response)
+                    : $this->sendResetLinkFailedResponse($request, $response);
+    }
+}
