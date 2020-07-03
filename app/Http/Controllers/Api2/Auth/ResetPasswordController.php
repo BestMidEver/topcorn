8877d08dfb657117d0017e4851cafe6f3d0e6b47
Uygar Yılmaz <me20@mediasales.hu>
2020-07-03 11:07:17 +0200

    'default'

--- app/Http/Controllers/Api2/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api2/Auth/ResetPasswordController.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers\Api2\Auth;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\PasswordReset;
 
 class ResetPasswordController extends Controller
 {
     public function sendResetPasswordEmail(Request $request)
     {
         $this->validateEmail($request);
-
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
+        
         $response = $this->broker()->sendResetLink(
             $request->only('email')
         );
@@ -35,23 +35,64 @@ class ResetPasswordController extends Controller
     }
 
     public function resetPassword(Request $request)
-    {return $request;
+    {
         $this->validate($request, $this->rules(), $this->validationErrorMessages());
-
-        // Here we will attempt to reset the user's password. If it is successful we
-        // will update the password on an actual user model and persist it to the
-        // database. Otherwise we will parse the error and return the response.
+        
         $response = $this->broker()->reset(
             $this->credentials($request), function ($user, $password) {
-                $this->resetPassword($user, $password);
+                $this->changePassword($user, $password);
             }
         );
-
-        // If the password was successfully reset, we will redirect the user back to
-        // the application's home authenticated view. If there is an error we can
-        // redirect them back to where they came from with their error message.
+        
         return $response == Password::PASSWORD_RESET
                     ? $this->sendResetResponse($response)
                     : $this->sendResetFailedResponse($request, $response);
+    }
+
+    protected function rules()
+    {
+        return [
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed|min:6',
+        ];
+    }
+    
+    protected function validationErrorMessages()
+    {
+        return [];
+    }
+    
+    protected function credentials(Request $request)
+    {
+        return $request->only(
+            'email', 'password', 'password_confirmation', 'token'
+        );
+    }
+    
+    protected function changePassword($user, $password)
+    {
+        $user->password = Hash::make($password);
+
+        $user->setRememberToken(Str::random(60));
+
+        $user->save();
+
+        event(new PasswordReset($user));
+
+        return 1;
+    }
+    
+    protected function sendResetResponse($response)
+    {
+        return redirect($this->redirectPath())
+                            ->with('status', trans($response));
+    }
+    
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        return redirect()->back()
+                    ->withInput($request->only('email'))
+                    ->withErrors(['email' => trans($response)]);
     }
 }
