ff69ce6da010aa7a69e285de1e06ff124444105d
Uygar Yılmaz <me20@mediasales.hu>
2020-07-03 10:55:45 +0200

    'default'

--- app/Http/Controllers/Api2/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Api2/Auth/ResetPasswordController.php
@@ -34,5 +34,24 @@ class ResetPasswordController extends Controller
         return Password::broker();
     }
 
-    
+    public function resetPassword(Request $request)
+    {return $request;
+        $this->validate($request, $this->rules(), $this->validationErrorMessages());
+
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+                    ? $this->sendResetResponse($response)
+                    : $this->sendResetFailedResponse($request, $response);
+    }
 }
