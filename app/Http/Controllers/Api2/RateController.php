eaa01ec37fdeac8893cf7654a8dbe2b85f7f5bd9
Uygar Yılmaz <me20@mediasales.hu>
2020-07-21 17:30:37 +0200

    'default'

--- app/Http/Controllers/Api2/RateController.php
+++ app/Http/Controllers/Api2/RateController.php
@@ -120,6 +120,7 @@ class RateController extends Controller
             $data = $reviewToDelete->first();
             if($data) $data = $data->id;
             else $data = -1;
+            return $mode;
             Notification::where('user_id', Auth::id())->where('mode', 0)->delete();
             $reviewToDelete->delete();
         } else {
