7a4dca3c54d4511d70a9569238340f498e64d965
Uygar Yılmaz <me20@mediasales.hu>
2020-07-23 14:05:14 +0200

    'default'

--- resources/views/layout/appnew.blade.php
+++ resources/views/layout/appnew.blade.php
@@ -171,6 +171,7 @@
                             <!--<div class="fb-share-button" data-href="https://topcorn.xyz/" data-layout="button_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Ftopcorn.xyz%2F&amp;src=sdkpreparse"></a></div>-->
                             <a class="btn btn-link btn-sm fa40 text-muted" href="{{config('constants.instagram.our_link')}}" target="_blank" data-toggle="tooltip" data-placement="top" title="{{ __('general.users_instagram') }}"><i class="fab fa-instagram"></i></a>
                             <a class="btn btn-link btn-sm fa40 text-muted" href="{{config('constants.android.our_link')}}" target="_blank" data-toggle="tooltip" data-placement="top" title="{{ __('general.users_android') }}"><i class="fab fa-android"></i></a>
+                            <a class="btn btn-link btn-sm fa40 text-muted" href="{{config('constants.patreon.our_link')}}" target="_blank" data-toggle="tooltip" data-placement="top" title="{{ __('navbar.patreon') }}"><i class="fab fa-patreon"></i></a>
                             <!-- <a class="btn btn-link btn-sm fa40 text-muted" href="{{config('constants.twitter.our_link')}}" target="_blank" data-toggle="tooltip" data-placement="top" title="{{ __('general.users_twitter') }}"><i class="fab fa-twitter-square"></i></a> -->
                         </div>
                         <div class="text-middle-light small py-2"><span>© 2020 {{ config('app.name') }}. {{ __('navbar.all_rights_reserved') }}</span></div>
