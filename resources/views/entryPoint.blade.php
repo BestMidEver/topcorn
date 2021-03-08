7fef3f55ad5bac61e043d99325d56cddc48a021e
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-03-08 01:22:35 +0100

    entry point

---
+++ resources/views/entryPoint.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+
+<div id="app">
+	<app></app>
+</div>
+
+</body>
+<script src=" mix(js/app.js) "></script>
+</html>
