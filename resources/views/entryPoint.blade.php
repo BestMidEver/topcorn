3504a6f3ce5ea60efeedd496e542c0b2a74bfd88
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-03-08 01:45:40 +0100

    Revert "module"
    This reverts commit 172eb392ab86f2664c2f0a37f6f069d70f3331de.

--- resources/views/entryPoint.blade.php
+++ resources/views/entryPoint.blade.php
@@ -10,5 +10,5 @@
 </div>
 
 </body>
-<script type="module" src="/vue/app.js?v={{config('constants.version')}}"></script>
+<script src="/vue/app.js?v={{config('constants.version')}}"></script>
 </html>
