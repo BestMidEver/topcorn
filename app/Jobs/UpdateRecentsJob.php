5696c3843cee518278799dc76ff07b58fc82b795
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-08-29 22:28:45 +0200

    a

--- app/Jobs/UpdateRecentsJob.php
+++ app/Jobs/UpdateRecentsJob.php
@@ -41,7 +41,7 @@ class UpdateRecentsJob implements ShouldQueue
      */
     public function handle()
     {
-        /* if($this->type === 'movie') {
+        if($this->type === 'movie') {
             $recent = Recent_movie::updateOrCreate(array('user_id' => $this->userId, 'movie_id' => $this->objId));
             $recent->touch();
             Recent_movie::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
@@ -53,17 +53,18 @@ class UpdateRecentsJob implements ShouldQueue
             $recent = Recent_person::updateOrCreate(array('user_id' => $this->userId, 'person_id' => $this->objId));
             $recent->touch();
             Recent_person::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
+        } else if($this->type === 'user') {
+            $user = User::where('id', $this->objId);
+            if(!$user->count() > 0) return;
+            $recent = Recent_user::updateOrCreate(array('user_id' => $this->userId, 'subject_id' => $this->objId));
+            $recent->touch();
+            Recent_user::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
         } else if($this->type === 'list') {
             $list = Liste::where('id', $this->objId)->where('visibility', '=', 1)->where('user_id', '<>', $this->userId);
             if(!$list->count() > 0) return;
             $recent = Recent_list::updateOrCreate(array('user_id' => $this->userId, 'list_id' => $this->objId));
             $recent->touch();
             Recent_list::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
-        } else { */
-            $recent = Recent_user::updateOrCreate(array('user_id' => 1, 'subject_id' => 1));
-            /* $recent = Recent_user::updateOrCreate(array('user_id' => $this->userId, 'subject_id' => $this->objId));
-            $recent->touch();
-            Recent_user::where('user_id', $this->userId)->latest('updated_at')->skip(config('constants.recently_viewed.latest_n'))->take(2)->get()->each(function($row){ $row->delete(); });
-        } */
+        }
     }
 }
