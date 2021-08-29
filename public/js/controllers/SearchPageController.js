8d66c85b1ed14d7e75e66ef421c924f0748fac54
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-08-29 18:19:01 +0200

    a

--- public/js/controllers/SearchPageController.js
+++ public/js/controllers/SearchPageController.js
@@ -59,6 +59,7 @@ MyApp.controller('SearchPageController', function($scope, $http, $anchorScroll, 
 	$scope.generalinput='';
 	$scope.get_page_data = function()
 	{
+		console.log('GET PAGE DATA')
 		var temp=$scope.generalinput.replace(/ /g , "%20");
 		if(temp.length == 0){
 			$scope.reset_tab();
