d2da3345151557f92ca0531463d66fbf2b70c903
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-08-31 12:54:55 +0200

    a

--- resources/views/accountinterface.blade.php
+++ resources/views/accountinterface.blade.php
@@ -4,7 +4,7 @@
 
 @section('body')
 <!-- Topcorn commercials -->
-<div id="carouselExampleSlidesOnly" class="carousel slide mt-3" data-ride="carousel" data-interval="100000">
+<div id="carouselExampleSlidesOnly" class="carousel slide mt-3" data-ride="carousel" data-interval="1000">
   <div class="carousel-inner bg-secondary" style="height: 100px">
     <div class="carousel-item active d-flex justify-content-center align-items-center h-100 text-center h4">
 		<div>If enjoy using topcorn, consider supporting us <a class="btn btn-link btn-sm fa40" style="color:#ff424d" href="{{config('constants.patreon.our_link')}}" target="_blank" data-toggle="tooltip" data-placement="top" title="{{ __('navbar.patreon') }}"><i class="fab fa-patreon"></i></a></div>
