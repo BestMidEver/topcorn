5ec18a8f9fe84f9dd0b2362b3412a31b04b729f7
Uygar Yilmaz <uygary@anadolu.edu.tr>
2021-08-31 12:23:39 +0200

    a

--- resources/views/accountinterface.blade.php
+++ resources/views/accountinterface.blade.php
@@ -4,7 +4,19 @@
 
 @section('body')
 <!-- Topcorn commercials -->
-If enjoy using topcorn, consider supporting us
+<div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="If enjoy using topcorn, consider supporting us">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+</div>
 <!-- Topcorn commercials -->
 
 
